c0027bd94cf86b888f50aada3b8f3d59b22e3412
dimzhead <dimzhead@gmail.com>
2020-07-29 19:22:10 -0500

    fixed-web_test-issue

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\dimzh\OneDrive\Desktop\flutter_projects\talawa"
+export "FLUTTER_APPLICATION_PATH=C:\Users\dimzh\OneDrive\Desktop\flutter_projects\talawa2\talawa"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
