f7648164b7fa1d74fc059f53a0055a86ef709c88
Deandrew Moore <deamoore07@gmail.com>
2020-07-31 18:15:44 -0500

    commit beofre merge into master

--- lib/views/pages/switch_org_page.dart
+++ lib/views/pages/switch_org_page.dart
@@ -19,7 +19,8 @@ class _SwitchOrganizationState extends State<SwitchOrganization> {
   int isSelected = 0;
   Preferences preferences = Preferences();
   static String itemIndex;
-
+  List userOrg = [];
+  
   @override
   void initState() {
     super.initState();
@@ -32,49 +33,112 @@ class _SwitchOrganizationState extends State<SwitchOrganization> {
     setState(() {
       userID = id;
     });
+    fetchUserDetails();
+  }
+
+  Future fetchUserDetails() async {
+    GraphQLClient _client = graphQLConfiguration.clientToQuery();
+
+    QueryResult result = await _client.query(QueryOptions(
+        documentNode: gql(_query.fetchUserInfo), variables: {'id': userID}));
+    if (result.hasException) {
+      print(result.exception);
+      showError(result.exception.toString());
+    } else if (!result.hasException) {
+      setState(() {
+        userOrg = result.data['users'][0]['joinedOrganizations'];
+      });
+      print(result.data['users'][0]['joinedOrganizations']);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-            title: const Text('Switch Organization'),),
-        body: Query(
-            options: QueryOptions(
-                documentNode: gql(_query.fetchUserInfo),
-                variables: {'id': userID}),
-            builder: (QueryResult result,
-                {VoidCallback refetch, FetchMore fetchMore}) {
-              if (result.hasException) {
-                print(result.exception);
-                return Center(
-                  child: Text(
-                    result.exception.toString(),
-                    style: TextStyle(fontSize: 16),
-                    textAlign: TextAlign.center,
-                  ),
-                );
-              } else if (result.loading) {
-                return Center(child: CircularProgressIndicator());
-              }
-                final userOrg = result.data['users'][0]['joinedOrganizations'];
-                print(result.data['users'][0]['joinedOrganizations']);
-              return ListView.separated(
-                  itemCount: userOrg.length,
-                  itemBuilder: (context, index) {
-                  
-                  return RadioListTile(
-                    groupValue: isSelected,
-                    title: Text(userOrg[index]['name'].toString() + '\n' + userOrg[index]['description'].toString()),
-                    value: index,
-                    onChanged: (val) {
-                      setState(() {
-                        isSelected = val;
-                        itemIndex = userOrg[index]['_id'].toString();
-                      });
+          title: const Text('Switch Organization'),
+        ),
+        body: userOrg.isEmpty
+            ? Center(child: CircularProgressIndicator())
+            : Column(
+                children: <Widget>[
+                  ListView.separated(
+                    itemCount: userOrg.length,
+                    itemBuilder: (context, index) {
+                      final userOrganization = userOrg[index];
+                      return RadioListTile(
+                        groupValue: isSelected,
+                        title: Text(userOrganization['name'].toString() +
+                            '\n' +
+                            userOrganization['description'].toString()),
+                        value: index,
+                        onChanged: (val) {
+                          setState(() {
+                            isSelected = val;
+                            itemIndex = userOrganization['_id'].toString();
+                          });
+                        },
+                      );
                     },
-                  );
-                 }, separatorBuilder: (BuildContext context, int index) {  return Divider(); },);
-            }));
+                    separatorBuilder: (BuildContext context, int index) {
+                      return Divider();
+                    },
+                  ),
+                ],
+              ));
+  }
+
+  Widget showError(String msg) {
+    return Center(
+      child: Text(
+        msg,
+        style: TextStyle(fontSize: 16),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  _successToast(String msg) {
+    Widget toast = Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 12.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25.0),
+        color: Colors.green,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(msg),
+        ],
+      ),
+    );
+
+    fToast.showToast(
+      child: toast,
+      gravity: ToastGravity.BOTTOM,
+      toastDuration: Duration(seconds: 3),
+    );
+  }
+
+  _exceptionToast(String msg) {
+    Widget toast = Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 14.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25.0),
+        color: Colors.red,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(msg),
+        ],
+      ),
+    );
+
+    fToast.showToast(
+      child: toast,
+      gravity: ToastGravity.BOTTOM,
+      toastDuration: Duration(seconds: 3),
+    );
   }
 }
