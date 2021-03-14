b941286e74cd4d9e46678cc2793dacdd425cc9dc
CoderMayhem <namanmishra1900@gmail.com>
2021-03-14 10:54:12 +0530

    feat:adds password show/hide feature in Login and Regsiter forms

--- lib/views/pages/login_signup/register_form.dart
+++ lib/views/pages/login_signup/register_form.dart
@@ -38,6 +38,7 @@ class RegisterFormState extends State<RegisterForm> {
   GraphQLConfiguration graphQLConfiguration = GraphQLConfiguration();
   File _image;
   AuthController _authController = AuthController();
+  bool _obscureText = true;
 
   void toggleProgressBarState() {
     _progressBarState = !_progressBarState;
@@ -242,7 +243,7 @@ class RegisterFormState extends State<RegisterForm> {
             ),
             TextFormField(
               autofillHints: <String>[AutofillHints.password],
-              obscureText: true,
+              obscureText: _obscureText,
               controller: originalPassword,
               validator: (value) => Validator.validatePassword(value),
               textAlign: TextAlign.left,
@@ -252,6 +253,12 @@ class RegisterFormState extends State<RegisterForm> {
                     borderSide: BorderSide(color: Colors.white),
                     borderRadius: BorderRadius.circular(20.0)),
                 prefixIcon: Icon(Icons.lock),
+                suffixIcon: FlatButton(
+                  onPressed: _toggle,
+                  child: Icon(_obscureText
+                      ? Icons.visibility_off
+                      : Icons.visibility),
+                ),
                 labelText: "Password",
                 labelStyle: TextStyle(color: Colors.white),
                 focusColor: UIData.primaryColor,
@@ -429,5 +436,12 @@ class RegisterFormState extends State<RegisterForm> {
       gravity: ToastGravity.BOTTOM,
       toastDuration: Duration(seconds: 5),
     );
+  }
+
+  //function toggles _obscureText value
+  void _toggle() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
   }
 }
