83ec5f7a6cd74c6fff235305de5146cb28da21de
Sumitra Saksham <35868598+sumitra19jha@users.noreply.github.com>
2021-03-16 01:29:39 +0530

    Update switch_org_page.dart

--- lib/views/pages/organization/switch_org_page.dart
+++ lib/views/pages/organization/switch_org_page.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart';
 import 'package:talawa/services/Queries.dart';
 import 'package:talawa/services/preferences.dart';
 import 'package:talawa/utils/GQLClient.dart';
+import 'package:talawa/utils/globals.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:talawa/utils/uidata.dart';
 import 'package:talawa/views/pages/organization/profile_page.dart';
@@ -102,7 +103,6 @@ class _SwitchOrganizationState extends State<SwitchOrganization> {
       body: _progressBarState
           ? Center(child: CircularProgressIndicator())
           : ListView.separated(
-              padding: EdgeInsets.only(top: 10.0),
               itemCount: userOrg.length,
               itemBuilder: (context, index) {
                 return RadioListTile(
