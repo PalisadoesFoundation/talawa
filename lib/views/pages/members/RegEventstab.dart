5a036131da4f919bae29291e91958db11dc7dff7
Pranshul Aggarwal <pranshulaggarwal000@gmail.com>
2021-03-16 13:49:45 +0530

    added text if no tasks and events are there

--- lib/views/pages/members/RegEventstab.dart
+++ lib/views/pages/members/RegEventstab.dart
@@ -5,6 +5,7 @@ import 'package:talawa/utils/apiFuctions.dart';
 
 class RegisterdEvents extends StatefulWidget {
   Map member;
+
   RegisterdEvents({
     Key key,
     @required this.member,
@@ -37,17 +38,26 @@ class _RegisterdEventsState extends State<RegisterdEvents> {
 
   @override
   Widget build(BuildContext context) {
-    if(userEvents.length!=0) {
+    if (userEvents.length != 0) {
       if (userEvents[0]['title'] == null) {
         userEvents = [];
       }
     }
-    return ListView.builder(
-        itemCount: userEvents.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: Text('${userEvents[index]['title']}'),
+    return userEvents.length != 0
+        ? ListView.builder(
+            itemCount: userEvents.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: Text('${userEvents[index]['title']}'),
+              );
+            })
+        : Container(
+            child: Center(
+                child: Text(
+              "No registered events",
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+            )),
           );
-        });
   }
 }
