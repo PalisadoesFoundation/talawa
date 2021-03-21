3eea11769cd23d18d0cd38519783d7eeb65f4c97
ANURAG MISHRA <anuragmishra.ofc@gmail.com>
2021-03-21 13:41:41 +0530

    Update login_form.dart

--- lib/views/pages/login_signup/login_form.dart
+++ lib/views/pages/login_signup/login_form.dart
@@ -238,15 +238,14 @@ class LoginFormState extends State<LoginForm> {
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
-<<<<<<< HEAD
+
 
           
 
-          Center(child: Expanded(child:Text(msg))),
+          
 
-=======
-          Center(child: Text(msg)),
->>>>>>> upstream/master
+
+
         ],
       ),
     );
