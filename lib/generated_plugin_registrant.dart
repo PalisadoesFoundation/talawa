e911a241c35dd33ef2f0bcf028aef111718414b2
Hrishikesh Bhagwat <bhagwatcoder@gmail.com>
2021-03-30 23:28:17 +0530

    Made a fix to issue #524

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -7,6 +7,7 @@
 import 'package:connectivity_for_web/connectivity_for_web.dart';
 import 'package:file_picker/src/file_picker_web.dart';
 import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
