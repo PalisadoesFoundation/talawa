ae833b283b42356b13ee237028db8f13b35a77b3
raunak_k02 <raunakk728@gmail.com>
2021-03-31 08:30:38 +0530

    chore: proper comment for new changes

--- test/updateProfile_test.dart
+++ test/updateProfile_test.dart
@@ -4,11 +4,13 @@ import 'package:talawa/utils/validator.dart';
 void main() {
   test('Empty Email Test', () {
     var result = Validator.validateEmail('');
+    //Email should be non-empty
     expect(result, 'Email must not be left blank');
   });
 
   test('Invalid Email Test', () {
     var result = Validator.validateEmail('abc');
+    //Email should be valid
     expect(result, 'Please enter a valid Email Address');
   });
 
