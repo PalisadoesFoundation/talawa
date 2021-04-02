e44f859f2ef068745b2ea2424474cc4bc172ca62
raunak_k02 <raunakk728@gmail.com>
2021-04-02 17:36:40 +0530

    chore: modified text to const text

--- lib/views/pages/newsfeed/newsfeed.dart
+++ lib/views/pages/newsfeed/newsfeed.dart
@@ -126,7 +126,7 @@ class _NewsFeedState extends State<NewsFeed> {
         appBar: CustomAppBar('NewsFeed',key: Key('NEWSFEED_APP_BAR')),
         floatingActionButton: addPostFab(),
         body: postList.isEmpty
-            ? Text(
+            ? const Text(
               'No posts to show',
               key: Key('empty_newsfeed_text'),
               style: TextStyle(
