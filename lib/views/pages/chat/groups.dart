4e2760466a458d19bf3015ceb7ce11c80945bba1
raunak_k02 <raunakk728@gmail.com>
2021-04-02 18:46:58 +0530

    ptch: adds logic for fetching registered events

--- lib/views/pages/chat/groups.dart
+++ lib/views/pages/chat/groups.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 //pages are called here
 import 'package:talawa/utils/uidata.dart';
 import 'package:talawa/services/preferences.dart';
+import 'package:talawa/services/Queries.dart';
 import 'package:talawa/utils/apiFuctions.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 import 'package:talawa/views/pages/chat/chat.dart';
@@ -21,6 +22,41 @@ class _GroupsState extends State<Groups> {
   ApiFunctions apiFunctions = ApiFunctions();
   bool fetched = true;
   var events;
+
+  //variable for organization Id
+  String _currOrgId;
+
+  //function to get the events
+  Future<void> getEvents() async {
+    fetched = false;
+    final String currentOrgID = await preferences.getCurrentOrgId();
+    _currOrgId = currentOrgID;
+    Map result = await apiFunctions.gqlquery(Queries().fetchOrgEvents(currentOrgID));
+    // print(result);
+    eventList = result == null ? [] : result['events'].reversed.toList();
+    eventList.removeWhere((element) =>
+        element['title'] == 'Talawa Congress' ||
+        element['title'] == 'test' ||
+        element['title'] == 'Talawa Conference Test' ||
+        element['title'] == 'mayhem' ||
+        element['title'] == 'mayhem1' ||
+        element['isRegistered'] == false ||
+        element['organization']['_id'] != currentOrgID); //dont know who keeps adding these
+    // This removes all invalid date formats other than Unix time
+    eventList.removeWhere((element) => int.tryParse(element['startTime']) == null);
+    eventList.sort((a, b) {
+      return DateTime.fromMicrosecondsSinceEpoch(int.parse(a['startTime']))
+          .compareTo(DateTime.fromMicrosecondsSinceEpoch(int.parse(b['startTime'])));
+    });
+    // eventsToDates(eventList, DateTime.now());
+    setState(() {
+      displayedEvents = eventList;
+    });
+    fetched = true;
+
+    // print('orgID ==== $currentOrgID');
+    // print(displayedEvents);
+  }
   
   @override
   Widget build(BuildContext context) {
