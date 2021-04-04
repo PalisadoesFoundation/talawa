cb7069ab069ca4ac007fac51ff36dd7a4de652e9
Udit singh <Udit-singh@users.noreply.github.com>
2021-04-04 18:56:55 +0530

    RenderFlex overflowed issue fixed

--- lib/views/pages/organization/create_organization.dart
+++ lib/views/pages/organization/create_organization.dart
@@ -509,17 +509,12 @@ class _CreateOrganizationState extends State<CreateOrganization> { //defining th
 
   _exceptionToast(String msg) {
     Widget toast = Container(
-      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 14.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(25.0),
-        color: Colors.red,
-      ),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(msg),
-        ],
-      ),
+        padding: const EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25.0),
+          color: Colors.red,
+        ),
+        child: Text(msg)
     );
 
     fToast.showToast(
