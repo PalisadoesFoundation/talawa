aaaf6f8915462a414129514b76ca7f8537024833
Sumitra Saksham <sumitra19jha@gmail.com>
2021-04-10 21:22:17 +0530

    cli check update

--- test/unit_tests/commentModel_test.dart
+++ test/unit_tests/commentModel_test.dart
@@ -2,15 +2,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:talawa/model/comment.dart';
 
 void main() async {
-  CommentModel _commentModel;
-
-  setUp(() {
-      _commentModel = CommentModel("id", "test");
-  });
-
   group('Comment Model', () {
     test('Check individual values', () async {
-      _commentModel = CommentModel("id123", "test123");
+      CommentModel _commentModel = CommentModel("id123", "test123");
       expect(_commentModel.postId, 'id123');
       expect(_commentModel.comment.runtimeType, equals(String));
       expect(_commentModel.comment, isNotNull);
