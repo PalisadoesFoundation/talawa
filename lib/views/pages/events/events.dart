b018f19a0c057a579a59e4dc385b358ab164e331
raunak_k02 <raunakk728@gmail.com>
2021-04-15 17:28:12 +0530

    ptch: adds function to show toast

--- lib/views/pages/events/events.dart
+++ lib/views/pages/events/events.dart
@@ -558,4 +558,27 @@ class _EventsState extends State<Events> {
           );
         });
   }
+
+  //function to show exceptions
+  _exceptionToast(String msg) {
+    Widget toast = Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 14.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25.0),
+        color: Colors.red,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(msg),
+        ],
+      ),
+    );
+
+    fToast.showToast(
+      child: toast,
+      gravity: ToastGravity.BOTTOM,
+      toastDuration: Duration(seconds: 1),
+    );
+  }
 }
