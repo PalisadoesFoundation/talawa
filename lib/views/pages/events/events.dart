cf2a078718f10cbb84311295cb204aa5541624eb
raunak_k02 <raunakk728@gmail.com>
2021-04-15 20:03:22 +0530

    chore: undo changes from code formatter

--- lib/views/pages/events/events.dart
+++ lib/views/pages/events/events.dart
@@ -160,30 +160,30 @@ class _EventsState extends State<Events> {
   Future<void> getEvents() async {
     final String currentOrgID = await preferences.getCurrentOrgId();
     _currOrgId = currentOrgID;
-    Map result =
+      Map result =
       await apiFunctions.gqlquery(Queries().fetchOrgEvents(currentOrgID));
-    eventList = result == null ? [] : result['events'].reversed.toList();
-    eventList.removeWhere((element) =>
-        element['title'] == 'Talawa Congress' ||
-        element['title'] == 'test' ||
-        element['title'] == 'Talawa Conference Test' ||
-        element['title'] == 'mayhem' ||
-        element['title'] == 'mayhem1' ||
-        element['organization']['_id'] != currentOrgID); //dont know who keeps adding these
-    // This removes all invalid date formats other than Unix time
-    eventList.removeWhere((element) => int.tryParse(element['startTime']) == null);
-    eventList.sort((a, b) {
-      return DateTime.fromMicrosecondsSinceEpoch(
+      eventList = result == null ? [] : result['events'].reversed.toList();
+      eventList.removeWhere((element) =>
+          element['title'] == 'Talawa Congress' ||
+          element['title'] == 'test' ||
+          element['title'] == 'Talawa Conference Test' ||
+          element['title'] == 'mayhem' ||
+          element['title'] == 'mayhem1' ||
+          element['organization']['_id'] != currentOrgID); //dont know who keeps adding these
+      // This removes all invalid date formats other than Unix time
+      eventList.removeWhere((element) => int.tryParse(element['startTime']) == null);
+      eventList.sort((a, b) {
+        return DateTime.fromMicrosecondsSinceEpoch(
           int.parse(a['startTime']))
           .compareTo(
           DateTime.fromMicrosecondsSinceEpoch(int.parse(b['startTime'])));
-    });
-    eventsToDates(eventList, DateTime.now());
-    setState(() {
-      displayedEvents = eventList;
-    });
-    // print('orgID ==== $currentOrgID');
-    // print(displayedEvents);
+      });
+      eventsToDates(eventList, DateTime.now());
+      setState(() {
+        displayedEvents = eventList;
+      });
+      // print('orgID ==== $currentOrgID');
+      // print(displayedEvents);
   }
 
   //functions to edit the event
