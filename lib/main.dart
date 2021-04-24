499aebc96ef0c8f00625b6a24469535dbfab4f7f
Sumitra Saksham <sumitra19jha@gmail.com>
2021-04-24 17:26:47 +0530

    adding state to handle error and empty condition

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:provider/provider.dart';
 import 'package:talawa/controllers/auth_controller.dart';
 import 'package:talawa/controllers/org_controller.dart';
 import 'package:talawa/services/comment.dart';
+import 'package:talawa/services/post_provider.dart';
 import 'package:talawa/services/preferences.dart';
 import 'package:talawa/utils/gql_client.dart';
 import 'package:talawa/views/pages/_pages.dart';
@@ -33,7 +34,8 @@ Future<void> main() async {
         ChangeNotifierProvider<OrgController>(create: (_) => OrgController()),
         ChangeNotifierProvider<AuthController>(create: (_) => AuthController()),
         ChangeNotifierProvider<Preferences>(create: (_) => Preferences()),
-        ChangeNotifierProvider<CommentHandler>(create: (_) => CommentHandler())
+        ChangeNotifierProvider<CommentHandler>(create: (_) => CommentHandler()),
+        ChangeNotifierProvider<PostProvider>(create: (_) => PostProvider()),
       ],
       child: MyApp(),
     ));
