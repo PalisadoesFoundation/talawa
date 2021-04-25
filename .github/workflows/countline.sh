46fac0d86a282fa8a362357efdde15e5671a4274
StrangeNoob <sipun63427452@gmail.com>
2021-04-26 03:00:25 +0530

    Adding detail Comments in countline.sh

--- .github/workflows/countline.sh
+++ .github/workflows/countline.sh
@@ -2,7 +2,8 @@
 maxLine=$1;
 
 # Listing of Files which have more line of code that maxLine
-# if you need to exclude files or directory please look ':!:<path-to-directory/file>' 
+# if you need to exclude files or directory use this ':!:<path-to-directory/file>' 
+# Please have a look at this if you have doubt https://stackoverflow.com/questions/36753573/how-do-i-exclude-files-from-git-ls-files
 files=$(git ls-files '*.dart' ':!:lib/services/*.dart' | xargs wc -l | awk -v max=$maxLine '$1 > max' | sed '$d' | sort -rn)
 
 # No. of Files which have more line of code that maxLine 
