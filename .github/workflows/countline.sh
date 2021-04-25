894174fe037868e286a35c2bab3770b4347d1575
StrangeNoob <sipun63427452@gmail.com>
2021-04-26 00:32:23 +0530

    Change in countline.sh

--- .github/workflows/countline.sh
+++ .github/workflows/countline.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+maxLine=$1;
 
-files=$(find . -type f -name="*.dart" -type d -name "../../lib/services/" -prune   | xargs wc -l | awk '$1 > 300'| sed '$d')
-echo "$files"
+files=$(git ls-files '*.dart' ':!:lib/services/*.dart' | xargs wc -l | awk -v max=$maxLine '$1 > max' | sed '$d' | sort -rn)
+
+no_of_files=$(git ls-files '*.dart' ':!:lib/services/*.dart' | xargs wc -l | awk -v max=$maxLine '$1 > max' | sed '$d' | wc -l | sort -rn)
+
 if [[ "$files" == "" ]]
 	then
 		exit 0
 	else
+		echo "Below $no_of_files files have more than $maxLine lines"
+		echo "Lines  Path"
 		echo "$files"
 		exit 1
 fi
+
