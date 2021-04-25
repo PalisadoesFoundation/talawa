bddab088ff61aac8ea2bbd125f2d797fce13a28f
StrangeNoob <sipun63427452@gmail.com>
2021-04-25 16:16:43 +0530

    Add countline.sh

---
+++ .github/workflows/countline.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+files=$(git ls-files '*.dart' | xargs wc -l | awk '$1 > 300'| sed '$d')
+
+if [[ "$files" == "" ]]
+	then
+		exit 0
+	else
+		echo "$files"
+		exit 1
+fi
+
+echo "$files"
