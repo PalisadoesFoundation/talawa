a14f2bbafdeea4ec37675c6ba6d8dde1175e8c65
utkarshshendge <utkarshcpark@gmail.com>
2021-04-29 01:13:35 +0530

    add file custom_toast.dart

---
+++ lib/utils/custom_toast.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:talawa/utils/uidata.dart';
+
+class CustomToast {
+  static void sucessToast(
+      {@required String msg, Color toastColor = UIData.toastSucessColor}) {
+    Fluttertoast.showToast(msg: msg, backgroundColor: toastColor);
+  }
+
+  static void exceptionToast(
+      {@required String msg, Color toastColor = UIData.toastErrorColor}) {
+    Fluttertoast.showToast(msg: msg, backgroundColor: toastColor);
+  }
+}
