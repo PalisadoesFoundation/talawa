8eb243e21c59d0b4979ce739b80319781ce5efb8
Sumitra Saksham <sumitra19jha@gmail.com>
2021-04-28 22:19:40 +0530

    structure added

--- .github/workflows/countline.py
+++ .github/workflows/countline.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
-
 import os
 import glob
 import sys
 
-#get current working dir, set count, and select file delimiter
+#get current working dir
 path = os.getcwd()
-#os.chdir(path)
-
 
 #parses through files and saves to a dict
 names={}
@@ -16,12 +13,24 @@ for fn in glob.glob(pathname = pathName, recursive = True):
     with open(fn) as f:
         names[fn]=sum(1 for line in f if line.strip() and not line.startswith('#'))    
 
+#if the line rule is voilated then value is changed to 1 
+isLineRuleVoilated = 0
+fileCount = 0
+for key, value in names.items():
+    if value > 1000:
+        isLineRuleVoilated = 1
+        fileCount += 1 
+        print("{}: {}".format(key, value))
 
-condition = 0
-for k, v in names.items():
-    if v > 1000:
-        condition = 1
-        print("{}: {}".format(k, v))
+"""Error
+If the number of coded lines is more than 300 in a File
+Than the error message will be displayed
 
-if condition != 0:
-    sys.exit("Please keep the following file size below 300 lines of code")
+Empty lines are not counted as part of the file size
+"""
+if isLineRuleVoilated != 0:
+    msg = "Below " + str(fileCount) + " files have more than 300 lines"
+    print(msg)
+    sys.exit(1)
+else:
+    sys.exit(0)
