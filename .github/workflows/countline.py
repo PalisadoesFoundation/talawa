b96d338c1cd14c1700a0c3538fc4a6f1f355f5fe
Sumitra Saksham <sumitra19jha@gmail.com>
2021-04-29 19:37:39 +0530

    pylint, pydocstyle, & pycodestyle compily

--- .github/workflows/countline.py
+++ .github/workflows/countline.py
@@ -1,50 +1,63 @@
 #!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""
+This is an countline script.
+
+It runs on lib and test directory to find files above 300 lines of code
+and print all files above 300 lines of code.
+"""
+
 import os
 import sys
 
-#get current working dir
-path = os.getcwd()
 
-#parses through files and saves to a dict
-fileNamesWithSize={}
+def main():
+    """Find, print and exit, for files having code lines above 300."""
+    # parses through files and saves to a dict
+    file_names_with_size = {}
 
-#libPath and testPath dir location
-libPath = os.path.expanduser(os.path.join(path, 'lib'))
-testPath = os.path.expanduser(os.path.join(path, 'test'))
+    # libPath and testPath dir location
+    lib_path = os.path.expanduser(os.path.join(os.getcwd(), 'lib'))
+    test_path = os.path.expanduser(os.path.join(os.getcwd(), 'test'))
 
-#counting lines in lib dir
-for root, dirs, files in os.walk(libPath, topdown=False):
-    for name in files:
-        fileLocation = os.path.join(root, name)
-        with open(fileLocation) as f:
-            fileNamesWithSize[fileLocation]=sum(1 for line in f if line.strip() and not line.startswith('#'))
+    # counting lines in lib dir
+    for root, _, files in os.walk(lib_path, topdown=False):
+        for name in files:
+            file_location = os.path.join(root, name)
+            with open(file_location) as code:
+                total_lines = sum(
+                    1 for line in code
+                    if line.strip() and not line.startswith('#')
+                )
+                file_names_with_size[file_location] = total_lines
+    # counting lines in test dir
+    for root, _, files in os.walk(test_path, topdown=False):
+        for name in files:
+            file_location = os.path.join(root, name)
+            with open(file_location) as code:
+                total_lines = sum(
+                    1 for line in code
+                    if line.strip() and not line.startswith('#')
+                )
+                file_names_with_size[file_location] = total_lines
+    # if the line rule is voilated then value is changed to 1
 
-#counting lines in test dir
-for root, dirs, files in os.walk(testPath, topdown=False):
-    for name in files:
-        fileLocation = os.path.join(root, name)
-        with open(fileLocation) as f:
-            fileNamesWithSize[fileLocation]=sum(1 for line in f if line.strip() and not line.startswith('#'))   
+    is_line_rule_voilated = 0
+    file_count = 0
 
-#if the line rule is voilated then value is changed to 1 
-isLineRuleVoilated = 0
-fileCount = 0
+    for key, value in file_names_with_size.items():
+        if value > 300:
+            is_line_rule_voilated = 1
+            file_count += 1
+            print("{}: {}".format(key, value))
+    if is_line_rule_voilated != 0:
+        msg = "Above " + str(file_count) + " files have more than 300 lines"
+        print(msg)
+        sys.exit(1)
+    else:
+        sys.exit(0)
 
-for key, value in fileNamesWithSize.items():
-    if value > 300:
-        isLineRuleVoilated = 1
-        fileCount += 1 
-        print("{}: {}".format(key, value))
 
-"""Error
-If the number of coded lines is more than 300 in a File
-Than the error message will be displayed
-
-Empty lines are not counted as part of the file size
-"""
-if isLineRuleVoilated != 0:
-    msg = "Above " + str(fileCount) + " files have more than 300 lines"
-    print(msg)
-    sys.exit(1)
-else:
-    sys.exit(0)
+if __name__ == "__main__":
+    main()
