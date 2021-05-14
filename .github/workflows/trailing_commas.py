ba663baf4937c64128e6930a2029c475df64712c
Astha Nayak <nayakastha2911@gmail.com>
2021-05-15 00:11:22 +0530

    logic correction

--- .github/workflows/trailing_commas.py
+++ .github/workflows/trailing_commas.py
@@ -33,15 +33,57 @@ def syntax_matcher(root: str, files: list):
         f = open(file_location, 'w')
         for index in range(0, len(data)):
             if ') {' and not '() {' in data[index]:
-                data[index] = data[index].replace(') {', ',) {')
-                file_count = file_count + 1
+                if 'if' in data[index]:
+                    file_count = file_count + 0
+                elif '}) {' in data[index]:
+                    data[index] = data[index].replace('}) {', ',}) {')
+                    file_count = file_count + 1
+                elif 'for' in data[index]:
+                    file_count = file_count + 0
+                elif 'switch' in data[index]:
+                    file_count = file_count + 0
+                elif 'try' in data[index]:
+                    file_count = file_count + 0
+                elif 'catch' in data[index]:
+                    file_count = file_count + 0
+                elif '!' in data[index]:
+                    file_count = file_count + 0
+                else:
+                    data[index] = data[index].replace(') {', ',) {')
+                    file_count = file_count + 1
 
-            if '))' and not ('toString())' or ')) {' or ')).') in data[index]:
-                data[index] = data[index].replace('))', '),)')
-                file_count = file_count + 1
+            """ if ');' and not '();' in data[index]:
+                if '});' in data[index]:
+                    file_count = file_count + 0
+                elif '\n);' in data[index]:
+                    file_count = file_count + 0
+                else:
+                    data[index] = data[index].replace(');', ',);')
+                    file_count = file_count + 1 """
+
+            if '))' in data[index]:
+                if '),\n)' in data[index]:
+                    file_count = file_count + 0
+                elif 'toString()' in data[index]:
+                    file_count = file_count + 0
+                elif ')) {' in data[index]:
+                    file_count = file_count + 0
+                elif ')).' in data[index]:
+                    file_count = file_count + 0
+                elif 'as' in data[index]:
+                    file_count = file_count + 0
+                elif 'contains' in data[index]:
+                    file_count = file_count + 0
+                else:
+                    data[index] = data[index].replace('))', '),)')
+                    file_count = file_count + 1
 
             if ') async {' and not '() async {' in data[index]:
-                data[index] = data[index].replace(') async {', ',) async {')
+                if '}) async {' in data[index]:
+                    file_count = file_count + 0
+                else:
+                    data[index] = data[index].replace(
+                        ') async {', ',) async {')
 
             f.write(data[index])
 
