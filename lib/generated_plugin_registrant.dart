5fd6e8a40ce0c22e1d64cbbdf0b306c1528f6eb3
rutvik11062000 <rutvik.chandla@gmail.com>
2021-06-04 14:58:20 +0530

    fixed generated plugin error

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -5,7 +5,6 @@
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:connectivity_for_web/connectivity_for_web.dart';
-import 'package:connectivity_plus_web/connectivity_plus_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:uni_links_web/uni_links_web.dart';
 
