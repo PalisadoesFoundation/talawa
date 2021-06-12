f97b2d2594a835d9953e149305eb72ae368c6e92
utkarshshendge <utkarshcpark@gmail.com>
2021-06-12 21:00:21 +0530

    add profile and edit profile screen

---
+++ lib/view_model/edit_profile_view_model.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:talawa/locator.dart';
+import 'package:talawa/models/organization/org_info.dart';
+import 'package:talawa/models/user/user_info.dart';
+import 'package:talawa/services/database_mutation_functions.dart';
+import 'package:talawa/view_model/base_view_model.dart';
+
+class EditProfilePageViewModel extends BaseModel {
+  final user = Hive.box<User>('currentUser');
+  final org = Hive.box<OrgInfo>('currentOrg');
+  TextEditingController firstNameTextController =
+      TextEditingController(text: 'Bruce');
+  TextEditingController lastNameTextController =
+      TextEditingController(text: 'Wayne');
+  FocusNode firstNameFocus = FocusNode();
+  FocusNode lastNameFocus = FocusNode();
+  final databaseService = locator<DataBaseMutationFunctions>();
+
+  initialize() {}
+}
---
+++ lib/view_model/profile_page_view_model.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:talawa/enums/view_state.dart';
+import 'package:talawa/locator.dart';
+import 'package:talawa/models/organization/org_info.dart';
+import 'package:talawa/models/user/user_info.dart';
+import 'package:talawa/services/navigation_service.dart';
+import 'package:talawa/view_model/base_view_model.dart';
+
+class ProfilePageViewModel extends BaseModel {
+  late final Box<User> user;
+  late final Box<dynamic> url;
+  late final Box<OrgInfo> orgization;
+  late OrgInfo currentOrg;
+
+  initialize(OrgInfo org) {
+    setState(ViewState.busy);
+    currentOrg = org;
+    print('page refreshed');
+    user = Hive.box<User>('currentUser');
+    url = Hive.box('url');
+    orgization = Hive.box<OrgInfo>('currentOrg');
+    setState(ViewState.idle);
+  }
+
+  Future<void> logout(BuildContext context) async {
+    print('Logging out');
+    user.clear();
+    url.clear();
+    orgization.clear();
+    locator<NavigationService>()
+        .removeAllAndPush('/selectLang', '/', arguments: '0');
+  }
+}
