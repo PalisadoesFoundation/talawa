805c61ffed8bdaff5c3fcd593b8bfafb932368f9
Aditya Birangal <aditya.birangal@gmail.com>
2021-09-04 18:11:03 +0530

    illustrator images & content

--- lib/views/pre_auth_screens/introduction_slides.dart
+++ lib/views/pre_auth_screens/introduction_slides.dart
@@ -14,19 +14,22 @@ class _IntroductionSlidesState extends State<IntroductionSlides> {
     return IntroductionScreen(
       pages: [
         PageViewModel(
-          title: "Title 1",
-          body: "Body",
-          image: Image.asset("assets/images/launcher_icon.png"),
+          title: "Communities",
+          body:
+              "Discover community or\n Start your own community & watch it grow",
+          image: Image.asset("assets/images/community.png"),
         ),
         PageViewModel(
-          title: "Title 2",
-          body: "Body",
-          image: Image.asset("assets/images/launcher_icon.png"),
+          title: "Event Calendar",
+          body:
+              "Schedule, create and view your upcoming community events on Talawa.\nStart your own or register for existing events.",
+          image: Image.asset("assets/images/event.png"),
         ),
         PageViewModel(
-          title: "Title 3",
-          body: "Body",
-          image: Image.asset("assets/images/launcher_icon.png"),
+          title: "Group Chat",
+          body:
+              "Talk to your own community groups.\nTalawa app makes it simple & secure to use.",
+          image: Image.asset("assets/images/chat.png"),
         ),
       ],
       onDone: () {
