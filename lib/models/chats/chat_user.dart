cb221d95587c889ae5e627a0212b10d97d84b454
Michaela Wright <75323745+Mich-moon@users.noreply.github.com>
2021-10-21 23:34:09 -0500

    added comments

--- lib/models/chats/chat_user.dart
+++ lib/models/chats/chat_user.dart
@@ -1,14 +1,20 @@
 import 'package:json_annotation/json_annotation.dart';
 part 'chat_user.g.dart';
 
+/// A ChatUser object engages in chat messaging and can be encoded into a string.
 @JsonSerializable()
 class ChatUser {
+  /// Returns a new instance of ChatUser class
   ChatUser({this.name, this.id, this.image});
 
+  /// Connects the generated [_$ChatUserFromJson] function to the `fromJson` factory.
   factory ChatUser.fromJson(Map<String, dynamic> json) =>
       _$ChatUserFromJson(json);
+
+  /// Connects the generated [_$ChatUserToJson] function to the `toJson` method. 
   Map<String, dynamic> toJson() => _$ChatUserToJson(this);
 
+  /// Nullable instance properties
   String? name;
   String? id;
   String? image;
