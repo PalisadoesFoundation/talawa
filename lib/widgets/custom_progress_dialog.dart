d8e6f3b8582f1e80f6cbf416166701b7e0474abf
Simran1604 <simran251601@gmail.com>
2021-12-10 10:04:10 +0530

    removed foundation dependency

--- lib/widgets/custom_progress_dialog.dart
+++ lib/widgets/custom_progress_dialog.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:talawa/services/size_config.dart';
 import 'package:talawa/utils/app_localization.dart';
