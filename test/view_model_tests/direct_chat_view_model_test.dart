d6968cedd1e845f8587b51bec8996191f0180533
srirampatibanda <patibandasriram2k02@gmail.com>
2022-01-11 20:01:47 +0530

    fix: refactor direct chat view model test

--- test/view_model_tests/direct_chat_view_model_test.dart
+++ test/view_model_tests/direct_chat_view_model_test.dart
@@ -3,21 +3,23 @@ import 'package:talawa/demo_server_data/direct_chat_demo_data.dart';
 import 'package:talawa/models/chats/chat_list_tile_data_model.dart';
 import 'package:talawa/view_model/after_auth_view_models/chat_view_models/direct_chat_view_model.dart';
 
+DirectChatViewModel _getModel() => DirectChatViewModel();
 void main() {
   group('DirectChatViewModelTest -', () {
-    final model = DirectChatViewModel();
-
     test('if incrementIterator is called and chats is null', () {
+      final model = _getModel();
       expect(model.incrementIterator(), false);
     });
 
     test('if reInitialiseIterator is called when chats is null', () {
+      final model = _getModel();
       model.reInitialiseIterator();
       expect(model.reInitialiseIterator, returnsNormally);
     });
 
     group('initialise - ', () {
       test('If initialise is called', () {
+        final model = _getModel();
         expect(model.chats, isNull);
         model.initialise();
         expect(model.chats, isNotNull);
@@ -29,11 +31,13 @@ void main() {
       });
 
       test('if reInitialiseIterator is called when chats is not null', () {
+        final model = _getModel();
         model.initialise();
         expect(model.chatIterator, model.chats!.head);
       });
 
       test('if incrementIterator is called and chats is not null', () {
+        final model = _getModel();
         model.initialise();
         final node = model.chatIterator!.next;
         expect(model.incrementIterator(), true);
