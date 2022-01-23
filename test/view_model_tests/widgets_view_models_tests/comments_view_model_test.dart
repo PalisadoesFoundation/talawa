752f6d1b0374df8f17c4ce7af7ee0abaacfc59ea
Simran1604 <simran251601@gmail.com>
2022-01-23 11:48:26 +0530

    test for comments_view_model

--- test/view_model_tests/widgets_view_models_tests/comments_view_model_test.dart
+++ test/view_model_tests/widgets_view_models_tests/comments_view_model_test.dart
@@ -1,70 +1,65 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:talawa/models/comment/comment_model.dart';
 import 'package:talawa/services/user_config.dart';
-import 'package:talawa/view_model/base_view_model.dart';
+import 'package:talawa/view_model/widgets_view_models/comments_view_model.dart';
 
-import '../helpers/test_helpers.dart';
-import '../helpers/test_locator.dart';
-
-class MockCommentsViewModel extends BaseModel {
-  late String _postID;
-  late List<Comment> _commentlist;
-  late UserConfig _userConfig;
-
-  // Getters
-  List<Comment> get commentList => _commentlist;
-
-  //initialises the variables
+class MockCommentsViewModel extends Mock implements CommentsViewModel {
+  late List<Comment> commentList;
+  @override
   void initialise(String postID) {
-    _userConfig = getAndRegisterUserConfig();
-    _commentlist = [];
-    _postID = postID;
-    notifyListeners();
+    commentList = [];
   }
 
-  //gets the already created and stored comments
-  List getComments() {
+  @override
+  Future<List<Comment>> getComments() async {
     return commentList;
   }
 
-  //creates new comments
+  @override
   Future createComment(String msg) async {
+    final Comment comment = Comment(
+        text: msg,
+        createdAt: DateTime.now().toString(),
+        creator: UserConfig().currentUser,
+        post: "post",
+        likeCount: "0");
     addCommentLocally(msg);
+    return comment;
   }
 
-  //adds comments locally 
+  @override
   void addCommentLocally(String msg) {
-    final _creator = _userConfig.currentUser;
-    final Comment _localComment = Comment(
-        text: msg, createdAt: DateTime.now().toString(), creator: _creator);
-    _commentlist.insert(0, _localComment);
-    notifyListeners();
+    final Comment comment = Comment(
+        text: msg,
+        createdAt: DateTime.now().toString(),
+        creator: UserConfig().currentUser,
+        post: "post",
+        likeCount: "0");
+    commentList.add(comment);
   }
 }
 
 void main() {
+  final MockCommentsViewModel model = MockCommentsViewModel();
   group("check for comments view model", () {
-    final model = MockCommentsViewModel();
     model.initialise("TEST ID");
 
     test("Check for no comments created.", () {
-      final comments = model.getComments();
+      final hasComments = model.commentList.isEmpty;
 
       //checks if no comments ae created
-      expect(model.getComments().isEmpty, true);
+      expect(hasComments, true);
     });
 
     test("check if comment is created", () {
       model.createComment("test message");
 
       //checks if comments are created
-      expect(model.getComments().isNotEmpty, true);
-      
+      expect(model.commentList.isNotEmpty, true);
+
       //checks if created comment is same as input
-      expect(model._commentlist.first.text, "test message");
+      expect(model.commentList.first.text, "test message");
     });
   });
 }
-
-
-
