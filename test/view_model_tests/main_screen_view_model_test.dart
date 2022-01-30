ebca690109ec626a0125a53a954d301104b0ca80
Patrick Witter <pwitterop123@gmail.com>
2022-01-30 08:24:57 -0500

    Resolved failing tests

--- test/view_model_tests/main_screen_view_model_test.dart
+++ test/view_model_tests/main_screen_view_model_test.dart
@@ -15,26 +15,25 @@ class MockCallBack extends Mock {
 
 class MockBuildContext extends Mock implements BuildContext {}
 
-class MockTutorialCoach extends Mock implements TutorialCoachMark {}
-
 MainScreenViewModel getModel() {
   final model = MainScreenViewModel();
+  model.context = MockBuildContext();
   return model;
 }
 
-void main() {
-  void verifyInteraction(dynamic x, {required String mockName}) {
-    // Ensures that navigation service was called
-    try {
-      verifyZeroInteractions(x);
-      //If 0 interactions passes that means navigation service with app tour dialog was not called hence test fails
-      throw Exception("Expected interaction but found 0 with $mockName");
-    } on TestFailure {
-      //If test fails then 1 or more interactions with navigation service hence test passes
-      expect(true, true);
-    }
+void verifyInteraction(dynamic x, {required String mockName}) {
+  // Ensures that navigation service was called
+  try {
+    verifyZeroInteractions(x);
+    //If 0 interactions passes that means mock was not called hence test fails
+    throw Exception("Expected interaction but found 0 with $mockName");
+  } on TestFailure {
+    //If test fails then 1 or more interactions with navigation service hence test passes
+    expect(true, true);
   }
+}
 
+void main() {
   group("MainScreen ViewModel Tests - ", () {
     setUp(() {
       locator.registerFactory(() => SizeConfig());
@@ -45,27 +44,28 @@ void main() {
       locator.unregister<SizeConfig>();
     });
 
-    MainScreenViewModel model = getModel();
-    model.context = MockBuildContext();
+    final MainScreenViewModel mainTestModel = getModel();
+
     final TargetFocus testTarget =
-        TargetFocus(identify: "TestTarget", keyTarget: model.keyBNChat);
+        TargetFocus(identify: "TestTarget", keyTarget: mainTestModel.keyBNChat);
+
     test("When initialized current index should be 0", () {
-      expect(model.currentIndex, 0);
+      expect(mainTestModel.currentIndex, 0);
     });
     group("onTabTapped -", () {
       test(
           "When an index is passed that, current index should equal that index",
           () {
-        model.onTabTapped(4);
-        expect(model.currentIndex, 4);
+        mainTestModel.onTabTapped(4);
+        expect(mainTestModel.currentIndex, 4);
       });
 
       test("When called function should notify listeners of tab change", () {
         final mockcallback = MockCallBack();
 
-        model.addListener(mockcallback);
+        mainTestModel.addListener(mockcallback);
 
-        model.onTabTapped(0);
+        mainTestModel.onTabTapped(0);
         verify(mockcallback()).called(1);
       });
     });
@@ -75,10 +75,13 @@ void main() {
       setUp(() => registerServices());
       tearDown(() => unregisterServices());
 
-      void runIntialize({required bool fSignUp, int mainIndex = 1}) {
-        model = getModel();
-        model.initialise(
-          context,
+      void runIntialize(
+          {required bool fSignUp,
+          int mainIndex = 1,
+          required MainScreenViewModel model,
+          BuildContext? pcontext}) {
+        mainTestModel.initialise(
+          pcontext ?? context,
           fromSignUp: fSignUp,
           mainScreenIndex: mainIndex,
         );
@@ -90,26 +93,30 @@ void main() {
         const bool fSignup = true;
         const int mainIndex = 1;
 
-        runIntialize(fSignUp: fSignup, mainIndex: mainIndex);
+        runIntialize(
+            fSignUp: fSignup, mainIndex: mainIndex, model: mainTestModel);
 
-        expect(model.showAppTour, fSignup);
-        expect(model.currentIndex, mainIndex);
+        expect(mainTestModel.showAppTour, fSignup);
+        expect(mainTestModel.currentIndex, mainIndex);
       });
 
       test(
-          "When fromSignUp is false tourComplete should equal true and tourSkipped false",
+          "When fromSignUp is false tourComplete should equal true, tourSkipped and showApptour false",
           () {
-        runIntialize(fSignUp: false);
-        expect(model.tourComplete, true);
-        expect(model.tourSkipped, false);
+        runIntialize(fSignUp: false, model: mainTestModel);
+        expect(mainTestModel.tourComplete, true);
+        expect(mainTestModel.tourSkipped, false);
+        expect(mainTestModel.showAppTour, false);
       });
 
       test("When fromSignUp is false, App Tour dialog should not be displayed",
           () async {
         final mocknav = getAndRegisterNavigationService();
-        runIntialize(fSignUp: false);
-        //Waits for any delay in navigation being called
-        await Future.delayed(const Duration(seconds: 2));
+
+        // runIntialize(fSignUp: true, model: localmodel);
+        mainTestModel.initialise(MockBuildContext(),
+            fromSignUp: false, mainScreenIndex: 0);
+        print("AppTour ${mainTestModel.showAppTour}");
 
         // Ensures that navigation service was not called
         verifyZeroInteractions(mocknav);
@@ -118,72 +125,79 @@ void main() {
       test("When fromSignUp is true, App Tour dialog should be displayed",
           () async {
         final mocknav = getAndRegisterNavigationService();
-        runIntialize(fSignUp: true);
+        // final localmodel = getModel();
+        // runIntialize(fSignUp: true, model: localmodel);
+
         //Waits for any delay in navigation being called
+        mainTestModel.initialise(MockBuildContext(),
+            fromSignUp: true, mainScreenIndex: 0);
+
         await Future.delayed(const Duration(seconds: 2));
-        //Ensures that naviagation service was called
         verifyInteraction(mocknav, mockName: "NavigationService");
-      });
+      }
+          //Ensures that naviagation service was called
+          );
     });
 
     group("showTutorial", () {
       test("When called tutorial coach should be assigned a value", () {
-        model.showTutorial(onClickTarget: (TargetFocus x) {}, onFinish: () {});
-        expect(model.tutorialCoachMark, isNotNull);
+        mainTestModel.showTutorial(
+            onClickTarget: (TargetFocus x) {}, onFinish: () {});
+        expect(mainTestModel.tutorialCoachMark, isNotNull);
       });
     });
 
     group("tourHomeTargets", () {
       test("target list should be cleared before adding new targets", () {
         //Adding a testtarget before method is called
-        model.targets.add(testTarget);
-        // model.context = MockBuildContext();
-        model.tourHomeTargets();
+        mainTestModel.targets.add(testTarget);
+        // mainTestModel.context = MockBuildContext();
+        mainTestModel.tourHomeTargets();
         // targets list should not contain testtarget
-        expect(model.targets.contains(testTarget), false);
+        expect(mainTestModel.targets.contains(testTarget), false);
       });
     });
 
     group("tourEventTargets", () {
       test("target list should be cleared before adding new targets", () {
         //Adding a target before method is called
-        model.targets.add(testTarget);
+        mainTestModel.targets.add(testTarget);
 
-        model.tourEventTargets();
+        mainTestModel.tourEventTargets();
         // targets list should not contain target
-        expect(model.targets.contains(testTarget), false);
+        expect(mainTestModel.targets.contains(testTarget), false);
       });
     });
 
     group("tourAddPost", () {
       test("target list should be cleared before adding new targets", () {
         //Adding a target before method is called
-        model.targets.add(testTarget);
+        mainTestModel.targets.add(testTarget);
 
-        model.tourAddPost();
+        mainTestModel.tourAddPost();
         // targets list should not contain target
-        expect(model.targets.contains(testTarget), false);
+        expect(mainTestModel.targets.contains(testTarget), false);
       });
     });
     group("tourChat", () {
       test("target list should be cleared before adding new targets", () {
         //Adding a target before method is called
-        model.targets.add(testTarget);
+        mainTestModel.targets.add(testTarget);
 
-        model.tourChat();
+        mainTestModel.tourChat();
         // targets list should not contain target
-        expect(model.targets.contains(testTarget), false);
+        expect(mainTestModel.targets.contains(testTarget), false);
       });
     });
 
     group("tourProfile", () {
       test("target list should be cleared before adding new targets", () {
         //Adding a target before method is called
-        model.targets.add(testTarget);
+        mainTestModel.targets.add(testTarget);
 
-        model.tourProfile();
+        mainTestModel.tourProfile();
         // targets list should not contain target
-        expect(model.targets.contains(testTarget), false);
+        expect(mainTestModel.targets.contains(testTarget), false);
       });
     });
   });
