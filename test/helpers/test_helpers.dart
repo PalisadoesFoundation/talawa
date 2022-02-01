69f38fd776de87aba10ba42f4c9a277937c83d24
ahluwaliatikant <atikant.ahluwalia@gmail.com>
2022-02-01 11:55:35 +0530

    linting

--- test/helpers/test_helpers.dart
+++ test/helpers/test_helpers.dart
@@ -18,7 +18,6 @@ import 'package:talawa/services/post_service.dart';
 import 'package:talawa/services/size_config.dart';
 import 'package:talawa/services/third_party_service/multi_media_pick_service.dart';
 import 'package:talawa/services/user_config.dart';
-import 'package:talawa/utils/queries.dart';
 import 'package:talawa/view_model/after_auth_view_models/add_post_view_models/add_post_view_model.dart';
 import 'package:talawa/view_model/after_auth_view_models/chat_view_models/direct_chat_view_model.dart';
 import 'package:talawa/view_model/after_auth_view_models/event_view_models/event_info_view_model.dart';
