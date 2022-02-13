efdb8354f503d9c2a132313b42feaedefe26f438
Atikant Ahluwalia <52076751+ahluwaliatikant@users.noreply.github.com>
2022-02-13 12:04:02 +0530

    Tests added for like_button_view_model (#1209)
    * tests for login_button_view_model
    
    * format
    
    * small change

--- test/view_model_tests/widgets_view_model_test/like_button_view_model_test.dart
+++ test/view_model_tests/widgets_view_model_test/like_button_view_model_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:talawa/models/post/post_model.dart';
 import 'package:talawa/view_model/widgets_view_models/like_button_view_model.dart';
 
@@ -7,30 +8,84 @@ import '../../helpers/test_locator.dart';
 
 void main() {
   registerServices();
-  final model = LikeButtonViewModel();
+
   final List<LikedBy> _likedBy = [
     LikedBy(sId: 'Test user 1'),
     LikedBy(sId: 'Test user 2')
   ];
-  //model.initialize(_likedBy, 'Test post_id');
-  test("Testing setIsLiked", () {
-    model.initialize(_likedBy, 'Test post_id');
-    model.setIsLiked();
-    expect(model.isLiked, true);
-  });
-  test(
-      'Testing checkAndSetTheIsLiked function when user is not present in likedby',
-      () {
-    model.initialize(_likedBy, 'Test post_id');
-    model.checkAndSetTheIsLiked();
-    expect(model.isLiked, false);
-  });
 
-  test('Testing checkAndSetTheIsLiked function when user is present in likedby',
-      () {
-    _likedBy.add(LikedBy(sId: userConfig.currentUser.id));
-    model.initialize(_likedBy, 'Test post_id');
-    model.checkAndSetTheIsLiked();
-    expect(model.isLiked, true);
+  final post = Post(
+    sId: "test_post_id",
+    creator: userConfig.currentUser,
+    likedBy: _likedBy,
+  );
+
+  group("Tests for login_button_view_model", () {
+    test("Testing setIsLiked", () {
+      final model = LikeButtonViewModel();
+      model.initialize(_likedBy, 'Test post_id');
+      model.setIsLiked();
+      expect(model.isLiked, true);
+    });
+    test(
+        'Testing checkAndSetTheIsLiked function when user is not present in likedby',
+        () {
+      final model = LikeButtonViewModel();
+      model.initialize(_likedBy, 'Test post_id');
+      model.checkAndSetTheIsLiked();
+      expect(model.isLiked, false);
+    });
+
+    test(
+        'Testing checkAndSetTheIsLiked function when user is present in likedby',
+        () {
+      final model = LikeButtonViewModel();
+      _likedBy.add(LikedBy(sId: userConfig.currentUser.id));
+      model.initialize(_likedBy, 'Test post_id');
+      model.checkAndSetTheIsLiked();
+      expect(model.isLiked, true);
+      _likedBy
+          .removeWhere((element) => element.sId == userConfig.currentUser.id);
+    });
+
+    test("testing toggleIsLiked", () {
+      final model = LikeButtonViewModel();
+      model.initialize(_likedBy, post.sId);
+      model.setIsLiked(val: false);
+      expect(model.isLiked, false);
+      final LikedBy likedBy = LikedBy(
+        sId: userConfig.currentUser.id,
+      );
+      when(postService.addLike(post.sId)).thenAnswer((realInvocation) async {
+        model.likedBy.add(likedBy);
+      });
+      model.toggleIsLiked();
+      expect(model.likesCount, 3);
+      model.setIsLiked(val: true);
+      when(postService.removeLike(post.sId)).thenAnswer((realInvocation) async {
+        model.likedBy
+            .removeWhere((element) => element.sId == userConfig.currentUser.id);
+      });
+      model.toggleIsLiked();
+      expect(model.likesCount, 2);
+      expect(model.likedBy.contains(likedBy), false);
+    });
+
+    test("Testing updatePost", () {
+      final model = LikeButtonViewModel();
+      model.initialize(_likedBy, post.sId);
+      final Post newPost = Post(
+          sId: "new_post_id", creator: userConfig.currentUser, likedBy: []);
+      // with different post
+      model.updatePost(newPost);
+      expect(model.likesCount, 2);
+
+      final LikedBy likedBy3 = LikedBy(sId: "Test user 3");
+      post.likedBy!.add(likedBy3);
+      // with same post
+      model.updatePost(post);
+      expect(model.likesCount, 3);
+      expect(model.likedBy.contains(likedBy3), true);
+    });
   });
 }
