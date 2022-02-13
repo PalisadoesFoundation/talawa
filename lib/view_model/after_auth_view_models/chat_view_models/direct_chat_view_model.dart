2eaccb9e402df42bbf3492f8dee104352440c4a2
Sriram Patibanda <patibandasriram2k02@gmail.com>
2022-02-13 17:22:58 +0530

    rfac: Refactor the direct chat feature to fetch messages

--- lib/view_model/after_auth_view_models/chat_view_models/direct_chat_view_model.dart
+++ lib/view_model/after_auth_view_models/chat_view_models/direct_chat_view_model.dart
@@ -1,42 +1,22 @@
-import 'package:talawa/demo_server_data/direct_chat_demo_data.dart';
-import 'package:talawa/models/chats/chat_list_tile_data_model.dart';
-import 'package:talawa/view_model/after_auth_view_models/chat_view_models/lru_cache.dart';
+import 'dart:async';
+
+import 'package:talawa/enums/enums.dart';
+import 'package:talawa/locator.dart';
+import 'package:talawa/services/chat_service.dart';
 import 'package:talawa/view_model/base_view_model.dart';
 
 class DirectChatViewModel extends BaseModel {
-  LRUChatListCache? _chats;
-  ChatListNode? _chatIterator;
+  final ChatService _chatService = locator<ChatService>();
+  late StreamSubscription _chatSubscription;
 
-  //Getters
-  LRUChatListCache? get chats => _chats;
-  ChatListNode? get chatIterator => _chatIterator;
-
-  void initialise() {
-    _chats = LRUChatListCache();
-    const List<Map<String, dynamic>> _chatListTileJson = directChatDemoData;
-
-    _chatListTileJson.forEach((chat) {
-      final _chatListTile = ChatListTileDataModel.fromJson(chat);
-      final _chatNode =
-          ChatListNode(chat: _chatListTile, key: _chatListTile.sender!.id);
-      if (_chats != null) _chats!.addNewIncomingChat(_chatNode);
-    });
-    reInitialiseIterator();
-    notifyListeners();
+  Future<void> initialise() async {
+    setState(ViewState.busy);
+    await _chatService.getDirectChatsByUserId();
+    setState(ViewState.idle);
   }
 
-  /* To use with pre-built Animatedlist (takes "List" as an input),
-  "ChatIterator imitates the behaviour of serial iteration of Arraylist"*/
-  void reInitialiseIterator() {
-    if (_chats != null) _chatIterator = _chats!.head;
+  Future<void> getDirectChatList() async {
+    await _chatService.getDirectChatsByUserId();
   }
-
-  // Imitates "index++"" for the LRUCacheList
-  bool incrementIterator() {
-    if (_chats != null && _chatIterator!.next != _chats!.tail) {
-      _chatIterator = _chatIterator!.next;
-      return true;
-    }
-    return false;
-  }
+  
 }
--- lib/views/after_auth_screens/chat/direct_chats.dart
+++ lib/views/after_auth_screens/chat/direct_chats.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:talawa/constants/routing_constants.dart';
-import 'package:talawa/models/chats/chat_list_tile_data_model.dart';
 import 'package:talawa/view_model/after_auth_view_models/chat_view_models/direct_chat_view_model.dart';
 import 'package:talawa/views/base_view.dart';
 
@@ -11,68 +9,10 @@ class DirectChats extends StatelessWidget {
   Widget build(BuildContext context) {
     return BaseView<DirectChatViewModel>(
       onModelReady: (model) => model.initialise(),
-      builder: (context, model, child) => AnimatedList(
-        key: const PageStorageKey("Save-Direct-Chat-Page-Position"),
-        initialItemCount: model.chats!.length,
-        itemBuilder: (context, index, animation) {
-          final bool _isValidIncrement = model.incrementIterator();
-
-          if (_isValidIncrement) {
-            return ChatTile(
-                chat: model.chatIterator!.chat!, animation: animation);
-          }
-
-          return Container();
-        },
-      ),
-    );
-  }
-}
-
-class ChatTile extends StatelessWidget {
-  const ChatTile({
-    Key? key,
-    required this.chat,
-    required this.animation,
-  }) : super(key: key);
-
-  final ChatListTileDataModel chat;
-  final Animation<double> animation;
-  @override
-  Widget build(BuildContext context) {
-    return SizeTransition(
-      axis: Axis.vertical,
-      sizeFactor: animation,
-      child: ListTile(
-        onTap: () => Navigator.pushNamed(context, Routes.chatMessageScreen,
-            arguments: chat),
-        leading: const CircleAvatar(
-          radius: 25,
-        ),
-        title: Text(chat.sender!.name!),
-        subtitle: Text(chat.lastMessage!.text!),
-        trailing: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              "12:00 PM",
-              style:
-                  Theme.of(context).textTheme.caption!.copyWith(fontSize: 10.0),
-            ),
-            chat.unreadCount != null && chat.unreadCount! > 0
-                ? CircleAvatar(
-                    backgroundColor: Theme.of(context).colorScheme.secondary,
-                    radius: 10,
-                    child: Text(
-                      chat.unreadCount!.toString(),
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.white),
-                    ),
-                  )
-                : Container(),
-          ],
-        ),
-      ),
+      builder: (context, model, child) {
+        return ElevatedButton(
+            onPressed: () => model.getDirectChatList(), child: Text('Click'));
+      },
     );
   }
 }
