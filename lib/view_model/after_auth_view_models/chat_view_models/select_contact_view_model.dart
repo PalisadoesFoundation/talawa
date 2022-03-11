d6f87c4de46d7df493b7e56e4ee5cff484a13d28
NISHNATA DEBNATH <53905682+nishnatadebnath@users.noreply.github.com>
2022-03-12 01:42:19 +0530

    Added select contact pages. (#1260)
    * Testing custom drawer view model
    
    * Linting code
    
    * Undoing last commit
    
    * Linting
    
    * Linting

---
+++ lib/view_model/after_auth_view_models/chat_view_models/select_contact_view_model.dart
@@ -0,0 +1,22 @@
+import 'package:talawa/locator.dart';
+import 'package:talawa/models/user/user_info.dart';
+import 'package:talawa/services/org_service.dart';
+import 'package:talawa/view_model/base_view_model.dart';
+
+class SelectContactViewModel extends BaseModel {
+  late OrganizationService _organizationService;
+  late List<User> orgMembersList = [];
+
+  void initialise() {
+    _organizationService = locator<OrganizationService>();
+  }
+
+  getCurrentOrgUsersList() async {
+    if (orgMembersList.isEmpty) {
+      orgMembersList = await _organizationService
+          .getOrgMembersList(userConfig.currentOrg.id!);
+    }
+
+    //return orgMembersList;
+  }
+}
