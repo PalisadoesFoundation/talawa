f0e2994ea64ec16e1daa68a16cc079ee98d3f8e4
noman2002 <nomn2002@gmail.com>
2022-06-16 12:51:12 +0530

    Added icons for notification and request

--- lib/views/after_auth_screens/feed/organization_feed.dart
+++ lib/views/after_auth_screens/feed/organization_feed.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:talawa/services/size_config.dart';
 import 'package:talawa/view_model/after_auth_view_models/feed_view_models/organization_feed_view_model.dart';
 import 'package:talawa/view_model/main_screen_view_model.dart';
 import 'package:talawa/views/base_view.dart';
@@ -39,6 +40,28 @@ class OrganizationFeed extends StatelessWidget {
               onPressed: () =>
                   MainScreenViewModel.scaffoldKey.currentState!.openDrawer(),
             ),
+              actions: [
+              Padding(
+                  padding: EdgeInsets.only(
+                    right: SizeConfig.screenWidth! * 0.027,
+                  ),
+                  child: Row(
+                    children: [
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.person_add_alt,
+                          ),
+                          color: Theme.of(context).iconTheme.color),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.notifications_none_outlined,
+                          ),
+                          color: Theme.of(context).iconTheme.color),
+                    ],
+                  )),
+            ],
           ),
           body: model.isBusy
               ? const CircularProgressIndicator()
