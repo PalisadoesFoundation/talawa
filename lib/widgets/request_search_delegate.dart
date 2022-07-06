d0a53e035262438b87d803165a9a8d6c1e8130df
noman2002 <nomn2002@gmail.com>
2022-07-06 15:03:25 +0530

    added search delegate for requests

---
+++ lib/widgets/request_search_delegate.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:talawa/models/request/request_model.dart';
+import 'package:talawa/services/size_config.dart';
+import 'package:talawa/view_model/after_auth_view_models/request_view_model/request_view_model.dart';
+import 'package:talawa/widgets/request_card.dart';
+
+class RequestSearch extends SearchDelegate<Request> {
+  RequestSearch({
+    required this.requestList,
+    required this.requestViewModel,
+  });
+  RequestViewModel requestViewModel;
+
+  final List<Request> requestList;
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () {
+          query.isNotEmpty ? query = '' : close(context, requestList.first);
+        },
+        icon: const Icon(Icons.clear),
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, requestList.first);
+      },
+      icon: const Icon(Icons.arrow_back),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestions = requestList.where((request) {
+      final requestLowerCase = request.name!.toLowerCase();
+      final queryLowerCase = query.toLowerCase();
+      return requestLowerCase.startsWith(queryLowerCase);
+    }).toList();
+    return buildSuggestionsSucess(suggestions);
+  }
+
+  Widget buildSuggestionsSucess(List<Request> suggestions) {
+    return SingleChildScrollView(
+      child: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: SizeConfig.screenWidth! * 0.027,
+          crossAxisSpacing: SizeConfig.screenWidth! * 0.027,
+        ),
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        itemCount: suggestions.length,
+        itemBuilder: (BuildContext context, int index) {
+          // final highlightedText =
+          //     suggestions[index].name!.substring(0, query.length);
+          // final normalText = suggestions[index].name!.substring(query.length);
+          return RequestCard(
+            request: suggestions[index],
+          );
+        },
+      ),
+    );
+  }
+}
