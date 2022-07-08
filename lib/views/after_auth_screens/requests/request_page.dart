027996663b4b25a2f36ca09829c67f9b3d23e4ca
noman2002 <nomn2002@gmail.com>
2022-07-08 14:46:05 +0530

    added request page

--- lib/views/after_auth_screens/requests/request_page.dart
+++ lib/views/after_auth_screens/requests/request_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:talawa/services/size_config.dart';
+import 'package:talawa/view_model/after_auth_view_models/request_view_model/request_view_model.dart';
+import 'package:talawa/views/base_view.dart';
 import 'package:talawa/widgets/request_card.dart';
+import 'package:talawa/widgets/request_search_delegate.dart';
 
 class RequestPage extends StatelessWidget {
   const RequestPage({
@@ -10,84 +13,89 @@ class RequestPage extends StatelessWidget {
   // final RequestModel? model;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).primaryColor,
-        elevation: 0.0,
-        iconTheme: IconThemeData(
-          color: Theme.of(context).iconTheme.color,
-        ),
-        title: Text(
-          "Requests",
-          style: Theme.of(context).textTheme.headline6!.copyWith(
-                fontWeight: FontWeight.w600,
-                fontSize: 20,
-              ),
-        ),
-        centerTitle: true,
-        actions: [
-          Padding(
-            padding: EdgeInsets.only(
-              right: SizeConfig.screenWidth! * 0.027,
-            ),
-            child: IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.search, size: 20),
+    return BaseView<RequestViewModel>(
+      onModelReady: (model) => model.initialise(),
+      builder: (context, model, child) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).primaryColor,
+            elevation: 0.0,
+            iconTheme: IconThemeData(
               color: Theme.of(context).iconTheme.color,
             ),
-          )
-        ],
-      ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: SizeConfig.screenWidth! * 0.027,
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: SizeConfig.screenHeight! * 0.0215,
-              ),
-              Text(
-                "Pending Requests: 20",
-                style: Theme.of(context).textTheme.headline5!.copyWith(
-                      fontSize: 20,
-                      color: Theme.of(context).colorScheme.secondary,
-                    ),
-              ),
-              SizedBox(
-                height: SizeConfig.screenHeight! * 0.0215,
-              ),
-              GridView.count(
-                mainAxisSpacing: SizeConfig.screenWidth! * 0.027,
-                crossAxisSpacing: SizeConfig.screenWidth! * 0.027,
-                shrinkWrap: true,
-                crossAxisCount: 2,
-                padding: EdgeInsets.zero,
+            title: Text(
+              "Requests",
+              style: Theme.of(context).textTheme.headline6!.copyWith(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20,
+                  ),
+            ),
+            centerTitle: true,
+            actions: [
+              Padding(
+                padding: EdgeInsets.only(
+                  right: SizeConfig.screenWidth! * 0.027,
+                ),
+                child: IconButton(
+                  onPressed: () {
+                    showSearch(
+                      context: context,
+                      delegate: RequestSearch(
+                        requestList: model.requests,
+                        requestViewModel: model,
+                      ),
+                    );
+                  },
+                  icon: const Icon(Icons.search, size: 20),
+                  color: Theme.of(context).iconTheme.color,
+                ),
+              )
+            ],
+          ),
+          body: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: SizeConfig.screenWidth! * 0.027,
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const RequestCard(
-                    name: "First",
-                    description: "First Description",
+                  SizedBox(
+                    height: SizeConfig.screenHeight! * 0.0215,
                   ),
-                  const RequestCard(
-                    name: "Second",
-                    description: "Second Description",
+                  Text(
+                    "Pending Requests: ${model.requests.length}",
+                    style: Theme.of(context).textTheme.headline5!.copyWith(
+                          fontSize: 20,
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
                   ),
-                  const RequestCard(
-                    name: "Third",
-                    description: "Third Description",
+                  SizedBox(
+                    height: SizeConfig.screenHeight! * 0.0215,
                   ),
-                  const RequestCard(
-                    name: "Fourth",
-                    description: "Fourth Description",
+                  GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: SizeConfig.screenWidth! * 0.027,
+                      crossAxisSpacing: SizeConfig.screenWidth! * 0.027,
+                    ),
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: model.requests.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return RequestCard(
+                        request: model.requests[index],
+                        //   name: "Noman",
+                        //   description: "Open-source Contributor",
+                      );
+                    },
                   ),
                 ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
