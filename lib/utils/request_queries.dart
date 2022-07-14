b85fff9b257b59472072db3455fbeefffbca3e14
noman2002 <nomn2002@gmail.com>
2022-07-14 23:39:12 +0530

    fixed getrequest queries

--- lib/utils/request_queries.dart
+++ lib/utils/request_queries.dart
@@ -1,42 +1,40 @@
 class RequestQueries {
-
   String getRequests(String orgId) {
     return """
- query{
-  Organization(id: "$orgId"){
-    organizations(id: "$orgId"){
-      _id
-      membershipRequests{
-        _id
-        user{
+      query{
+        organizations(id: "$orgId"){
           _id
-          firstName
-          lastName
-          image
+          membershipRequests{
+            _id
+            user{
+              _id
+              firstName
+              lastName
+              image
+            }
+          }
         }
       }
-    }
-  }
-  }""";
+    """;
   }
 
   String acceptRequest(String requestId) {
     return """
-  mutation AcceptMembershipRequest(id: $requestId){
-    acceptMembershipRequest(id: $requestId){
-      _id
+      mutation {
+        acceptMembershipRequest(id: "$requestId"){
+          _id
+        }
       }
-   }
-  """;
+   """;
   }
 
-   String rejectRequest(String requestId) {
+  String rejectRequest(String requestId) {
     return """
-  mutation RejectMembershipRequest(\$id: $requestId){
-    rejectMembershipRequest(id: $requestId){
-      _id
+      mutation {
+        rejectMembershipRequest(id: $requestId){
+          _id
+        }
       }
-   }
-  """;
+    """;
   }
 }
