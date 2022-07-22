1f02f03d3918e2c77d54f11f5a299251a0f52eb3
noman2002 <nomn2002@gmail.com>
2022-07-23 00:16:44 +0530

    fixed request view model

--- lib/view_model/after_auth_view_models/request_view_model/request_view_model.dart
+++ lib/view_model/after_auth_view_models/request_view_model/request_view_model.dart
@@ -5,6 +5,7 @@ import 'package:talawa/locator.dart';
 import 'package:talawa/models/request/request_model.dart';
 import 'package:talawa/services/request_service.dart';
 import 'package:talawa/view_model/base_view_model.dart';
+import 'package:talawa/widgets/custom_alert_dialog.dart';
 
 class RequestViewModel extends BaseModel {
   final _requestService = locator<RequestService>();
@@ -14,11 +15,10 @@ class RequestViewModel extends BaseModel {
   List<Request> _requests = [];
   final Set<String> _uniqueRequestIds = {};
   late StreamSubscription _currentOrganizationStreamSubscription;
-  late final List<Request> _bufferRequests;
+  // late final List<Request> _bufferRequests;
   List<Request> get requests => _requests;
   RequestService get requestService => _requestService;
   String get emptyListMessage => _emptyListMessage;
-
 
   Future<void> fetchNewRequests() async {
     setState(ViewState.busy);
@@ -27,7 +27,7 @@ class RequestViewModel extends BaseModel {
     setState(ViewState.idle);
   }
 
-  Future<void> refreshEvents() async {
+  Future<void> refreshRequests() async {
     setState(ViewState.busy);
     _requests.clear();
     _uniqueRequestIds.clear();
@@ -38,52 +38,39 @@ class RequestViewModel extends BaseModel {
   Future<void> initialise() async {
     setState(ViewState.busy);
     _currentOrganizationStreamSubscription = userConfig.currentOrgInfoStream
-        .listen((updatedOrganization) => refreshEvents());
+        .listen((updatedOrganization) => refreshRequests());
     await _requestService.getRequests();
+    _requestStreamSubscription = _requestService.requestStream.listen(
+      (newRequest) => checkIfExistsAndAddNewRequest(newRequest),
+    );
 
-   
-    _bufferRequests = _requests;
+    // _bufferRequests = _requests;
     setState(ViewState.idle);
   }
 
- 
-  // void _parseRequestDateTime(Request newRequest) {
-  //   final DateTime _dateTime = DateTime.fromMicrosecondsSinceEpoch(
-  //     int.parse(newRequest.dateTime!),
-  //   ).toLocal();
-    
-  //   newRequest.dateTime = DateFormat('yMd').format(_dateTime);
-   
-  //   _requests.insert(0, newRequest);
-  // }
+  Future<void> checkIfExistsAndAddNewRequest(Request newRequest) async {
+    if (!_uniqueRequestIds.contains(newRequest.id)) {
+      _uniqueRequestIds.add(newRequest.id!);
+      _requests.insert(0, newRequest);
+      notifyListeners();
+    }
+  }
 
-  // Future<void> rejectRequest({required String requestId}) async {
-  //   navigationService.pushDialog(
-  //     CustomAlertDialog(
-  //       reverse: true,
-  //       dialogSubTitle: 'Are you sure you want to reject this request?',
-  //       successText: 'Delete',
-  //       success: () {
-  //         navigationService.pop();
-  //         _requestService.rejectRequest(requestId).then(
-  //           (result) async {
-  //             if (result != null) {
-  //               navigationService.pop();
-  //               setState(ViewState.busy);
-  //               print(result);
-  //               _uniqueRequestIds.remove(RequestId);
-  //               _requests.removeWhere((element) => element.id == requestId);
-  //               await Future.delayed(const Duration(milliseconds: 500));
-  //               setState(ViewState.idle);
-  //             }
-  //           },
-  //         );
-  //       },
-  //     ),
-  //   );
-  // }
+  Future<void> acceptRequest({required String requestId}) async {
+    setState(ViewState.busy);
+    notifyListeners();
+    await _requestService.acceptRequest(requestId);
+    refreshRequests();
+    setState(ViewState.idle);
+  }
 
-
+  Future<void> rejectRequest({required String requestId}) async {
+    setState(ViewState.busy);
+    notifyListeners();
+    await _requestService.rejectRequest(requestId);
+    refreshRequests();
+    setState(ViewState.idle);
+  }
 
   @override
   void dispose() {
