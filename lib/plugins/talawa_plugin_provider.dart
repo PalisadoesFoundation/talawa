b854778ce938cd455fd999e3e3aee465dbebea9f
SiddheshKukade <littlelord124@gmail.com>
2022-07-23 18:48:56 +0530

    Docs for 'TalawaPluginProvider'

--- lib/plugins/talawa_plugin_provider.dart
+++ lib/plugins/talawa_plugin_provider.dart
@@ -16,22 +16,25 @@ class TalawaPluginProvider extends StatelessWidget {
   }) : super(key: key);
 
   ///child contains the widget for the plugin UI.
+  ///child contains the widget for the plugin UI.
   final Widget? child;
+
+  ///visible is the property that decides visibility of the UI.
 
   ///visible is the property that decides visibility of the UI.
   final bool visible;
 
-  ///name of plugin preferred with underscores(_) instead of spaces
+  ///name of plugin preferred with underscores(_) insted of spaces
   final String pluginName;
 
+  ///return `bool` decides the final visibility of the verifying from database and current OrgId
   ///return `bool` decides the final visibility of the verifying from database and current OrgId
   bool checkFromPluginList() {
     final UserConfig userConfig = locator<UserConfig>();
     final Box box;
     bool res = false;
-    box = Hive.box('pluginBox');
-    var pluginList = box.get('plugins');
-    pluginList ??= []; // if null then make it  []
+    box = Hive.box('myBox');
+    final pluginList = box.get('name');
 
     ///mapping over the list from the server
     pluginList
@@ -52,6 +55,7 @@ class TalawaPluginProvider extends StatelessWidget {
     return res;
   }
 
+  ///build the Plugin combining local `visibility` property and `serverVisible` property
   ///build the Plugin combining local `visibility` property and `serverVisible` property
   @override
   Widget build(BuildContext context) {
