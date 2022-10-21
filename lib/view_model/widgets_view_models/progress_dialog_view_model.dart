5ab8175b4acef6d6dca606542a35294e6f2f5e1a
vedant-jain03 <76901313+vedant-jain03@users.noreply.github.com>
2022-10-21 10:50:54 +0530

    [doc] : lib/view_model/widgets_view_models

--- lib/view_model/widgets_view_models/progress_dialog_view_model.dart
+++ lib/view_model/widgets_view_models/progress_dialog_view_model.dart
@@ -3,10 +3,13 @@ import 'package:talawa/enums/enums.dart';
 import 'package:talawa/locator.dart';
 import 'package:talawa/view_model/base_view_model.dart';
 
+/// ProgressDialogViewModel class helps to serve the data and
+/// to react to user's input for Progress Dialog Widget.
 class ProgressDialogViewModel extends BaseModel {
   late ConnectivityResult connectivityResult;
   bool connectivityPresent = false;
 
+  // initialiser
   Future<void> initialise() async {
     setState(ViewState.busy);
     connectivityResult = await connectivity.checkConnectivity();
