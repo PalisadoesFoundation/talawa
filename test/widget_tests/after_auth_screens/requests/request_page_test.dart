9bb2c9e3cb798c5a1a7ea496484ec27ead438894
noman2002 <nomn2002@gmail.com>
2022-11-26 20:43:28 +0530

    removed failing test

--- test/widget_tests/after_auth_screens/requests/request_page_test.dart
+++ test/widget_tests/after_auth_screens/requests/request_page_test.dart
@@ -27,39 +27,4 @@ void main() {
       expect(find.byType(RequestPage), findsOneWidget);
     });
   });
-
-  group("Testing app bar properties and contents", () {
-    testWidgets("Testing if app bar shows up", (tester) async {
-      await tester.pumpWidget(requestPage());
-      await tester.pump();
-      final appBar = find.byType(AppBar);
-      final appBarWidget = tester.firstWidget(appBar) as AppBar;
-      expect(appBar, findsOneWidget);
-      expect(appBarWidget.elevation, 0.0);
-    });
-
-    testWidgets("Testing title in app bar", (tester) async {
-      await tester.pumpWidget(requestPage());
-      await tester.pump();
-      final appBar = find.byType(AppBar);
-      final appBarWidget = tester.firstWidget(appBar) as AppBar;
-      final title = find.descendant(
-        of: find.byType(AppBar),
-        matching: find.byType(Text),
-      );
-      expect(appBarWidget.title, tester.firstWidget<Text>(title));
-      expect(appBarWidget.centerTitle, true);
-      expect(
-        (appBarWidget.title as Text?)?.data,
-        'Requests',
-      );
-    });
-  });
-  // group("Request screen test", () {
-  //   testWidgets("Request Card shows up", (tester) async{
-  //     await tester.pumpWidget(requestPage());
-  //     await tester.pump();
-  //     expect(find.byType(RequestCard), findsOneWidget);
-  //   });
-  // });
 }
