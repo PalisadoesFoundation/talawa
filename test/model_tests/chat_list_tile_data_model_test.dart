34e0953b8877e3eb628ec20cc83c9d4669285dff
droy312 <dhritiman.roy312@gmail.com>
2023-01-27 19:29:56 +0530

    feat: Add test for chat_list_tile_data_mode.dart

---
+++ test/model_tests/chat_list_tile_data_model_test.dart
@@ -0,0 +1,44 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:talawa/models/chats/chat_list_tile_data_model.dart';
+import 'package:talawa/models/chats/chat_user.dart';
+
+void main() {
+  group('Test ChatListTileDataModel', () {
+    test('Test ChatListTileDataModel.fromJson', () {
+      const int listLength = 4;
+
+      final List<ChatUser> users = List.generate(listLength, (index) {
+        return ChatUser(
+          id: '$index',
+          firstName: 'First$index',
+          image: 'Image$index',
+        );
+      });
+
+      final ChatListTileDataModel chatListTileDataModel = ChatListTileDataModel(
+        users,
+        '123',
+      );
+
+      final Map<String, dynamic> json = {
+        '_id': '123',
+        'users': List.generate(listLength, (index) {
+          return {
+            '_id': '$index',
+            'firstName': 'First$index',
+            'image': 'Image$index',
+          };
+        }),
+      };
+
+      final ChatListTileDataModel chatListTileDataModelFromJson = ChatListTileDataModel.fromJson(json);
+
+      expect(chatListTileDataModel.id, chatListTileDataModelFromJson.id);
+      for (int index = 0; index < listLength; index++) {
+        expect(chatListTileDataModel.users?[index].id, chatListTileDataModelFromJson.users?[index].id);
+        expect(chatListTileDataModel.users?[index].firstName, chatListTileDataModelFromJson.users?[index].firstName);
+        expect(chatListTileDataModel.users?[index].image, chatListTileDataModelFromJson.users?[index].image);
+      }
+    });
+  });
+}
