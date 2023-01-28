99e080c3b1bc2c6db5d75843de833a1de345e395
droy312 <dhritiman.roy312@gmail.com>
2023-01-28 11:17:55 +0530

    rfac: Updated test for chat_list_tile_data_model.dart

--- test/model_tests/chat_list_tile_data_model_test.dart
+++ test/model_tests/chat_list_tile_data_model_test.dart
@@ -4,45 +4,65 @@ import 'package:talawa/models/chats/chat_user.dart';
 
 void main() {
   group('Test ChatListTileDataModel', () {
-    test('Test ChatListTileDataModel.fromJson', () {
-      const int listLength = 4;
+    const int listLength = 4;
 
-      final List<ChatUser> users = List.generate(listLength, (index) {
-        return ChatUser(
-          id: '$index',
-          firstName: 'First$index',
-          image: 'Image$index',
-        );
-      });
-
-      final ChatListTileDataModel chatListTileDataModel = ChatListTileDataModel(
-        users,
-        '123',
+    final List<ChatUser> users = List.generate(listLength, (index) {
+      return ChatUser(
+        id: '$index',
+        firstName: 'First$index',
+        image: 'Image$index',
       );
+    });
 
-      final Map<String, dynamic> json = {
-        '_id': '123',
-        'users': List.generate(listLength, (index) {
-          return {
-            '_id': '$index',
-            'firstName': 'First$index',
-            'image': 'Image$index',
-          };
-        }),
-      };
+    final ChatListTileDataModel chatListTileDataModel = ChatListTileDataModel(
+      users,
+      '123',
+    );
 
+    final Map<String, dynamic> json = {
+      '_id': '123',
+      'users': List.generate(listLength, (index) {
+        return {
+          '_id': '$index',
+          'firstName': 'First$index',
+          'image': 'Image$index',
+        };
+      }),
+    };
+
+    void checkUsers(List<ChatUser>? users1, List<ChatUser>? users2) {
+      if (users1 == null || users2 == null) {
+        expect(users1, users2);
+        return;
+      }
+
+      for (int index = 0; index < users1.length; index++) {
+        expect(users1[index].id, users2[index].id);
+        expect(users1[index].firstName, users2[index].firstName);
+        expect(users1[index].image, users2[index].image);
+      }
+    }
+
+    test('Test constructor', () {
+      expect(chatListTileDataModel.id, '123');
+      checkUsers(chatListTileDataModel.users, users);
+    });
+
+    test('Test ChatListTileDataModel.fromJson', () {
       final ChatListTileDataModel chatListTileDataModelFromJson =
           ChatListTileDataModel.fromJson(json);
 
       expect(chatListTileDataModel.id, chatListTileDataModelFromJson.id);
-      for (int index = 0; index < listLength; index++) {
-        expect(chatListTileDataModel.users?[index].id,
-            chatListTileDataModelFromJson.users?[index].id);
-        expect(chatListTileDataModel.users?[index].firstName,
-            chatListTileDataModelFromJson.users?[index].firstName);
-        expect(chatListTileDataModel.users?[index].image,
-            chatListTileDataModelFromJson.users?[index].image);
-      }
+      checkUsers(chatListTileDataModel.users, chatListTileDataModelFromJson.users);
+    });
+
+    test('Test toJson', () {
+      final Map<String, dynamic> json = chatListTileDataModel.toJson();
+
+      expect(json['users'], isA<List<ChatUser>>());
+
+      expect(json['_id'], chatListTileDataModel.id);
+      checkUsers(json['users'] as List<ChatUser>, chatListTileDataModel.users);
     });
   });
 }
