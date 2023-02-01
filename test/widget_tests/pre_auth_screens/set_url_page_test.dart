3f0de6fd596b36ccaba7ef7f0a14d9da75b5229f
Ayush0Chaudhary <ayush0000ayush@gmail.com>
2023-02-01 17:39:51 +0530

    deleted file

--- test/widget_tests/pre_auth_screens/set_url_page_test.dart
+++ test/widget_tests/pre_auth_screens/set_url_page_test.dart
@@ -1,7 +1,5 @@
 // import 'dart:io';
 
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 // import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
