1abb314794b14976a1601ea4aa84dbe594c181ea
Amogh35 <105104954+Amogh35@users.noreply.github.com>
2023-02-22 00:19:02 +0530

    Added test for post_queries.dart (#1551)
    * Added test for comment_queries.dart
    
    * added test for post_queries.dart

---
+++ test/utils/post_queries_test.dart
@@ -0,0 +1,70 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:talawa/utils/post_queries.dart';
+
+void main() {
+  group("Tests for post_queries.dart", () {
+    test("Check if getPostsById works correctly", () {
+      const data = """
+      query {
+        postsByOrganization(id: "sampleID",orderBy: createdAt_DESC )
+        { 
+          _id
+          text
+          createdAt
+          imageUrl
+          videoUrl
+          title
+          commentCount
+          likeCount
+          creator{
+            _id
+            firstName
+            lastName
+            image
+          }
+          organization{
+            _id
+          }
+          likedBy{
+            _id
+          }
+          comments{
+            _id
+          }
+        }
+      }
+""";
+      final fnData = PostQueries().getPostsById("sampleID");
+      expect(fnData, data);
+    });
+
+    test("Check if addLike works correctly", () {
+      const data = """
+     mutation likePost(\$postID: ID!) { 
+      likePost( id: \$postID,)
+      {
+        _id
+      }
+    }
+  """;
+      final fnData = PostQueries().addLike();
+      expect(fnData, data);
+    });
+
+    test("Check if removeLike works correctly", () {
+      const data = """
+     mutation unlikePost(\$postID: ID!) { 
+      unlikePost( id: \$postID,)
+      {
+        _id
+        likedBy{
+        _id
+        }
+      }
+    }
+  """;
+      final fnData = PostQueries().removeLike();
+      expect(fnData, data);
+    });
+  });
+}
