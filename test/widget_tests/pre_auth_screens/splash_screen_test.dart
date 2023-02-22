afe6442e208d39f64d2801d0242a526fc8775d2e
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-02-22 23:44:43 +0530

    Test splash_screen.dart (#1556)
    * Create .gitpod.yml
    
    * Create .gitpod.Dockerfile
    
    * Add/temp-file
    
    * Add/temp-plugin
    
    gives sample demo for `TalawaPluginProvider `
    
    * remove/unecessary-files#1
    
    * remove/unecessary-files#2
    
    * Add/TalwaPluginProvider
    
    * Add/queries `getPluginsList`
    
    * Docs for 'TalawaPluginProvider'
    
    * `Add/fetch_plugin_list`
    
    * Refractoring & adding `fetchPlugins` in main
    
    * Delete sa.dart
    
    * Delete tempPlugin.dart
    
    * Test/ `Tawla Plugin Provider`
    
    * Refractor : Added `Braintree` config in AndroidManifest.xml
    
    * Refractor : Added `Talawa_plugin_provider_nav` to control plugins visibility in navbar
    
    * Update : Main file setState fix
    
    * Changed  ios version from  `10.0` to `12.0`
    
    * Add : `Donation` feature as Plugin with braintree API
    
    * Add : `createDonation` Mutation
    
    * Fix : `Plugin` render bug
    
    * Chores : Formatted  files
    
    * Add : `navBarItems` (features) as plugins
    
    * Chores : Formatting
    
    * Update : Profile removed key
    
    * Chores : Add `Flutter_Braintree`
    
    * Chores : Fix `Lint errors`
    
    * Fix : Formatting Errors
    
    * Delete talawa_plugin_provider_nav.dart
    
    * Update pull-request.yml
    
    * Added a empty line in main
    
    * Update pull-request.yml
    
    * Fix  : Faling tests
    
    * Fix : Failing test #2 commented out `teste.pumpAndSetttle`
    
    * Remove : all the changes done in the `custom_drawer_test.dart`
    
    Removed everthing that I've added to check if it will pass the test or not.
    
    * PR Fail Fix  Attempt #1 : Added `fetchPluginLIst` in `main.dart`
    
    * PR Fail Fix Attempt #2  : Commenting test cases to check furthur problems
    
    * Chores  : Commented  Imports
    
    * Deleted : Test file
    
    * Fix:  Trigger workflow
    
    * Add : Faling test back `custom_drawer_test.dart`
    
    * Add Test for `utils/query.dart`
    
    * Add : Tests `queries_test.dart`
    
    * Add : Tests `chat_queries_test.dart`
    
    * Add : Test for `comment_queries_test.dart`
    
    * Add : test for `event_queries_test`
    
    * Add :  test for `post_qurie..dart`
    
    * Add : test for `post_query.dart`
    
    * Add : test for `task_queries.dart`
    
    * Add  : test for `utils/validator.dart`
    
    * Fix : failing tests
    
    * Test : Failing test uncommented `custom_drawer_test.dart`
    
    * Fix : Commented `custom_drawer_test.dart`
    
    * removed utils tests
    
    * Removed Commented Code
    
    * add/table of contents and link to installation file.
    
    * Add/splash  tests

--- test/widget_tests/pre_auth_screens/splash_screen_test.dart
+++ test/widget_tests/pre_auth_screens/splash_screen_test.dart
@@ -62,166 +62,166 @@ Widget createSplashScreenDark({ThemeMode themeMode = ThemeMode.dark}) =>
     );
 
 Future<void> main() async {
-  // setupLocator();
-  // graphqlConfig.test();
+  setupLocator();
+  graphqlConfig.test();
 
   // setupFirebaseMocks();
   // await Firebase.initializeApp();
 
-  // group('Splash Screen Widget Test in light mode', () {
-  //   testWidgets("Testing if Splash Screen shows up", (tester) async {
-  //     await tester.pumpWidget(createSplashScreenLight());
-  //     await tester.pumpAndSettle();
-  //     final screenScaffoldWidget =
-  //         find.byKey(const Key('SplashScreenScaffold'));
-  //     expect(screenScaffoldWidget, findsOneWidget);
-  //     expect(
-  //       (tester.firstWidget(find.byKey(const Key('Root'))) as MaterialApp)
-  //           .theme!
-  //           .scaffoldBackgroundColor,
-  //       TalawaTheme.lightTheme.scaffoldBackgroundColor,
-  //     );
-  //   });
-  //   testWidgets("Testing if app logo shows up", (tester) async {
-  //     await tester.pumpWidget(createSplashScreenLight());
-  //     await tester.pumpAndSettle();
-  //     final logoWidget = find.byKey(const Key('LogoPainter'));
-  //     expect(logoWidget, findsOneWidget);
-  //     expect(
-  //       (tester.firstWidget(logoWidget) as CustomPaint).size,
-  //       Size(
-  //         SizeConfig.screenWidth! * 0.6,
-  //         SizeConfig.screenWidth! * 0.6,
-  //       ),
-  //     );
-  //   });
-  //   testWidgets("Testing if app name shows up", (tester) async {
-  //     await tester.pumpWidget(createSplashScreenLight());
-  //     await tester.pumpAndSettle();
-  //     final findAppNameWidget = find.text('TALAWA');
-  //     expect(findAppNameWidget, findsOneWidget);
-  //     expect(
-  //       (tester.firstWidget(findAppNameWidget) as Text).style!.color,
-  //       TalawaTheme.lightTheme.textTheme.headlineMedium!.color,
-  //     );
-  //     expect(
-  //       (tester.firstWidget(findAppNameWidget) as Text).style!.fontFamily,
-  //       TalawaTheme.lightTheme.textTheme.headlineMedium!.fontFamily,
-  //     );
-  //     expect(
-  //       (tester.firstWidget(findAppNameWidget) as Text).style!.fontSize,
-  //       TalawaTheme.lightTheme.textTheme.headlineMedium!.fontSize,
-  //     );
-  //   });
-  //   testWidgets("Testing if provider text shows up", (tester) async {
-  //     await tester.pumpWidget(createSplashScreenLight());
-  //     await tester.pumpAndSettle();
-  //     final findProviderTextWidget = find.text('from');
-  //     expect(findProviderTextWidget, findsOneWidget);
-  //     expect(
-  //       (tester.firstWidget(findProviderTextWidget) as Text).style!.color,
-  //       TalawaTheme.lightTheme.textTheme.bodySmall!.color,
-  //     );
-  //     expect(
-  //       (tester.firstWidget(findProviderTextWidget) as Text).style!.fontFamily,
-  //       TalawaTheme.lightTheme.textTheme.bodySmall!.fontFamily,
-  //     );
-  //     expect(
-  //       (tester.firstWidget(findProviderTextWidget) as Text).style!.fontSize,
-  //       TalawaTheme.lightTheme.textTheme.bodySmall!.fontSize,
-  //     );
-  //   });
-  //   testWidgets("Testing if provider name shows up", (tester) async {
-  //     await tester.pumpWidget(createSplashScreenLight());
-  //     await tester.pumpAndSettle();
-  //     final findProviderNameWidget = find.text('PALISADOES');
-  //     expect(findProviderNameWidget, findsOneWidget);
-  //     expect(
-  //       (tester.firstWidget(findProviderNameWidget) as Text).style!.color,
-  //       TalawaTheme.lightTheme.textTheme.titleSmall!.color,
-  //     );
-  //     expect(
-  //       (tester.firstWidget(findProviderNameWidget) as Text).style!.fontFamily,
-  //       TalawaTheme.lightTheme.textTheme.titleSmall!.fontFamily,
-  //     );
-  //   });
-  // });
-  // group('Splash Screen Widget Test in dark mode', () {
-  //   testWidgets("Testing if Splash Screen shows up", (tester) async {
-  //     await tester.pumpWidget(createSplashScreenDark());
-  //     await tester.pumpAndSettle();
-  //     final screenScaffoldWidget =
-  //         find.byKey(const Key('SplashScreenScaffold'));
-  //     expect(screenScaffoldWidget, findsOneWidget);
-  //     expect(
-  //       (tester.firstWidget(find.byKey(const Key('Root'))) as MaterialApp)
-  //           .darkTheme!
-  //           .scaffoldBackgroundColor,
-  //       TalawaTheme.darkTheme.scaffoldBackgroundColor,
-  //     );
-  //   });
-  //   testWidgets("Testing if app logo shows up", (tester) async {
-  //     await tester.pumpWidget(createSplashScreenDark());
-  //     await tester.pumpAndSettle();
-  //     final logoWidget = find.byKey(const Key('LogoPainter'));
-  //     expect(logoWidget, findsOneWidget);
-  //     expect(
-  //       (tester.firstWidget(logoWidget) as CustomPaint).size,
-  //       Size(
-  //         SizeConfig.screenWidth! * 0.6,
-  //         SizeConfig.screenWidth! * 0.6,
-  //       ),
-  //     );
-  //   });
-  //   testWidgets("Testing if app name shows up", (tester) async {
-  //     await tester.pumpWidget(createSplashScreenDark());
-  //     await tester.pumpAndSettle();
-  //     final findAppNameWidget = find.text('TALAWA');
-  //     expect(findAppNameWidget, findsOneWidget);
-  //     expect(
-  //       (tester.firstWidget(findAppNameWidget) as Text).style!.color,
-  //       TalawaTheme.darkTheme.textTheme.headlineMedium!.color,
-  //     );
-  //     expect(
-  //       (tester.firstWidget(findAppNameWidget) as Text).style!.fontFamily,
-  //       TalawaTheme.darkTheme.textTheme.headlineMedium!.fontFamily,
-  //     );
-  //     expect(
-  //       (tester.firstWidget(findAppNameWidget) as Text).style!.fontSize,
-  //       TalawaTheme.darkTheme.textTheme.headlineMedium!.fontSize,
-  //     );
-  //   });
-  //   testWidgets("Testing if provider text shows up", (tester) async {
-  //     await tester.pumpWidget(createSplashScreenDark());
-  //     await tester.pumpAndSettle();
-  //     final findProviderTextWidget = find.text('from');
-  //     expect(findProviderTextWidget, findsOneWidget);
-  //     expect(
-  //       (tester.firstWidget(findProviderTextWidget) as Text).style!.color,
-  //       TalawaTheme.darkTheme.textTheme.bodySmall!.color,
-  //     );
-  //     expect(
-  //       (tester.firstWidget(findProviderTextWidget) as Text).style!.fontFamily,
-  //       TalawaTheme.darkTheme.textTheme.bodySmall!.fontFamily,
-  //     );
-  //     expect(
-  //       (tester.firstWidget(findProviderTextWidget) as Text).style!.fontSize,
-  //       TalawaTheme.darkTheme.textTheme.bodySmall!.fontSize,
-  //     );
-  //   });
-  //   testWidgets("Testing if provider name shows up", (tester) async {
-  //     await tester.pumpWidget(createSplashScreenDark());
-  //     await tester.pumpAndSettle();
-  //     final findProviderNameWidget = find.text('PALISADOES');
-  //     expect(findProviderNameWidget, findsOneWidget);
-  //     expect(
-  //       (tester.firstWidget(findProviderNameWidget) as Text).style!.color,
-  //       TalawaTheme.darkTheme.textTheme.titleSmall!.color,
-  //     );
-  //     expect(
-  //       (tester.firstWidget(findProviderNameWidget) as Text).style!.fontFamily,
-  //       TalawaTheme.darkTheme.textTheme.titleSmall!.fontFamily,
-  //     );
-  //   });
-  // });
+  group('Splash Screen Widget Test in light mode', () {
+    testWidgets("Testing if Splash Screen shows up", (tester) async {
+      await tester.pumpWidget(createSplashScreenLight());
+      await tester.pumpAndSettle();
+      final screenScaffoldWidget =
+          find.byKey(const Key('SplashScreenScaffold'));
+      expect(screenScaffoldWidget, findsOneWidget);
+      expect(
+        (tester.firstWidget(find.byKey(const Key('Root'))) as MaterialApp)
+            .theme!
+            .scaffoldBackgroundColor,
+        TalawaTheme.lightTheme.scaffoldBackgroundColor,
+      );
+    });
+    testWidgets("Testing if app logo shows up", (tester) async {
+      await tester.pumpWidget(createSplashScreenLight());
+      await tester.pumpAndSettle();
+      final logoWidget = find.byKey(const Key('LogoPainter'));
+      expect(logoWidget, findsOneWidget);
+      expect(
+        (tester.firstWidget(logoWidget) as CustomPaint).size,
+        Size(
+          SizeConfig.screenWidth! * 0.6,
+          SizeConfig.screenWidth! * 0.6,
+        ),
+      );
+    });
+    testWidgets("Testing if app name shows up", (tester) async {
+      await tester.pumpWidget(createSplashScreenLight());
+      await tester.pumpAndSettle();
+      final findAppNameWidget = find.text('TALAWA');
+      expect(findAppNameWidget, findsOneWidget);
+      expect(
+        (tester.firstWidget(findAppNameWidget) as Text).style!.color,
+        TalawaTheme.lightTheme.textTheme.headlineMedium!.color,
+      );
+      expect(
+        (tester.firstWidget(findAppNameWidget) as Text).style!.fontFamily,
+        TalawaTheme.lightTheme.textTheme.headlineMedium!.fontFamily,
+      );
+      expect(
+        (tester.firstWidget(findAppNameWidget) as Text).style!.fontSize,
+        TalawaTheme.lightTheme.textTheme.headlineMedium!.fontSize,
+      );
+    });
+    testWidgets("Testing if provider text shows up", (tester) async {
+      await tester.pumpWidget(createSplashScreenLight());
+      await tester.pumpAndSettle();
+      final findProviderTextWidget = find.text('from');
+      expect(findProviderTextWidget, findsOneWidget);
+      expect(
+        (tester.firstWidget(findProviderTextWidget) as Text).style!.color,
+        TalawaTheme.lightTheme.textTheme.bodySmall!.color,
+      );
+      expect(
+        (tester.firstWidget(findProviderTextWidget) as Text).style!.fontFamily,
+        TalawaTheme.lightTheme.textTheme.bodySmall!.fontFamily,
+      );
+      expect(
+        (tester.firstWidget(findProviderTextWidget) as Text).style!.fontSize,
+        TalawaTheme.lightTheme.textTheme.bodySmall!.fontSize,
+      );
+    });
+    testWidgets("Testing if provider name shows up", (tester) async {
+      await tester.pumpWidget(createSplashScreenLight());
+      await tester.pumpAndSettle();
+      final findProviderNameWidget = find.text('PALISADOES');
+      expect(findProviderNameWidget, findsOneWidget);
+      expect(
+        (tester.firstWidget(findProviderNameWidget) as Text).style!.color,
+        TalawaTheme.lightTheme.textTheme.titleSmall!.color,
+      );
+      expect(
+        (tester.firstWidget(findProviderNameWidget) as Text).style!.fontFamily,
+        TalawaTheme.lightTheme.textTheme.titleSmall!.fontFamily,
+      );
+    });
+  });
+  group('Splash Screen Widget Test in dark mode', () {
+    testWidgets("Testing if Splash Screen shows up", (tester) async {
+      await tester.pumpWidget(createSplashScreenDark());
+      await tester.pumpAndSettle();
+      final screenScaffoldWidget =
+          find.byKey(const Key('SplashScreenScaffold'));
+      expect(screenScaffoldWidget, findsOneWidget);
+      expect(
+        (tester.firstWidget(find.byKey(const Key('Root'))) as MaterialApp)
+            .darkTheme!
+            .scaffoldBackgroundColor,
+        TalawaTheme.darkTheme.scaffoldBackgroundColor,
+      );
+    });
+    testWidgets("Testing if app logo shows up", (tester) async {
+      await tester.pumpWidget(createSplashScreenDark());
+      await tester.pumpAndSettle();
+      final logoWidget = find.byKey(const Key('LogoPainter'));
+      expect(logoWidget, findsOneWidget);
+      expect(
+        (tester.firstWidget(logoWidget) as CustomPaint).size,
+        Size(
+          SizeConfig.screenWidth! * 0.6,
+          SizeConfig.screenWidth! * 0.6,
+        ),
+      );
+    });
+    testWidgets("Testing if app name shows up", (tester) async {
+      await tester.pumpWidget(createSplashScreenDark());
+      await tester.pumpAndSettle();
+      final findAppNameWidget = find.text('TALAWA');
+      expect(findAppNameWidget, findsOneWidget);
+      expect(
+        (tester.firstWidget(findAppNameWidget) as Text).style!.color,
+        TalawaTheme.darkTheme.textTheme.headlineMedium!.color,
+      );
+      expect(
+        (tester.firstWidget(findAppNameWidget) as Text).style!.fontFamily,
+        TalawaTheme.darkTheme.textTheme.headlineMedium!.fontFamily,
+      );
+      expect(
+        (tester.firstWidget(findAppNameWidget) as Text).style!.fontSize,
+        TalawaTheme.darkTheme.textTheme.headlineMedium!.fontSize,
+      );
+    });
+    testWidgets("Testing if provider text shows up", (tester) async {
+      await tester.pumpWidget(createSplashScreenDark());
+      await tester.pumpAndSettle();
+      final findProviderTextWidget = find.text('from');
+      expect(findProviderTextWidget, findsOneWidget);
+      expect(
+        (tester.firstWidget(findProviderTextWidget) as Text).style!.color,
+        TalawaTheme.darkTheme.textTheme.bodySmall!.color,
+      );
+      expect(
+        (tester.firstWidget(findProviderTextWidget) as Text).style!.fontFamily,
+        TalawaTheme.darkTheme.textTheme.bodySmall!.fontFamily,
+      );
+      expect(
+        (tester.firstWidget(findProviderTextWidget) as Text).style!.fontSize,
+        TalawaTheme.darkTheme.textTheme.bodySmall!.fontSize,
+      );
+    });
+    testWidgets("Testing if provider name shows up", (tester) async {
+      await tester.pumpWidget(createSplashScreenDark());
+      await tester.pumpAndSettle();
+      final findProviderNameWidget = find.text('PALISADOES');
+      expect(findProviderNameWidget, findsOneWidget);
+      expect(
+        (tester.firstWidget(findProviderNameWidget) as Text).style!.color,
+        TalawaTheme.darkTheme.textTheme.titleSmall!.color,
+      );
+      expect(
+        (tester.firstWidget(findProviderNameWidget) as Text).style!.fontFamily,
+        TalawaTheme.darkTheme.textTheme.titleSmall!.fontFamily,
+      );
+    });
+  });
 }
