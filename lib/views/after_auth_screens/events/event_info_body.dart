1abcb0d69de1963a84033fbf61ebf4108bf12496
Aditya Thakur <itsadityathakur001@gmail.com>
2023-03-02 21:32:07 +0530

    Implement scrolling functionality for the Event Details page #1601
    1. see all option removed.
    2. Members is scrollable now.

--- lib/views/after_auth_screens/events/event_info_body.dart
+++ lib/views/after_auth_screens/events/event_info_body.dart
@@ -23,242 +23,238 @@ class EventInfoBody extends StatelessWidget {
     return SliverToBoxAdapter(
       child: Padding(
         padding: const EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  // event title
-                  event.title!,
-                  style: Theme.of(context)
-                      .textTheme
-                      .headlineMedium!
-                      .copyWith(fontSize: 26),
-                ),
-                const Icon(Icons.chat_bubble_outline)
-              ],
-            ),
-            Text(
-              // Display event creator full name.
-              "${AppLocalizations.of(context)!.strictTranslate("Created by")}: ${event.creator!.firstName} ${event.creator!.lastName}",
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyMedium!
-                  .copyWith(fontWeight: FontWeight.w600),
-            ),
-            SizedBox(
-              height: SizeConfig.screenHeight! * 0.011,
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Icon(
-                  Icons.calendar_today,
-                  size: 13,
-                ),
-                SizedBox(
-                  width: SizeConfig.screenWidth! * 0.027,
-                ),
-                Text(
-                  // Display start and end date of the Event.
-                  "${event.startDate!} - ${event.endDate!}",
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-                const Spacer(),
-                // If event type is public then renders lock_open icon else renders lock icon.
-                event.isPublic!
-                    ? Icon(
-                        Icons.lock_open,
-                        size: 13,
-                        color: Theme.of(context).colorScheme.secondary,
-                      )
-                    : Icon(
-                        Icons.lock,
-                        size: 13,
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
-                SizedBox(
-                  width: SizeConfig.screenWidth! * 0.027,
-                ),
-                // If event type is public then renders 'public'
-                // else renders 'private' text translated into the app language.
-                event.isPublic!
-                    ? Text(
-                        AppLocalizations.of(context)!.strictTranslate('public'),
-                        style: Theme.of(context).textTheme.bodySmall,
-                      )
-                    : Text(
-                        AppLocalizations.of(context)!
-                            .strictTranslate('private'),
-                        style: Theme.of(context).textTheme.bodySmall,
-                      ),
-              ],
-            ),
-            SizedBox(
-              height: SizeConfig.screenHeight! * 0.011,
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // display schedule icon with the start and end date of the time.
-                const Icon(
-                  Icons.schedule,
-                  size: 12,
-                ),
-                SizedBox(
-                  width: SizeConfig.screenWidth! * 0.025,
-                ),
-                Text(
-                  "${event.startTime!} - ${event.endTime!}",
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-              ],
-            ),
-            SizedBox(
-              height: SizeConfig.screenHeight! * 0.011,
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Icon(
-                  Icons.place,
-                  size: 12,
-                ),
-                SizedBox(
-                  width: SizeConfig.screenWidth! * 0.027,
-                ),
-                Text(
-                  event.location!,
-                  style: Theme.of(context).textTheme.bodySmall,
-                  overflow: TextOverflow.ellipsis,
-                  textAlign: TextAlign.left,
-                  maxLines: 3,
-                ),
-                const Spacer(),
-                // If the event location is not null then display the location
-                //on the map with the reference of latitude and longitude provided for the event.
-                if (event.latitude != null && event.longitude != null)
-                  GestureDetector(
-                    onTap: () => navigationService.pushScreen(
-                      Routes.mapScreen,
-                      arguments: {
-                        "latitude": event.latitude,
-                        "longitude": event.longitude,
-                      },
-                    ),
-                    child: Text(
-                      'View on map',
-                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                            color: Theme.of(context).colorScheme.secondary,
-                          ),
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.left,
-                      maxLines: 3,
-                    ),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    // event title
+                    event.title!,
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineMedium!
+                        .copyWith(fontSize: 26),
                   ),
-              ],
-            ),
-            const Divider(),
-            SizedBox(
-              height: SizeConfig.screenHeight! * 0.013,
-            ),
-            Text(
-              AppLocalizations.of(context)!.strictTranslate("Description"),
-              style: Theme.of(context)
-                  .textTheme
-                  .headlineSmall!
-                  .copyWith(fontSize: 16),
-            ),
-            SizedBox(width: SizeConfig.screenWidth! * 0.013),
-            Text(
-              // Display the Description of the event if not null.
-              event.description!,
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-            SizedBox(height: SizeConfig.screenHeight! * 0.013),
-            Text(
-              AppLocalizations.of(context)!.strictTranslate("Admins"),
-              style: Theme.of(context)
-                  .textTheme
-                  .headlineSmall!
-                  .copyWith(fontSize: 16),
-            ),
-            Divider(
-              color: Theme.of(context).colorScheme.onBackground,
-              thickness: 2,
-            ),
-            ListView.builder(
-              padding: EdgeInsets.zero,
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: event.admins!.length,
-              itemBuilder: (BuildContext context, int index) {
-                return CustomListTile(
-                  key: Key(
-                    '${AppLocalizations.of(context)!.strictTranslate("Admins")}$index',
+                  const Icon(Icons.chat_bubble_outline)
+                ],
+              ),
+              Text(
+                // Display event creator full name.
+                "${AppLocalizations.of(context)!.strictTranslate("Created by")}: ${event.creator!.firstName} ${event.creator!.lastName}",
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: SizeConfig.screenHeight! * 0.011,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Icon(
+                    Icons.calendar_today,
+                    size: 13,
                   ),
-                  index: index,
-                  type: TileType.user,
-                  userInfo: event.admins![index],
-                  onTapUserInfo: () {},
-                );
-              },
-            ),
-            SizedBox(
-              height: SizeConfig.screenHeight! * 0.013,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  AppLocalizations.of(context)!.strictTranslate("Attendees"),
-                  style: Theme.of(context)
-                      .textTheme
-                      .headlineSmall!
-                      .copyWith(fontSize: 16),
-                ),
-                Text(
-                  AppLocalizations.of(context)!.strictTranslate('See all'),
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodySmall!
-                      .copyWith(color: const Color(0xff4285F4)),
-                ),
-              ],
-            ),
-            Divider(
-              color: Theme.of(context).colorScheme.onBackground,
-              thickness: 2,
-            ),
-            if (model.isBusy)
-              // if the model is still fetching the attendees details then display Circular Progress Indicator Icon.
-              const Padding(
-                padding: EdgeInsets.only(top: 12.0),
-                child: Center(child: CircularProgressIndicator()),
-              )
-            else
-              // else if the model fetched the attendees details successfully
-              //then renders all the attendees in ListView.
+                  SizedBox(
+                    width: SizeConfig.screenWidth! * 0.027,
+                  ),
+                  Text(
+                    // Display start and end date of the Event.
+                    "${event.startDate!} - ${event.endDate!}",
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                  const Spacer(),
+                  // If event type is public then renders lock_open icon else renders lock icon.
+                  event.isPublic!
+                      ? Icon(
+                          Icons.lock_open,
+                          size: 13,
+                          color: Theme.of(context).colorScheme.secondary,
+                        )
+                      : Icon(
+                          Icons.lock,
+                          size: 13,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                  SizedBox(
+                    width: SizeConfig.screenWidth! * 0.027,
+                  ),
+                  // If event type is public then renders 'public'
+                  // else renders 'private' text translated into the app language.
+                  event.isPublic!
+                      ? Text(
+                          AppLocalizations.of(context)!.strictTranslate('public'),
+                          style: Theme.of(context).textTheme.bodySmall,
+                        )
+                      : Text(
+                          AppLocalizations.of(context)!
+                              .strictTranslate('private'),
+                          style: Theme.of(context).textTheme.bodySmall,
+                        ),
+                ],
+              ),
+              SizedBox(
+                height: SizeConfig.screenHeight! * 0.011,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // display schedule icon with the start and end date of the time.
+                  const Icon(
+                    Icons.schedule,
+                    size: 12,
+                  ),
+                  SizedBox(
+                    width: SizeConfig.screenWidth! * 0.025,
+                  ),
+                  Text(
+                    "${event.startTime!} - ${event.endTime!}",
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: SizeConfig.screenHeight! * 0.011,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Icon(
+                    Icons.place,
+                    size: 12,
+                  ),
+                  SizedBox(
+                    width: SizeConfig.screenWidth! * 0.027,
+                  ),
+                  Text(
+                    event.location!,
+                    style: Theme.of(context).textTheme.bodySmall,
+                    overflow: TextOverflow.ellipsis,
+                    textAlign: TextAlign.left,
+                    maxLines: 3,
+                  ),
+                  const Spacer(),
+                  // If the event location is not null then display the location
+                  //on the map with the reference of latitude and longitude provided for the event.
+                  if (event.latitude != null && event.longitude != null)
+                    GestureDetector(
+                      onTap: () => navigationService.pushScreen(
+                        Routes.mapScreen,
+                        arguments: {
+                          "latitude": event.latitude,
+                          "longitude": event.longitude,
+                        },
+                      ),
+                      child: Text(
+                        'View on map',
+                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                              color: Theme.of(context).colorScheme.secondary,
+                            ),
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.left,
+                        maxLines: 3,
+                      ),
+                    ),
+                ],
+              ),
+              const Divider(),
+              SizedBox(
+                height: SizeConfig.screenHeight! * 0.013,
+              ),
+              Text(
+                AppLocalizations.of(context)!.strictTranslate("Description"),
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineSmall!
+                    .copyWith(fontSize: 16),
+              ),
+              SizedBox(width: SizeConfig.screenWidth! * 0.013),
+              Text(
+                // Display the Description of the event if not null.
+                event.description!,
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+              SizedBox(height: SizeConfig.screenHeight! * 0.013),
+              Text(
+                AppLocalizations.of(context)!.strictTranslate("Admins"),
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineSmall!
+                    .copyWith(fontSize: 16),
+              ),
+              Divider(
+                color: Theme.of(context).colorScheme.onBackground,
+                thickness: 2,
+              ),
               ListView.builder(
                 padding: EdgeInsets.zero,
                 physics: const NeverScrollableScrollPhysics(),
                 shrinkWrap: true,
-                itemCount: model.registrants.length,
+                itemCount: event.admins!.length,
                 itemBuilder: (BuildContext context, int index) {
                   return CustomListTile(
                     key: Key(
-                      '${AppLocalizations.of(context)!.strictTranslate("Attendee")}$index',
+                      '${AppLocalizations.of(context)!.strictTranslate("Admins")}$index',
                     ),
                     index: index,
                     type: TileType.user,
-                    userInfo: model.registrants[index],
+                    userInfo: event.admins![index],
                     onTapUserInfo: () {},
                   );
                 },
-              )
-          ],
+              ),
+              SizedBox(
+                height: SizeConfig.screenHeight! * 0.013,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    AppLocalizations.of(context)!.strictTranslate("Attendees"),
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineSmall!
+                        .copyWith(fontSize: 16),
+                  ),
+                  
+                ],
+              ),
+              Divider(
+                color: Theme.of(context).colorScheme.onBackground,
+                thickness: 2,
+              ),
+              if (model.isBusy)
+                // if the model is still fetching the attendees details then display Circular Progress Indicator Icon.
+                const Padding(
+                  padding: EdgeInsets.only(top: 12.0),
+                  child: Center(child: CircularProgressIndicator()),
+                )
+              else
+                // else if the model fetched the attendees details successfully
+                //then renders all the attendees in ListView.
+                ListView.builder(
+                  padding: EdgeInsets.zero,
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: model.registrants.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return CustomListTile(
+                      key: Key(
+                        '${AppLocalizations.of(context)!.strictTranslate("Attendee")}$index',
+                      ),
+                      index: index,
+                      type: TileType.user,
+                      userInfo: model.registrants[index],
+                      onTapUserInfo: () {},
+                    );
+                  },
+                )
+            ],
+          ),
         ),
       ),
     );
