af5c13a7b6394a541be62a069ffffb591f0e2a23
Kushal Agrawal <agrawalskushal@gmail.com>
2023-03-05 18:18:05 +0530

    Unused imports

--- test/views/after_auth_screens/events/create_event_page_test.dart
+++ test/views/after_auth_screens/events/create_event_page_test.dart
@@ -11,7 +11,7 @@ import 'package:talawa/router.dart' as router;
 import 'package:talawa/services/size_config.dart';
 import 'package:talawa/services/third_party_service/multi_media_pick_service.dart';
 import 'package:talawa/utils/app_localization.dart';
-import 'package:talawa/view_model/after_auth_view_models/event_view_models/create_event_view_model.dart';
+// import 'package:talawa/view_model/after_auth_view_models/event_view_models/create_event_view_model.dart';
 import 'package:talawa/view_model/lang_view_model.dart';
 import 'package:talawa/views/after_auth_screens/events/create_event_page.dart';
 import 'package:talawa/views/base_view.dart';
