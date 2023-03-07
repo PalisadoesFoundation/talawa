4173651621fe6db207be74d5e28e50c657d7fc9d
Ikjot Singh Dhody <55295613+ikjot-2605@users.noreply.github.com>
2023-03-08 00:32:27 +0530

    Add test for Edit Task Page to validate onSave callback. (#1613)
    * Add test for Edit Task Page
    
    Signed-off-by: Ikjot Singh Dhody <ikjotsd@gmail.com>
    
    * Add tests for task form callbacks.
    
    Signed-off-by: Ikjot Singh Dhody <ikjotsd@gmail.com>
    
    * Fix task schedule test - remove registerFactory.
    
    Signed-off-by: Ikjot Singh Dhody <ikjotsd@gmail.com>
    
    * Remove unused import from task_schedule_test.dart
    
    Signed-off-by: Ikjot Singh Dhody <ikjotsd@gmail.com>
    
    ---------
    
    Signed-off-by: Ikjot Singh Dhody <ikjotsd@gmail.com>

--- test/widget_tests/after_auth_screens/tasks/edit_task_page_test.dart
+++ test/widget_tests/after_auth_screens/tasks/edit_task_page_test.dart
@@ -4,6 +4,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:talawa/constants/custom_theme.dart';
 import 'package:talawa/models/events/event_model.dart';
 import 'package:talawa/models/task/task_model.dart';
 import 'package:talawa/models/user/user_info.dart';
@@ -15,14 +16,19 @@ import 'package:talawa/widgets/task_form.dart';
 import '../../../helpers/test_helpers.dart';
 
 Widget createEditTaskPage() => MaterialApp(
+      locale: const Locale('en'),
       localizationsDelegates: [
-        AppLocalizations.delegate,
+        const AppLocalizationsDelegate(isTest: true),
         GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
       ],
+      themeMode: ThemeMode.light,
+      theme: TalawaTheme.lightTheme,
       home: EditTaskPage(
         task: Task(
           id: '1',
           title: 'title',
+          description: 'description',
           event: Event(),
           creator: User(),
           createdAt: '123456',
@@ -31,14 +37,31 @@ Widget createEditTaskPage() => MaterialApp(
     );
 
 void main() {
-  setUp(() {
+  setUpAll(() {
     SizeConfig().test();
     registerServices();
     registerViewModels();
   });
-  testWidgets('EditTaskPage UI Test', (tester) async {
-    await tester.pumpWidget(createEditTaskPage());
-    await tester.pump();
-    expect(find.byType(TaskForm), findsOneWidget);
+
+  group("Functionality tests", () {
+    testWidgets('Check onSave functionality for TaskForm', (tester) async {
+      await tester.pumpWidget(createEditTaskPage());
+      await tester.pump();
+      await tester.tap(find.byType(TextButton));
+      expect(find.byType(TaskForm), findsOneWidget);
+    });
+  });
+
+  group("UI Tests", () {
+    testWidgets('EditTaskPage UI Test', (tester) async {
+      await tester.pumpWidget(createEditTaskPage());
+      await tester.pump();
+      expect(find.byType(TaskForm), findsOneWidget);
+    });
+  });
+
+  tearDownAll(() {
+    unregisterServices();
+    unregisterViewModels();
   });
 }
