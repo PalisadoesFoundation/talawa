726e570d1bd5a8b4c043afbea15a0de2a608a745
khaled <khaedmo@gmail.com>
2023-03-20 00:58:23 +0200

    FixCroppingImageBug

--- lib/services/third_party_service/multi_media_pick_service.dart
+++ lib/services/third_party_service/multi_media_pick_service.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: talawa_api_doc
 // ignore_for_file: talawa_good_doc_comments
 // ignore_for_file: talawa_lint_rules
+// ignore_for_file: talawa_lint
 
 /* This is an abstraction service for picking up Photos/videos
 Library used: image_picker (https://pub.dev/packages/image_picker)
