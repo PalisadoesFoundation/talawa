2ad2bdbc6f7b3f5a72f46cf33a4d15f7ce8453fb
Ayush0Chaudhary <ayush0000ayush@gmail.com>
2023-06-06 04:45:06 +0530

    feat: Add viewmodel logic for waiting screen.

--- lib/view_model/after_auth_view_models/send_access_request/send_access_request_view_model.dart
+++ lib/view_model/after_auth_view_models/send_access_request/send_access_request_view_model.dart
@@ -1,1 +1,26 @@
+import 'package:talawa/enums/enums.dart';
+import 'package:talawa/locator.dart';
+import 'package:talawa/services/database_mutation_functions.dart';
+import 'package:talawa/utils/queries.dart';
+import 'package:talawa/view_model/base_view_model.dart';
 
+class SendAccessRequestViewModel extends BaseModel {
+  late DataBaseMutationFunctions _dbFunctions;
+
+  void initialise() {
+    _dbFunctions = locator<DataBaseMutationFunctions>();
+  }
+
+  Future<void> sendMemberShipRequest() async {
+    try {
+      //TODO: Add the variable when the backend is ready
+
+      await _dbFunctions.gqlAuthMutation(
+        Queries().sendMembershipRequest("orgId"),
+      );
+    } catch (e) {
+      navigationService.showTalawaErrorSnackBar(
+          e.toString(), MessageType.error);
+    }
+  }
+}
