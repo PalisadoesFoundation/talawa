2c648667ea5181fbc9586d83f169d51b41967327
Ayush0Chaudhary <ayush0000ayush@gmail.com>
2023-06-29 16:13:21 +0530

    added documentation

--- lib/constants/routing_constants.dart
+++ lib/constants/routing_constants.dart
@@ -2,99 +2,99 @@
 ///These can be used to make push screen call via Routes.splashScreen to access these string
 
 class Routes {
-  /// static variable
+  /// static variables.
   static const String demoPageViewRoute = "/demoPageViewRoute";
 
-  /// static variable
+  /// static variables.
   static const String splashScreen = "/";
 
-  /// static variable
+  /// static variables.
   static const String languageSelectionRoute = "/selectLang";
 
-  /// static variable
+  /// static variables.
   static const String setUrlScreen = "/setUrl";
 
-  /// static variable
+  /// static variables.
   static const String loginScreen = "/login";
 
-  /// static variable
+  /// static variables.
   static const String selectOrgScreen = "/selectOrg";
 
-  /// static variable
+  /// static variables.
   static const String signupDetailScreen = "/signupDetails";
 
-  /// static variable
+  /// static variables.
   static const String waitingScreen = "/waiting";
 
-  /// static variable
+  /// static variables.
   static const String recoverScreen = "/recover";
 
-  /// static variable
+  /// static variables.
   static const String updateScreen = "/update";
 
-  /// static variable
+  /// static variables.
   static const String homeScreen = "/homeScreen";
 
-  /// static variable
+  /// static variables.
   static const String mainScreen = "/mainScreen";
 
-  /// static variable
+  /// static variables.
   static const String progressDialog = "/progress";
 
-  /// static variable
+  /// static variables.
   static const String individualPost = "/individualPost";
 
-  /// static variable
+  /// static variables.
   static const String pinnedPostPage = "/pinnedPostPage";
 
-  /// static variable
+  /// static variables.
   static const String exploreEventsScreen = "/exploreEvents";
 
-  /// static variable
+  /// static variables.
   static const String eventInfoPage = "/eventInfo";
 
-  /// static variable
+  /// static variables.
   static const String createEventPage = "/createEventPage";
 
-  /// static variable
+  /// static variables.
   static const String profilePage = "/profilePage";
 
-  /// static variable
+  /// static variables.
   static const String editProfilePage = "/editProfilePage";
 
-  /// static variable
+  /// static variables.
   static const String joinOrg = '/joinOrg';
 
-  /// static variable
+  /// static variables.
   static const String requestAccess = '/requestAccess';
 
-  /// static variable
+  /// static variables.
   static const String editEventPage = "/editEventPage";
 
-  /// static variable
+  /// static variables.
   static const String appSettings = '/appSettingsPage';
 
-  /// static variable
+  /// static variables.
   static const String chatMessageScreen = '/chatMessageScreen';
 
-  /// static variable
+  /// static variables.
   static const String mapScreen = '/mapScreen';
 
-  /// static variable
+  /// static variables.
   static const String calendar = '/calendar';
 
-  /// static variable
+  /// static variables.
   static const String eventTasks = '/event_tasks';
 
-  /// static variable
+  /// static variables.
   static const String userTasks = '/user_tasks';
 
-  /// static variable
+  /// static variables.
   static const String addTask = '/addTask';
 
-  /// static variable
+  /// static variables.
   static const String editTask = '/editTask';
 
-  /// static variable
+  /// static variables.
   static const String selectContact = '/selectContact';
 }
