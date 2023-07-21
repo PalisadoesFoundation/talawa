3390a61769b9df504456e628870e75cd6f1b7f02
Ravi Dev Pandey <pandeyravidev@gmail.com>
2023-07-22 00:09:35 +0530

    add doc for Encryptor class

--- lib/utils/encryptor.dart
+++ lib/utils/encryptor.dart
@@ -1,9 +1,23 @@
 import 'dart:convert';
 import 'package:crypto/crypto.dart';
 
+/// Handles all of the encryption tasks in the codebase.
 class Encryptor {
+  /// A global switch to flag the encryption.
+  ///
+  /// Since adding encryption invalidates all of the previous
+  /// data, disabling it by default will help in keeping
+  /// other contributors working without any issue.
+  /// TODO: Remove this and make encryption default.
   static bool shouldEncrypt = false;
 
+  /// Encrypts a given string with SHA256 Encryption.
+  ///
+  /// **params**:
+  /// * `data`: The string data to encrypt
+  ///
+  /// **returns**:
+  /// * `String`: SHA256 encrypted data
   static String encryptString(String data) {
     if (!shouldEncrypt) return data;
     return sha256.convert(utf8.encode(data)).toString();
