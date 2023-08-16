d968fa672a79f828f876af390a879f9bad88c2d8
Ayush Chaudhary <95746190+Ayush0Chaudhary@users.noreply.github.com>
2023-08-16 23:04:48 +0530

    Tool tip to help with the password (#1969)
    * tooptip addition
    
    * changes
    
    * formatting
    
    * formatting
    
    * fromat
    
    * Merge conflict resolved
    
    * Increased the coverage

--- lib/views/pre_auth_screens/login.dart
+++ lib/views/pre_auth_screens/login.dart
@@ -1,7 +1,5 @@
-// ignore_for_file: talawa_api_doc
-// ignore_for_file: talawa_good_doc_comments
-
 import 'package:flutter/material.dart';
+import 'package:talawa/enums/enums.dart';
 import 'package:talawa/locator.dart';
 import 'package:talawa/services/size_config.dart';
 import 'package:talawa/utils/app_localization.dart';
@@ -137,8 +135,24 @@ class _LoginState extends State<Login> {
                     ),
                     //Option to recover the password if the user forgets his/her password.
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
+                        TextButton(
+                          onPressed: () {
+                            navigationService.showTalawaErrorSnackBar(
+                              'Your password must be at least 8 characters long, contain at least one numeric, one uppercase and one lowercase letters and one special character (@,#,\$,etc.)',
+                              MessageType.info,
+                            );
+                          },
+                          child: const Text(
+                            'Tip',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.green,
+                              fontFamily: 'open-sans',
+                            ),
+                          ),
+                        ),
                         TextButton(
                           onPressed: () {
                             model.emailFocus.unfocus();
