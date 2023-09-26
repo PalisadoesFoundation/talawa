73488b8bc7743411bc57f52a978c5f27f0768f49
Ayush0Chaudhary <ayush0000ayush@gmail.com>
2023-09-26 11:55:33 +0530

    default linting error

--- lib/apptheme.dart
+++ lib/apptheme.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+
 // import 'package:flutter_screenutil/flutter_screenutil.dart';
 /// This class will be use to uniform the apptheme of the app in future.
 class AppTheme {
