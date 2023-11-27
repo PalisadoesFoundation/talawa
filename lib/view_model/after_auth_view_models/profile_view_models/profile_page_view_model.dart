fb9a716e2663ac1e1c6c238ecbe90014927213a9
Ayush Raghuwanshi <ayushraghuvanshi03@gmail.com>
2023-11-28 00:58:49 +0530

    Revert: removal of qr code

--- lib/view_model/after_auth_view_models/profile_view_models/profile_page_view_model.dart
+++ lib/view_model/after_auth_view_models/profile_view_models/profile_page_view_model.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:hive/hive.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 import 'package:talawa/constants/constants.dart';
 import 'package:talawa/enums/enums.dart';
 import 'package:talawa/locator.dart';
@@ -12,6 +13,7 @@ import 'package:talawa/services/navigation_service.dart';
 import 'package:talawa/services/size_config.dart';
 import 'package:talawa/services/user_config.dart';
 import 'package:talawa/view_model/base_view_model.dart';
+import 'package:talawa/view_model/lang_view_model.dart';
 import 'package:talawa/widgets/custom_alert_dialog.dart';
 import 'package:talawa/widgets/custom_progress_dialog.dart';
 
@@ -23,7 +25,8 @@ class ProfilePageViewModel extends BaseModel {
   // Services
   final _userConfig = locator<UserConfig>();
   final _navigationService = locator<NavigationService>();
-  // final _appLanguageService = locator<AppLanguage>();
+  final _appLanguageService = locator<AppLanguage>();
+
   /// GlobalKey for scaffoldKey.
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
@@ -157,107 +160,112 @@ class ProfilePageViewModel extends BaseModel {
     );
   }
 
-  // / This function generates the organization invitation link in a Dialog Box.
-  // / Dialog box contains the QR-code of organization invite link and social media sharing options.
-  // void invite(BuildContext context) {
-  //   _appLanguageService.initialize();
-  //   // organization url
-  //   final String url =
-  //       'https://cyberwake.github.io/applink/invite?selectLang=${_appLanguageService.appLocal.languageCode}&setUrl=${GraphqlConfig.orgURI}&selectOrg=${userConfig.currentOrg.id!}';
-  //   // QR
-  //   final String qrData =
-  //       '${GraphqlConfig.orgURI}?orgid=${userConfig.currentOrg.id!}';
-  //   print(url);
-  //   print(qrData);
-  //   showModalBottomSheet(
-  //     context: context,
-  //     shape: const RoundedRectangleBorder(
-  //       borderRadius: BorderRadius.only(
-  //         topLeft: Radius.circular(30),
-  //         topRight: Radius.circular(30),
-  //       ),
-  //     ),
-  //     builder: (BuildContext context) {
-  //       return ClipRRect(
-  //         borderRadius: const BorderRadius.only(
-  //           topLeft: Radius.circular(30),
-  //           topRight: Radius.circular(30),
-  //         ),
-  //         child: Container(
-  //           height: MediaQuery.of(context).size.height * 0.75,
-  //           decoration: const BoxDecoration(
-  //             color: Colors.white,
-  //           ),
-  //           child: Column(
-  //             mainAxisSize: MainAxisSize.max,
-  //             mainAxisAlignment: MainAxisAlignment.center,
-  //             crossAxisAlignment: CrossAxisAlignment.center,
-  //             children: [
-  //               QrImage(
-  //                 data: qrData,
-  //                 version: QrVersions.auto,
-  //                 size: 200.0,
-  //                 foregroundColor: Colors.black,
-  //               ),
-  //               SizedBox(
-  //                 height: SizeConfig.screenHeight! * 0.08,
-  //               ),
-  //               Text(
-  //                 'Scan the QR to join ${userConfig.currentOrg.name}',
-  //                 style: const TextStyle(color: Colors.black),
-  //               ),
-  //               SizedBox(
-  //                 height: SizeConfig.screenHeight! * 0.02,
-  //               ),
-  //               Row(
-  //                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-  //                 mainAxisSize: MainAxisSize.min,
-  //                 children: [
-  //                   iconButton(
-  //                     const FaIcon(
-  //                       FontAwesomeIcons.twitter,
-  //                       size: 35,
-  //                       color: Color(0xFF1DA1F2),
-  //                     ),
-  //                     () async => SocialShare.shareTwitter('Join us', url: url),
-  //                   ),
-  //                   iconButton(
-  //                     CustomPaint(
-  //                       size: const Size(
-  //                         50,
-  //                         50 * 1.004,
-  //                       ), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
-  //                       painter: WhatsappLogo(),
-  //                     ),
-  //                     () async => SocialShare.shareWhatsapp(url),
-  //                   ),
-  //                   iconButton(
-  //                     CustomPaint(
-  //                       size: Size(
-  //                         45,
-  //                         (45 * 1).toDouble(),
-  //                       ), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
-  //                       painter: TelegramLogo(),
-  //                     ),
-  //                     () async => SocialShare.shareTelegram(url),
-  //                   ),
-  //                   iconButton(
-  //                     const FaIcon(
-  //                       FontAwesomeIcons.shareNodes,
-  //                       size: 30,
-  //                       color: Color(0xff40c351),
-  //                     ),
-  //                     () async => SocialShare.shareOptions(url),
-  //                   ),
-  //                 ],
-  //               ),
-  //             ],
-  //           ),
-  //         ),
-  //       );
-  //     },
-  //   );
-  // }
+  /// This Function creates a QR Code for latest release .
+  ///
+  /// **params**:
+  /// * `context`: Build Context
+  ///
+  /// **returns**:
+  ///   None
+  void invite(BuildContext context) {
+    _appLanguageService.initialize();
+    // organization url
+    // final String url =
+    //     'https://cyberwake.github.io/applink/invite?selectLang=${_appLanguageService.appLocal.languageCode}&setUrl=${GraphqlConfig.orgURI}&selectOrg=${userConfig.currentOrg.id!}';
+    // QR
+    const String qrData =
+        'https://github.com/PalisadoesFoundation/talawa/releases/download/automated/app-release.apk';
+    print(url);
+    print(qrData);
+    showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+      ),
+      builder: (BuildContext context) {
+        return ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.75,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                QrImage(
+                  data: qrData,
+                  version: QrVersions.auto,
+                  size: 200.0,
+                  foregroundColor: Colors.black,
+                ),
+                SizedBox(
+                  height: SizeConfig.screenHeight! * 0.08,
+                ),
+                Text(
+                  'Scan the QR to join ${userConfig.currentOrg.name}',
+                  style: const TextStyle(color: Colors.black),
+                ),
+                SizedBox(
+                  height: SizeConfig.screenHeight! * 0.02,
+                ),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                //   mainAxisSize: MainAxisSize.min,
+                //   children: [
+                //     iconButton(
+                //       const FaIcon(
+                //         FontAwesomeIcons.twitter,
+                //         size: 35,
+                //         color: Color(0xFF1DA1F2),
+                //       ),
+                //       () async => SocialShare.shareTwitter('Join us', url: url),
+                //     ),
+                //     iconButton(
+                //       CustomPaint(
+                //         size: const Size(
+                //           50,
+                //           50 * 1.004,
+                //         ), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
+                //         painter: WhatsappLogo(),
+                //       ),
+                //       () async => SocialShare.shareWhatsapp(url),
+                //     ),
+                //     iconButton(
+                //       CustomPaint(
+                //         size: Size(
+                //           45,
+                //           (45 * 1).toDouble(),
+                //         ), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
+                //         painter: TelegramLogo(),
+                //       ),
+                //       () async => SocialShare.shareTelegram(url),
+                //     ),
+                //     iconButton(
+                //       const FaIcon(
+                //         FontAwesomeIcons.shareNodes,
+                //         size: 30,
+                //         color: Color(0xff40c351),
+                //       ),
+                //       () async => SocialShare.shareOptions(url),
+                //     ),
+                //   ],
+                // ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
 
   /// This widget returns the button for social media sharing option.
   ///
