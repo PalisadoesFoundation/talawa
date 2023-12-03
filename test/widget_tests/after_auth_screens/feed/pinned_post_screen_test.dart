1c62076d37bd958934c2536efc545df6adb42a1e
Nidhin Ninan <nidhinvninan29@gmail.com>
2023-12-03 22:14:18 +0530

    modified

--- test/widget_tests/after_auth_screens/feed/pinned_post_screen_test.dart
+++ test/widget_tests/after_auth_screens/feed/pinned_post_screen_test.dart
@@ -3,21 +3,21 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-//import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:talawa/models/organization/org_info.dart';
 import 'package:talawa/models/post/post_model.dart';
 import 'package:talawa/models/user/user_info.dart';
 import 'package:talawa/router.dart' as router;
 import 'package:talawa/services/navigation_service.dart';
-//import 'package:talawa/services/size_config.dart';
+import 'package:talawa/services/size_config.dart';
 import 'package:talawa/utils/app_localization.dart';
-//import 'package:talawa/views/pre_auth_screens/signup_details.dart';
+import 'package:talawa/views/pre_auth_screens/signup_details.dart';
 
 import '../../../helpers/test_locator.dart';
 
 Widget createApp() {
-  final Post pinnedPost =
-      Post(sId: '1', creator: User(firstName: 'test', lastName: 'test'));
+  final Post pinnedPost = 
+    Post(sId: '1', creator: User(firstName: 'test',lastName: 'test'));
   return MaterialApp(
     locale: const Locale('en'),
     localizationsDelegates: [
@@ -46,22 +46,22 @@ Widget createApp() {
   );
 }
 
-//void main() {
-//  SizeConfig().test();
-//
-//  testSetupLocator();
+void main() {
+  SizeConfig().test();
 
-//  group('description', () {
-//    testWidgets('Check if SignUp screen shows up', (tester) async {
-//      await tester.pumpWidget(createApp());
-//      await tester.pump();
-//
-//      expect(find.byType(SignUpDetails), findsNothing);
-//
-//      await tester.tap(find.textContaining('Ayush'));
-//      await tester.pumpAndSettle();
+  testSetupLocator();
 
-// expect(find.byType(SignUpDetails), findsOneWidget);
-//    });
-//  });
-//}
+  //group('description', () {
+  //  testWidgets('Check if SignUp screen shows up', (tester) async {
+  //    await tester.pumpWidget(createApp());
+  //    await tester.pump();
+
+  //    expect(find.byType(SignUpDetails), findsNothing);
+
+  //    await tester.tap(find.textContaining('Ayush'));
+  //    await tester.pumpAndSettle();
+
+      // expect(find.byType(SignUpDetails), findsOneWidget);
+  //  });
+ // });
+}
