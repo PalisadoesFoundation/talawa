ce5a70abad68793f1e53c10dc0a9d58ec82f737e
Azad99-9 <shaikazad2003@gmail.com>
2023-12-08 23:25:45 +0530

    minor fixes

--- lib/models/mainscreen_navigation_args.dart
+++ lib/models/mainscreen_navigation_args.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: avoid_dynamic_calls
-
 /// This class creates a MainScreenArgs model.
 class MainScreenArgs {
   MainScreenArgs({
@@ -17,6 +15,16 @@ class MainScreenArgs {
   /// Determines if the application is in demo mode.
   final bool toggleDemoMode;
 
+  /// Overrides the equality operator to compare instances of the MainScreenArgs class.
+  ///
+  /// Checks whether the [other] object is of the same type and compares its properties:
+  /// If all properties match, returns `true`; otherwise, returns `false`.
+  ///
+  /// **params**:
+  /// * `other`: The object to compare against this MainScreenArgs instance.
+  ///
+  /// **returns**:
+  /// * `bool`: Returns `true` if the properties of both instances match; otherwise, `false`.
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
@@ -25,6 +33,9 @@ class MainScreenArgs {
           other.mainScreenIndex == mainScreenIndex &&
           other.toggleDemoMode == toggleDemoMode;
 
+  /// Overrides the hashCode getter to generate a hash code based on the properties of the MainScreenArgs instance.
+  ///
+  /// Returns an integer value representing the combined hash codes.
   @override
   int get hashCode =>
       fromSignUp.hashCode ^ mainScreenIndex.hashCode ^ toggleDemoMode.hashCode;
