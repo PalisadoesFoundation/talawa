5a9e133412a5e663df4cb9281d467dbca76428ff
Azad99-9 <shaikazad2003@gmail.com>
2023-12-11 16:14:20 +0530

    added tests to userConfig Service

--- test/service_tests/user_config_test.dart
+++ test/service_tests/user_config_test.dart
@@ -18,23 +18,20 @@ import '../helpers/test_locator.dart';
 
 class MockBox<T> extends Mock implements Box<T> {}
 
-class MockUser extends Mock implements User {
-  @override
-  void updateJoinedOrg(List<OrgInfo> orgList) {
-    // TODO: implement updateJoinedOrg
-  }
+// class MockUser extends Mock implements User {
+//   @override
+//   void updateJoinedOrg(List<OrgInfo> orgList) {
+//     // TODO: implement updateJoinedOrg
+//   }
 
-  @override
-  void updateMemberRequestOrg(List<OrgInfo> orgList) {
-    // TODO: implement updateMemberRequestOrg
-  }
-}
+//   @override
+//   void updateMemberRequestOrg(List<OrgInfo> orgList) {
+//     // TODO: implement updateMemberRequestOrg
+//   }
+// }
 
 void main() async {
-  setUp(() {
-    testSetupLocator();
-    registerServices();
-  });
+  testSetupLocator();
 
   final Directory dir = Directory('test/fixtures/core');
 
@@ -47,7 +44,40 @@ void main() async {
   final urlBox = await Hive.openBox('url');
   final orgBox = await Hive.openBox<OrgInfo>('currentOrg');
 
+  final mockUser = User(
+    adminFor: <OrgInfo>[
+      OrgInfo(id: 'org1', name: 'orga'),
+      OrgInfo(id: 'org2', name: 'orgb'),
+    ],
+    createdOrganizations: <OrgInfo>[
+      OrgInfo(id: 'org1', name: 'orga'),
+      OrgInfo(id: 'org2', name: 'orgb'),
+    ],
+    email: 'test@gmail.com',
+    firstName: 'user',
+    lastName: 'one',
+    image: 'sample.jpg',
+    joinedOrganizations: <OrgInfo>[
+      OrgInfo(id: 'org1', name: 'orga'),
+      OrgInfo(id: 'org2', name: 'orgb'),
+    ],
+    authToken: 'AuthToken',
+    refreshToken: 'Refreshtoken',
+    membershipRequests: <OrgInfo>[
+      OrgInfo(id: 'org1', name: 'orga'),
+      OrgInfo(id: 'org2', name: 'orgb'),
+    ],
+  );
+
+  final mockOrgDetails = <OrgInfo>[
+    OrgInfo(id: 'org3', name: 'orgc'),
+    OrgInfo(id: 'org4', name: 'orgd'),
+  ];
+
   group('Test UserConfig service', () {
+    setUpAll(() {
+      registerServices();
+    });
     test('Test for User log out.', () async {
       databaseFunctions.init();
 
@@ -63,7 +93,7 @@ void main() async {
         );
       });
 
-      when(navigationService.pop()).thenAnswer((realInvocation) async {});
+      when(navigationService.pop()).thenAnswer((_) async {});
       when(
         navigationService.pushDialog(
           const CustomProgressDialog(
@@ -88,6 +118,55 @@ void main() async {
 
       loggedOut = await UserConfig().userLogOut();
       expect(loggedOut, false);
+    });
+
+    test('Test for updateUserJoinedOrg', () async {
+      final model = UserConfig();
+      model.currentUser = mockUser;
+
+      await model.updateUserJoinedOrg(mockOrgDetails);
+
+      expect(mockUser.joinedOrganizations, mockOrgDetails);
+    });
+
+    test('Test for updateUserCreatedOrg', () async {
+      final model = UserConfig();
+      model.currentUser = mockUser;
+
+      await model.updateUserCreatedOrg(mockOrgDetails);
+
+      expect(mockUser.createdOrganizations, mockOrgDetails);
+    });
+
+    test('Test for updateUserMemberRequestOrg', () async {
+      final model = UserConfig();
+      model.currentUser = mockUser;
+      final expected = [...mockUser.membershipRequests!, ...mockOrgDetails];
+      await model.updateUserMemberRequestOrg(mockOrgDetails);
+      print(mockUser.membershipRequests);
+
+      expect(mockUser.membershipRequests, expected);
+    });
+
+    test('Test for updateUserAdminOrg', () async {
+      final model = UserConfig();
+      model.currentUser = mockUser;
+      
+      await model.updateUserAdminOrg(mockOrgDetails);
+
+      expect(mockUser.adminFor, mockOrgDetails);
+    });
+
+    test('Test for updateAccessToken', () async {
+      final model = UserConfig();
+      model.currentUser = mockUser;
+      const newAuthToken = 'newAccessToken';
+      const newRefreshToken = 'newRefreshToken';
+      
+      await model.updateAccessToken(accessToken: newAuthToken, refreshToken: newRefreshToken);
+
+      expect(mockUser.authToken, newAuthToken);
+      expect(mockUser.refreshToken, newRefreshToken);
     });
   });
 }
