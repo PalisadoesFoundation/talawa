4c6193696adb147242c386b8cc57c1a57b01dade
Parag Gupta <103507835+Dante291@users.noreply.github.com>
2023-12-14 17:54:27 +0530

    Enhancing UI for inviting (#2229)
    * Enhancing UI for inviting
    
    * fixing coverage and adding translation
    
    * fixing coverage and adding translation

--- lib/view_model/after_auth_view_models/profile_view_models/profile_page_view_model.dart
+++ lib/view_model/after_auth_view_models/profile_view_models/profile_page_view_model.dart
@@ -5,13 +5,16 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:hive/hive.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:talawa/constants/constants.dart';
+import 'package:talawa/custom_painters/talawa_logo.dart';
 import 'package:talawa/enums/enums.dart';
 import 'package:talawa/locator.dart';
 import 'package:talawa/models/organization/org_info.dart';
 import 'package:talawa/models/user/user_info.dart';
+import 'package:talawa/services/graphql_config.dart';
 import 'package:talawa/services/navigation_service.dart';
 import 'package:talawa/services/size_config.dart';
 import 'package:talawa/services/user_config.dart';
+import 'package:talawa/utils/app_localization.dart';
 import 'package:talawa/view_model/base_view_model.dart';
 import 'package:talawa/view_model/lang_view_model.dart';
 import 'package:talawa/widgets/custom_alert_dialog.dart';
@@ -88,6 +91,7 @@ class ProfilePageViewModel extends BaseModel {
     // push custom alert dialog with the confirmation message.
     navigationService.pushDialog(
       CustomAlertDialog(
+        key: const Key('customalertdialog'),
         reverse: true,
         dialogSubTitle: 'Are you sure you want to logout?',
         successText: 'Logout',
@@ -169,95 +173,60 @@ class ProfilePageViewModel extends BaseModel {
   ///   None
   void invite(BuildContext context) {
     _appLanguageService.initialize();
-    // organization url
-    // final String url =
-    //     'https://cyberwake.github.io/applink/invite?selectLang=${_appLanguageService.appLocal.languageCode}&setUrl=${GraphqlConfig.orgURI}&selectOrg=${userConfig.currentOrg.id!}';
-    // QR
-    const String qrData =
-        'https://github.com/PalisadoesFoundation/talawa/releases/download/automated/app-release.apk';
-    print(url);
-    print(qrData);
-    showModalBottomSheet(
+    final String qrData =
+        '${GraphqlConfig.orgURI}?orgid=${userConfig.currentOrg.id!}';
+
+    showDialog(
       context: context,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(30),
-          topRight: Radius.circular(30),
-        ),
-      ),
       builder: (BuildContext context) {
-        return ClipRRect(
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(30),
-            topRight: Radius.circular(30),
+        return Dialog(
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
           ),
           child: Container(
-            height: MediaQuery.of(context).size.height * 0.75,
-            decoration: const BoxDecoration(
-              color: Colors.white,
+            padding: const EdgeInsets.all(20),
+            constraints: BoxConstraints(
+              maxHeight: MediaQuery.of(context).size.height * 0.80,
             ),
             child: Column(
-              mainAxisSize: MainAxisSize.max,
+              mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
+                iconButton(
+                  CustomPaint(
+                    size: const Size(48, 48 * 1),
+                    painter: AppLogo(),
+                  ),
+                  () {},
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  '${userConfig.currentOrg.name}',
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+                const SizedBox(height: 20),
                 QrImageView(
                   data: qrData,
                   version: QrVersions.auto,
                   size: 200.0,
                 ),
-                SizedBox(
-                  height: SizeConfig.screenHeight! * 0.08,
-                ),
+                const SizedBox(height: 20),
                 Text(
-                  'Scan the QR to join ${userConfig.currentOrg.name}',
-                  style: const TextStyle(color: Colors.black),
+                  AppLocalizations.of(context)!.strictTranslate('JOIN'),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
                 ),
-                SizedBox(
-                  height: SizeConfig.screenHeight! * 0.02,
-                ),
-                // Row(
-                //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                //   mainAxisSize: MainAxisSize.min,
-                //   children: [
-                //     iconButton(
-                //       const FaIcon(
-                //         FontAwesomeIcons.twitter,
-                //         size: 35,
-                //         color: Color(0xFF1DA1F2),
-                //       ),
-                //       () async => SocialShare.shareTwitter('Join us', url: url),
-                //     ),
-                //     iconButton(
-                //       CustomPaint(
-                //         size: const Size(
-                //           50,
-                //           50 * 1.004,
-                //         ), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
-                //         painter: WhatsappLogo(),
-                //       ),
-                //       () async => SocialShare.shareWhatsapp(url),
-                //     ),
-                //     iconButton(
-                //       CustomPaint(
-                //         size: Size(
-                //           45,
-                //           (45 * 1).toDouble(),
-                //         ), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
-                //         painter: TelegramLogo(),
-                //       ),
-                //       () async => SocialShare.shareTelegram(url),
-                //     ),
-                //     iconButton(
-                //       const FaIcon(
-                //         FontAwesomeIcons.shareNodes,
-                //         size: 30,
-                //         color: Color(0xff40c351),
-                //       ),
-                //       () async => SocialShare.shareOptions(url),
-                //     ),
-                //   ],
-                // ),
+                const SizedBox(height: 30),
               ],
             ),
           ),
@@ -278,6 +247,7 @@ class ProfilePageViewModel extends BaseModel {
     return Stack(
       children: [
         IconButton(
+          key: const Key('iconbtn1'),
           onPressed: () {
             print('tapped');
             onTap();
@@ -303,6 +273,7 @@ class ProfilePageViewModel extends BaseModel {
     void Function(void Function()) setter,
   ) {
     return InkWell(
+      key: const Key('dombtn1'),
       onTap: () {
         setter(() {
           donationAmount.text = amount;
