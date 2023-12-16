ff24cae99005999108b41f53fa2c9e996a5d359b
Manik Mehta <mehtamanik96@gmail.com>
2023-12-16 19:16:08 +0530

    Tests for encryptor.dart written (#2220)
    * Tests for encryptor.dart written
    
    * Linting completed
    
    * Code Formatting Done
    
    * Final Code Formatting Done

--- lib/utils/encryptor.dart
+++ lib/utils/encryptor.dart
@@ -1,12 +1,15 @@
 import 'dart:convert';
+import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:crypto/crypto.dart';
 import 'package:hive/hive.dart';
 import 'package:pointycastle/asymmetric/oaep.dart';
 import 'package:pointycastle/asymmetric/rsa.dart';
+import 'package:pointycastle/key_generators/rsa_key_generator.dart';
 import 'package:pointycastle/pointycastle.dart';
 import 'package:pointycastle/random/fortuna_random.dart';
+import 'package:talawa/models/asymetric_keys/asymetric_keys.dart';
 
 /// Handles all of the encryption tasks in the codebase.
 class Encryptor {
@@ -43,15 +46,17 @@ class Encryptor {
   /// * `AsymmetricKeyPair<PublicKey, PrivateKey>`: The generated
   /// public and private keys.
   AsymmetricKeyPair<PublicKey, PrivateKey> generateRSAKeyPair() {
-    final keyGen = RSAKeyGeneratorParameters(BigInt.parse('65537'), 2048, 64);
     final secureRandom = FortunaRandom();
-    final random = SecureRandom('Fortuna')
-      ..seed(secureRandom as CipherParameters);
-    final keyGenParams = ParametersWithRandom(keyGen, random);
-
-    final keyGenerator = KeyGenerator('RSA');
-    keyGenerator.init(keyGenParams);
-
+    final random = Random.secure();
+    final List<int> seeds = [];
+    for (int i = 0; i < 32; i++) {
+      seeds.add(random.nextInt(255));
+    }
+    secureRandom.seed(KeyParameter(Uint8List.fromList(seeds)));
+    final rsapars = RSAKeyGeneratorParameters(BigInt.from(65537), 2048, 64);
+    final params = ParametersWithRandom(rsapars, secureRandom);
+    final keyGenerator = RSAKeyGenerator();
+    keyGenerator.init(params);
     return keyGenerator.generateKeyPair();
   }
 
@@ -66,13 +71,12 @@ class Encryptor {
   /// * `Future<void>`: None
   Future<void> saveKeyPair(
     AsymmetricKeyPair<PublicKey, PrivateKey> keyPair,
+    HiveInterface hiveInterface,
   ) async {
     // TODO: Implement secure storage here
-
-    await Hive.openBox<AsymmetricKeyPair<PublicKey, PrivateKey>>('user_keys');
-    final keysBox = await Hive.openBox('user_keys');
-
-    keysBox.put('key_pair', keyPair);
+    final Box<AsymetricKeys> keysBox =
+        await hiveInterface.openBox<AsymetricKeys>('user_keys');
+    keysBox.put('key_pair', AsymetricKeys(keyPair: keyPair));
   }
 
   /// Loads secret keys from the Hive db.
@@ -83,12 +87,11 @@ class Encryptor {
   /// **returns**:
   /// * `Future<AsymmetricKeyPair<PublicKey, PrivateKey>>`: The public and
   /// private key pair
-  Future<AsymmetricKeyPair<PublicKey, PrivateKey>> loadKeyPair() async {
-    await Hive.openBox<AsymmetricKeyPair<PublicKey, PrivateKey>>('user_keys');
-    final keysBox = await Hive.openBox('user_keys');
-
-    return (await keysBox.get('key_pair'))
-        as AsymmetricKeyPair<PublicKey, PrivateKey>;
+  Future<AsymmetricKeyPair<PublicKey, PrivateKey>> loadKeyPair(
+    HiveInterface hive,
+  ) async {
+    final keysBox = await hive.openBox<AsymetricKeys>('user_keys');
+    return keysBox.get('key_pair')!.keyPair;
   }
 
   /// Encrypts the given string data with Recipient's Public Key.
@@ -136,15 +139,21 @@ class Encryptor {
   /// **returns**:
   /// * `Future<void>`: None
   /// TODO: Use this somewhere
-  Future<void> receiveMessage(Map<String, dynamic> message) async {
-    final privateKey = (await loadKeyPair()).privateKey;
+  Future<void> receiveMessage(
+    Map<String, dynamic> message,
+    HiveInterface hive,
+  ) async {
+    try {
+      final encryptedMessage = message['encryptedMessage'] as String;
+      final privateKey = (await loadKeyPair(hive)).privateKey;
+      final decryptedMessage = assymetricDecryptString(
+        encryptedMessage,
+        privateKey as RSAPrivateKey,
+      );
 
-    final encryptedMessage = message['encryptedMessage'] as String;
-    final decryptedMessage = assymetricDecryptString(
-      encryptedMessage,
-      privateKey as RSAPrivateKey,
-    );
-
-    print('Decrypted Message: $decryptedMessage');
+      print('Decrypted Message: $decryptedMessage');
+    } catch (e) {
+      throw Exception('Error');
+    }
   }
 }
