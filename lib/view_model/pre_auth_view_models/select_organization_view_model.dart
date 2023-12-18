4b2ddd92cc0905623f6c23fd2e865a8973bb6b7c
Azad99-9 <shaikazad2003@gmail.com>
2023-12-18 18:28:30 +0530

    resolved conflicts

--- lib/view_model/pre_auth_view_models/select_organization_view_model.dart
+++ lib/view_model/pre_auth_view_models/select_organization_view_model.dart
@@ -259,9 +259,9 @@ class SelectOrganizationViewModel extends BaseModel {
           return {
             'organizationsConnection':
                 (existingOrganizations!["organizationsConnection"]
-                        as List<Map<String, dynamic>>) +
+                        as List<dynamic>) +
                     (newOrganizations!['organizationsConnection']
-                        as List<Map<String, dynamic>>),
+                        as List<dynamic>),
           };
         },
       ),
