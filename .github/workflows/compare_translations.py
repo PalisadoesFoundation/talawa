198fd6d6d205928ec88eec7c4e44b072bd689b5d
Ankit Varshney <lyfflame45@gmail.com>
2023-12-28 16:34:23 +0530

    made the requested changes

--- .github/workflows/compare_translations.py
+++ .github/workflows/compare_translations.py
@@ -1,10 +1,11 @@
-"""Script to encourage more efficient coding practices.
+"""
+Script to encourage more efficient coding practices.
 
 Methodology:
 
     Utility for comparing translations between default and other languages.
 
-    This module defines a function to compare two translations (default and another language)
+    This module defines a function to compare two translations
     and print any missing keys in the other language's translation.
 
 Functions:
@@ -15,14 +16,12 @@ Functions:
         Load the default translation and compare it with other translations.
 
 Usage:
-    This script can be executed to check and print missing translations in other languages
-    based on the default English translation.
+    This script can be executed to check and print missing
+    translations in other languages based on the default English translation.
 
 Example:
     python compare_translations.py
-
 NOTE:
-
     This script complies with our python3 coding and documentation standards
     and should be used as a reference guide. It complies with:
 
@@ -31,62 +30,78 @@ NOTE:
         3) Pycodestyle
         4) Flake8
 
-    Run these commands from the CLI to ensure the code is compliant for all
-    your pull requests.
-
 """
+# standard imports
 import json
+import os
+import sys
 
 
-def compare_translations(default_translation, other_translation, language):
-    """Compare two translations and print missing keys
+def compare_translations(default_translation, other_translation):
+    """Compare two translations and print missing keys.
 
     Args:
         default_translation: The default translation
         other_translation: The other translation
-        language: The language of the other translation
 
 
     Returns:
-        None
-
-    Prints:
-        missing_translations: The missing translations
+        missing_translations: List of missing translations
 
 
     """
     missing_translations = []
 
-    for key in default_translation.items():
+    for key in default_translation:
         if key not in other_translation:
             missing_translations.append(key)
 
-    if missing_translations:
-        print(f"Missing translations in {language}:")
-        for key in missing_translations:
-            print(f"  {key}")
-        print("\n")
-    else:
-        print(f"All translations present in {language}.")
+    return missing_translations
+
+
+def load_translation(filepath):
+    """Load translation from a file.
+
+    Args:
+        filepath: Path to the translation file
+
+    Returns:
+        translation: Loaded translation
+
+
+    """
+    with open(filepath, "r", encoding="utf-8") as file:
+        translation = json.load(file)
+    return translation
 
 
 def check_translations():
-    """Load default translation"""
-    with open("lang/en.json", "r", encoding="utf-8") as default_file:
-        default_translation = json.load(default_file)
+    """Load default translation and compare with other translations."""
+    default_translation = load_translation("lang/en.json")
+    translations_dir = "lang"
+    translations = os.listdir(translations_dir)
+    translations.remove("en.json")  # Exclude default translation
 
-    # Load all other translations
-    languages = ["en", "es", "fr", "de", "hi", "ja", "pt", "zh"]
-    for lang in languages:
-        if lang == "en":
-            continue
-
-        with open(f"lang/{lang}.json", "r", encoding="utf-8") as other_file:
-            other_translation = json.load(other_file)
+    for translation_file in translations:
+        translation_path = os.path.join(translations_dir, translation_file)
+        other_translation = load_translation(translation_path)
 
         # Compare translations
-        compare_translations(default_translation, other_translation, lang)
+        missing_translations = compare_translations(
+            default_translation, other_translation
+        )
+
+        # Print missing translations
+        if missing_translations:
+            print(f"Missing translations in {translation_file[:-5]}:")
+            for key in missing_translations:
+                print(f"  {key}")
+            print("\n")
+            sys.exit(1)  # Exit with an error status code
+        else:
+            print(f"All translations present in {translation_file[:-5]}.")
 
 
 if __name__ == "__main__":
     check_translations()
+    sys.exit(0)  # Exit with a success status code
