733444a8740449c11c92fbf390a4043d37586afe
Parag Gupta <103507835+Dante291@users.noreply.github.com>
2024-01-07 01:05:12 +0530

    Refactor: Decouple ViewModel by Moving UI Logic to View (#2306)
    * Refactor: Decouple ViewModel by Moving UI Logic to View
    
    * refactoring app_settings_view_model and app_seetings_page
    
    * writing test for missing lines

--- lib/view_model/after_auth_view_models/settings_view_models/app_setting_view_model.dart
+++ lib/view_model/after_auth_view_models/settings_view_models/app_setting_view_model.dart
@@ -1,18 +1,11 @@
-import 'package:flutter/material.dart';
-import 'package:talawa/enums/enums.dart';
 import 'package:talawa/locator.dart';
-import 'package:talawa/services/navigation_service.dart';
 import 'package:talawa/view_model/base_view_model.dart';
-import 'package:talawa/widgets/custom_alert_dialog.dart';
-import 'package:talawa/widgets/talawa_error_dialog.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 /// ViewModel for the App Settings functionality.
 ///
 /// This ViewModel handles the logic and data for the application settings.
 class AppSettingViewModel extends BaseModel {
-  // Services
-  final _navigationService = locator<NavigationService>();
   // final _appLanguageService = locator<AppLanguage>();
 
   /// This method destroys the user's session or sign out the user from app, The function asks for the confimation in Custom Alert Dialog.
@@ -22,9 +15,9 @@ class AppSettingViewModel extends BaseModel {
   ///
   /// **returns**:
   /// * `Future<void>`: Resolves when user logout
-  Future<void> logout(BuildContext context) async {
+  Future<void> logout() async {
     // push custom alert dialog with the confirmation message.
-    _navigationService.pushDialog(logoutDialog());
+    await userConfig.userLogOut();
   }
 
   /// Launches a website using the provided URL.
@@ -36,41 +29,4 @@ class AppSettingViewModel extends BaseModel {
   /// * `Future<bool>`: A [Future] that resolves to a [bool] value indicating
   /// whether the website launch was successful.
   Future<bool> launchWebsite(String url) async => await launchUrlString(url);
-
-  /// Creates a custom alert dialog for the logout confirmation.
-  ///
-  /// This dialog prompts the user with a confirmation message for logout.
-  /// The dialog provides options to logout or cancel the operation.
-  ///
-  /// **params**:
-  ///   None
-  ///
-  /// **returns**:
-  /// * `Widget`: A [Widget] representing the custom logout confirmation dialog.
-  Widget logoutDialog() {
-    return CustomAlertDialog(
-      reverse: true,
-      dialogSubTitle: 'Are you sure you want to logout?',
-      successText: 'Logout',
-      success: () async {
-        await userConfig.userLogOut();
-        navigationService.pop();
-        if (userConfig.loggedIn) {
-          navigationService.pushDialog(
-            const TalawaErrorDialog(
-              'Unable to logout, please try again.',
-              key: Key('TalawaError'),
-              messageType: MessageType.error,
-            ),
-          );
-        } else {
-          navigationService.removeAllAndPush(
-            '/selectLang',
-            '/',
-            arguments: '0',
-          );
-        }
-      },
-    );
-  }
 }
