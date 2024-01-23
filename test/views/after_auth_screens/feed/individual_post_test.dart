655b7c65eb21c69fee7c89db37801bb3d2f8050f
Dante291 <gparag222@gmail.com>
2024-01-23 11:02:48 +0530

    fixing coverage

--- test/views/after_auth_screens/feed/individual_post_test.dart
+++ test/views/after_auth_screens/feed/individual_post_test.dart
@@ -237,6 +237,43 @@ void main() {
       expect(find.byType(IndividualPageLikeSection), findsOneWidget);
       expect(find.byType(IndividualPostCommentSection), findsOneWidget);
     });
+    testWidgets("Checking if state changes when text is cleared",
+        (WidgetTester tester) async {
+      await tester.pumpWidget(createIndividualPostViewWidget(post));
+      await tester.pumpAndSettle();
+
+      final textFieldFinder = find.byKey(const Key('indi_post_tf_key'));
+      final textButtonFinder = find.byKey(const Key('sendButton'));
+      expect(tester.widget<TextButton>(textButtonFinder).enabled, isFalse);
+
+      // Clear the text field
+      await tester.enterText(textFieldFinder, '');
+      await tester.pumpAndSettle();
+      // The button should be disabled after clearing the text
+      expect(tester.widget<TextButton>(textButtonFinder).enabled, isFalse);
+
+      // Enter non-empty text
+      await tester.enterText(textFieldFinder, 'Test Comment');
+      await tester.pumpAndSettle();
+      // The button should now be enabled
+      expect(tester.widget<TextButton>(textButtonFinder).enabled, isTrue);
+
+      // Verify that the text field is cleared
+      expect(find.text('Test Comment'), findsOneWidget);
+
+      // Testing if state changes back
+      await tester.enterText(textFieldFinder, '');
+      await tester.pumpAndSettle();
+      expect(tester.widget<TextButton>(textButtonFinder).enabled, isFalse);
+
+      //  Tap the send button
+      await tester.tap(textButtonFinder);
+      await tester.pumpAndSettle();
+
+      expect(find.byType(NewsPost), findsOneWidget);
+      expect(find.byType(IndividualPageLikeSection), findsOneWidget);
+      expect(find.byType(IndividualPostCommentSection), findsOneWidget);
+    });
   });
 
   group("Test for likedUserCircleAvatar", () {
