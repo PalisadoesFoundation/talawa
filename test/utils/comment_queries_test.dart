c812541648f5d3c2953f50ed26a6b85768b7a192
imshivam-gupta <shivamggupta2604@gmail.com>
2024-01-28 11:01:23 +0530

    Fixed Comment

--- test/utils/comment_queries_test.dart
+++ test/utils/comment_queries_test.dart
@@ -1,6 +1,3 @@
-// ignore_for_file: talawa_api_doc
-// ignore_for_file: talawa_good_doc_comments
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:talawa/utils/comment_queries.dart';
 
@@ -8,18 +5,21 @@ void main() {
   group("Tests for comment_queries.dart", () {
     test("Check if getPostsComments works correctly", () {
       const data = """
-    query{
-      commentsByPost(id: "abc"){
-        _id
-        text
-        createdAt
+     query {
+        post(id: "abc")
+        {  _id,
+          comments{
+             _id,
+            text,
+             createdAt
         creator{
           firstName
           lastName
         }
+          }
+        }
       }
-    }
-    """;
+""";
       final fnData = CommentQueries().getPostsComments("abc");
       expect(fnData, data);
     });
