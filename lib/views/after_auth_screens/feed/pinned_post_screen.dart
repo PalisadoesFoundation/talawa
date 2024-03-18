c539e2dddc298302037ef45070308020f2b892cd
Marri Sai Soorya <96968484+M-SAI-SOORYA@users.noreply.github.com>
2024-03-18 10:49:45 +0530

    Fixed in the loading indicator in the organization feed
    Fixed in the loading indicator in the organization feed where i adjusted UI of circularprogressindicator and adjusted the aspect ratio

--- lib/views/after_auth_screens/feed/pinned_post_screen.dart
+++ lib/views/after_auth_screens/feed/pinned_post_screen.dart
@@ -62,7 +62,21 @@ class _PinnedPostScreenState extends State<PinnedPostScreen> {
               cacheManager: widget.cacheManager,
               imageUrl: widget.post['imageUrl']!,
               errorWidget: (context, url, error) {
-                return const CircularProgressIndicator();
+                return Center(
+                    child: Container(
+                      width: SizeConfig.safeBlockHorizontal! * 16,
+                      // Adjust the width to change the size of the circular indicator
+                      height: SizeConfig.safeBlockVertical! * 8,
+                      // Adjust the height to change the size of the circular indicator
+                      child: AspectRatio(
+                        aspectRatio: 1.0,
+                        // Maintain a 1:1 aspect ratio for a circular shape
+                        child: CircularProgressIndicator(
+                          strokeWidth: 4.0,
+                        ),
+                      ),
+                    ),
+                  );
               },
               height: SizeConfig.screenHeight! * .75,
               fit: BoxFit.cover,
