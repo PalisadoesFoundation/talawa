2d84fde424c0f4a214cca4918eb932b545574b9f
Peter Harrison <peter@colovore.com>
2024-06-02 08:33:44 -0700

    Deleted all files in the main branch in anticipation of merging develop into main cleanly

--- test/flutter_test_config.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:async';
-
-import 'package:flutter_test/flutter_test.dart';
-
-Future<void> testExecutable(FutureOr<void> Function() testMain) async {
-  WidgetController.hitTestWarningShouldBeFatal = true;
-  await testMain();
-}
