1974403349930fccaf0a3cb11ffac5e4bd79f7e0
Dante291 <gparag222@gmail.com>
2024-07-14 16:55:46 +0530

    adding tests

--- test/views/after_auth_screens/events/create_event_page_test.dart
+++ test/views/after_auth_screens/events/create_event_page_test.dart
@@ -7,8 +7,6 @@ import 'package:intl/intl.dart';
 import 'package:mockito/mockito.dart';
 import 'package:mocktail_image_network/mocktail_image_network.dart';
 import 'package:talawa/constants/custom_theme.dart';
-import 'package:talawa/models/events/event_venue.dart';
-import 'package:talawa/models/organization/org_info.dart';
 import 'package:talawa/router.dart' as router;
 import 'package:talawa/services/size_config.dart';
 import 'package:talawa/services/third_party_service/multi_media_pick_service.dart';
@@ -19,7 +17,6 @@ import 'package:talawa/views/after_auth_screens/events/create_event_page.dart';
 import 'package:talawa/views/after_auth_screens/events/venue_bottom_sheet.dart';
 import 'package:talawa/views/base_view.dart';
 import 'package:talawa/widgets/recurrence_dialog.dart';
-import 'package:talawa/widgets/venue_card.dart';
 
 import '../../../helpers/test_helpers.dart';
 import '../../../helpers/test_locator.dart';
