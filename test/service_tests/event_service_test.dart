26f8722affe1c859c3bbb8aa27c9b14ce97a5a6d
Dante291 <gparag222@gmail.com>
2024-09-20 22:41:21 +0530

    fixing codebase

--- test/service_tests/event_service_test.dart
+++ test/service_tests/event_service_test.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_dynamic_calls
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
