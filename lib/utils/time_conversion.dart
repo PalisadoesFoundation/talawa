77c54f7d42668e99a59db2340e7810fa7263bd31
Pranshu Gupta <pranshgupta54@gmail.com>
2024-10-15 01:26:31 +0530

    Added try-catch

--- lib/utils/time_conversion.dart
+++ lib/utils/time_conversion.dart
@@ -19,12 +19,18 @@ String combineDateTime(String date, String time) {
 ///
 /// **returns**:
 /// * `Map<String, String>`: A map containing the separate date and time strings.
+/// * Returns an empty map if the input is invalid.
 Map<String, String> splitDateTimeUTC(String dateTimeStr) {
-  final DateTime dateTime = DateTime.parse(dateTimeStr);
-  return {
-    'date': DateFormat('yyyy-MM-dd').format(dateTime),
-    'time': DateFormat("HH:mm:ss.SSS'Z'").format(dateTime),
-  };
+  try {
+    final DateTime dateTime = DateTime.parse(dateTimeStr);
+    return {
+      'date': DateFormat('yyyy-MM-dd').format(dateTime),
+      'time': DateFormat("HH:mm:ss.SSS'Z'").format(dateTime),
+    };
+  } catch (e) {
+    print('Timezone Error parsing UTC date time: $e $dateTimeStr');
+    return {};
+  }
 }
 
 /// Splits the given local date and time string into separate date and time strings.
@@ -34,12 +40,18 @@ Map<String, String> splitDateTimeUTC(String dateTimeStr) {
 ///
 /// **returns**:
 /// * `Map<String, String>`: A map containing the separate date and time strings.
+/// * Returns an empty map if the input is invalid.
 Map<String, String> splitDateTimeLocal(String dateTimeStr) {
-  final DateTime dateTime = DateTime.parse(dateTimeStr);
-  return {
-    'date': DateFormat('yyyy-MM-dd').format(dateTime),
-    'time': DateFormat('HH:mm').format(dateTime),
-  };
+  try {
+    final DateTime dateTime = DateTime.parse(dateTimeStr);
+    return {
+      'date': DateFormat('yyyy-MM-dd').format(dateTime),
+      'time': DateFormat('HH:mm').format(dateTime),
+    };
+  } catch (e) {
+    print('Timezone Error parsing local date time: $e $dateTimeStr');
+    return {};
+  }
 }
 
 /// Converts the given UTC time to local time.
@@ -49,9 +61,15 @@ Map<String, String> splitDateTimeLocal(String dateTimeStr) {
 ///
 /// **returns**:
 /// * `String`: The converted local time string.
+/// * Returns an empty string if the input is invalid.
 String convertUTCToLocal(String utcTime) {
-  final DateTime dateTime = DateTime.parse(utcTime).toLocal();
-  return DateFormat('yyyy-MM-ddTHH:mm:ss.SSS').format(dateTime);
+  try {
+    final DateTime dateTime = DateTime.parse(utcTime).toLocal();
+    return DateFormat('yyyy-MM-ddTHH:mm:ss.SSS').format(dateTime);
+  } catch (e) {
+    print('Timezone Error converting UTC to local: $e $utcTime');
+    return '';
+  }
 }
 
 /// Converts the given local time to UTC time.
@@ -61,9 +79,15 @@ String convertUTCToLocal(String utcTime) {
 ///
 /// **returns**:
 /// * `String`: The converted UTC time string.
+/// * Returns an empty string if the input is invalid.
 String convertLocalToUTC(String localTime) {
-  final DateTime dateTime = DateTime.parse(localTime).toUtc();
-  return DateFormat("yyyy-MM-ddTHH:mm:ss.SSS'Z'").format(dateTime);
+  try {
+    final DateTime dateTime = DateTime.parse(localTime).toUtc();
+    return DateFormat("yyyy-MM-ddTHH:mm:ss.SSS'Z'").format(dateTime);
+  } catch (e) {
+    print('Timezone Error converting local to UTC: $e $localTime');
+    return '';
+  }
 }
 
 /// Traverses a nested map and converts date and time fields to the desired format.
