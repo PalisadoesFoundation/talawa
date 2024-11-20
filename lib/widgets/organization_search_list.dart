87e0b70a978027786f38bfa4987e63b23131559e
Arya Pratap Singh <notaryasingh@gmail.com>
2024-11-21 02:27:35 +0530

    Update organization_search_list.dart

--- lib/widgets/organization_search_list.dart
+++ lib/widgets/organization_search_list.dart
@@ -5,18 +5,17 @@ import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:talawa/enums/enums.dart';
 import 'package:talawa/locator.dart';
-import 'package:talawa/models/organization/org_info.dart';
 import 'package:talawa/services/size_config.dart';
 import 'package:talawa/utils/queries.dart';
 import 'package:talawa/view_model/pre_auth_view_models/select_organization_view_model.dart';
 import 'package:talawa/widgets/custom_list_tile.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
-/// This class returns the widget that shows all the matching orgs searched in the search bar.
+/// This widget displays a list of organizations searched via the search bar.
 class OrganizationSearchList extends StatefulWidget {
   const OrganizationSearchList({required this.model, super.key});
 
-  /// Model constructor for the selectOrganization widget.
+  /// Model constructor for the select organization widget.
   final SelectOrganizationViewModel model;
 
   @override
@@ -30,14 +29,78 @@ class _OrganizationSearchListState extends State<OrganizationSearchList> {
   @override
   void initState() {
     super.initState();
-    // Initialize refetch counter.
-    _refetchCount.value = 0;
+    _refetchCount.value = 0; // Initialize the refetch counter.
   }
 
   @override
   void dispose() {
     _refetchCount.dispose();
     super.dispose();
+  }
+
+  /// Builds a list tile for the organization list.
+  Widget _buildListTile(BuildContext context, int index) {
+    return CustomListTile(
+      index: index,
+      type: TileType.org,
+      orgInfo: widget.model.organizations[index],
+      onTapOrgInfo: widget.model.selectOrg,
+      key: Key('orgTile_${widget.model.organizations[index].id}'),
+    );
+  }
+
+  /// Builds a visibility tile to handle fetching more data when scrolled to the end.
+  Widget _buildVisibilityTile(
+    BuildContext context,
+    int index,
+    Future<QueryResult> Function(FetchMoreOptions)? fetchMore,
+  ) {
+    return VisibilityDetector(
+      key: Key('orgTile_${widget.model.organizations[index].id}'),
+      onVisibilityChanged: (VisibilityInfo info) {
+        if (fetchMore != null) {
+          widget.model.fetchMoreHelper(fetchMore, widget.model.organizations);
+        }
+      },
+      child: _buildListTile(context, index),
+    );
+  }
+
+  /// Builds the main list view with organization tiles.
+  Widget _buildListView(
+    QueryResult result,
+    Future<QueryResult> Function(FetchMoreOptions)? fetchMore,
+  ) {
+    return ListView.separated(
+      controller: widget.model.controller,
+      padding: EdgeInsets.zero,
+      shrinkWrap: true,
+      itemCount: result.isLoading
+          ? widget.model.organizations.length + 1
+          : widget.model.organizations.length,
+      itemBuilder: (BuildContext context, int index) {
+        if (index == widget.model.organizations.length) {
+          return ListTile(
+            title: Center(
+              child: CupertinoActivityIndicator(
+                radius: SizeConfig.screenWidth! * 0.065,
+              ),
+            ),
+          );
+        } else if (index == widget.model.organizations.length - 3) {
+          return _buildVisibilityTile(context, index, fetchMore);
+        }
+        return _buildListTile(context, index);
+      },
+      separatorBuilder: (BuildContext context, int index) => Padding(
+        padding:
+            EdgeInsets.symmetric(horizontal: SizeConfig.screenWidth! * 0.2),
+        child: const Divider(
+          color: Color(0xFFE5E5E5),
+          thickness: 0.5,
+        ),
+      ),
+    );
   }
 
   @override
@@ -65,73 +128,17 @@ class _OrganizationSearchListState extends State<OrganizationSearchList> {
             } else {
               print('Max refetch attempts reached.');
             }
-          } else if (!result.isLoading) {
-            widget.model.organizations = OrgInfo().fromJsonToList(
-              result.data!['organizationsConnection'],
-            );
+            // } else if (!result.isLoading) {
+            //   widget.model.organizations = OrgInfo().fromJsonToList(
+            //     result.data!['organizationsConnection'],
+            //   );
           }
 
           return Scrollbar(
             thumbVisibility: true,
             interactive: true,
             controller: widget.model.controller,
-            child: ListView.separated(
-              controller: widget.model.controller,
-              padding: EdgeInsets.zero,
-              shrinkWrap: true,
-              itemCount: result.isLoading
-                  ? widget.model.organizations.length + 1
-                  : widget.model.organizations.length,
-              itemBuilder: (BuildContext context, int index) {
-                if (index == widget.model.organizations.length) {
-                  return ListTile(
-                    title: Center(
-                      child: CupertinoActivityIndicator(
-                        radius: SizeConfig.screenWidth! * 0.065,
-                      ),
-                    ),
-                  );
-                }
-                Widget buildListTile(int index) {
-                  return CustomListTile(
-                    index: index,
-                    type: TileType.org,
-                    orgInfo: widget.model.organizations[index],
-                    onTapOrgInfo: widget.model.selectOrg,
-                    key: Key('orgTile_${widget.model.organizations[index].id}'),
-                  );
-                }
-
-                if (index == widget.model.organizations.length - 3) {
-                  return VisibilityDetector(
-                    key: Key('orgTile_${widget.model.organizations[index].id}'),
-                    onVisibilityChanged: (VisibilityInfo info) {
-                      if (fetchMore != null) {
-                        widget.model.fetchMoreHelper(
-                          fetchMore,
-                          widget.model.organizations,
-                        );
-                      }
-                    },
-                    child: buildListTile(index),
-                  );
-                }
-
-                return buildListTile(index);
-              },
-              separatorBuilder: (BuildContext context, int index) {
-                return Padding(
-                  padding: EdgeInsets.only(
-                    left: SizeConfig.screenWidth! * 0.2,
-                    right: 12,
-                  ),
-                  child: const Divider(
-                    color: Color(0xFFE5E5E5),
-                    thickness: 0.5,
-                  ),
-                );
-              },
-            ),
+            child: _buildListView(result, fetchMore),
           );
         },
       ),
