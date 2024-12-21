db0d5975341c3a67247c1b47b310c708f384f2c9
Satyam Jha <135056339+may-tas@users.noreply.github.com>
2024-12-21 23:57:58 +0530

    test : added tests for `lib/view_model/widgets_view_models/progress_dialog_view_model.dart` (#2670)
    * added new tests cases for the ProgressDialogViewModel to enhance testing coverage
    
    * Changes Made as per the nitpick comments to enhance readability
    
    ---------
    
    Co-authored-by: Satyam Jha <satyamj210@gmail.com>

--- test/view_model_tests/progress_dialog_view_model_test.dart
+++ test/view_model_tests/progress_dialog_view_model_test.dart
@@ -10,17 +10,34 @@ import '../helpers/test_helpers.dart';
 void main() {
   group('ProgressDialogViewModelTest -', () {
     group('initialise -', () {
-      getAndRegisterConnectivity();
-      AppConnectivity.isOnline = true;
-
-      final model = ProgressDialogViewModel();
+      setUp(() {
+        getAndRegisterConnectivity();
+      });
 
       test(
           'When called and connectivity is present, connectivityPresent must be set to true',
           () async {
+        // Simulate online state
+        AppConnectivity.isOnline = true;
+
+        final model = ProgressDialogViewModel();
+
         await model.initialise();
 
         expect(model.connectivityPresent, true);
+      });
+
+      test(
+          'When called and connectivity is absent, connectivityPresent must be set to false',
+          () async {
+        // Simulate offline state
+        AppConnectivity.isOnline = false;
+
+        final model = ProgressDialogViewModel();
+
+        await model.initialise();
+
+        expect(model.connectivityPresent, false);
       });
     });
   });
