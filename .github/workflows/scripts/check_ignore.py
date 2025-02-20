e989ad1f1445d52e749207f839294d901ff5ae03
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2025-02-19 20:39:45 -0800

    Formatted python files

--- .github/workflows/scripts/check_ignore.py
+++ .github/workflows/scripts/check_ignore.py
@@ -1,9 +1,11 @@
-#! /usr/bin/python3.10
+#!/usr/bin/env python3
+"""Analyze dart files for custom lint rule suppression."""
 
 import argparse
 import os
 import subprocess
 import sys
+
 
 def _arg_parser_resolver():
     """Resolve the CLI arguments provided by the user.
@@ -20,19 +22,29 @@ def _arg_parser_resolver():
 
     # Getting merge branch name
     parser.add_argument(
-        '--merge_branch_name', type=str, required=True,
-        help='Name of the merging to branch')
+        "--merge_branch_name",
+        type=str,
+        required=True,
+        help="Name of the merging to branch",
+    )
 
     # Github repository
     parser.add_argument(
-        '--repository', type=str, required=True,
-        help='Name of the GitHub repository in the format "<USERNAME>/<REPO_NAME>"')
+        "--repository",
+        type=str,
+        required=True,
+        help='''\
+Name of the GitHub repository in the format "<USERNAME>/<REPO_NAME>"''',
+    )
 
     # Getting root directory of repository
     parser.add_argument(
-        '--directory', type=str, required=False,
+        "--directory",
+        type=str,
+        required=False,
         default=os.getcwd(),
-        help='The parent directory of files to analyze.')
+        help="The parent directory of files to analyze.",
+    )
 
     # Return parser
     return parser.parse_args()
@@ -62,41 +74,52 @@ def _filepaths_in_directories(directories):
 
 
 def _check_for_ignore_directive(filePath):
-    """Analyzes a given file and checks for presence of ignore directive corresponding
+    """Analyze a given file.
+
+    Also check for the presence of ignore directives corresponding
     to any of the custom lint rules.
 
     A valid ignore directive consists of exactly 3 parts.
 
-    - `//...`: where `...` represents that `/` can occur any number of times > 2.
-    - `ignore:` or `ignore_for_file:`: The part that distinguishes a normal comment from a
-        ignore directive.
-    - `name_of_lint_rule`: The rule that it is ignoring.
+    1. `//...`: where `...` represents that `/` can occur any number
+       of times > 2.
+    2. `ignore:` or `ignore_for_file:`: The part that distinguishes a
+        normal comment from a ignore directive.
+    3. `name_of_lint_rule`: The rule that it is ignoring.
 
-    These 3 parts may or may not have whitespace between them, but should strictly not have
-    any extra character. So
+    These 3 parts may or may not have whitespace between them, but should
+    strictly not have any extra character. So
 
-    - `//ignore:my_lint`, `/////ignore:my_lint`, `//ignore:     my_lint` etc are all valid
-        ignore directives.
-    - `// / ignore: my_lint`, `// sdf ignore: my_lint` etc are all invalid ones.
+    1. `//ignore:my_lint`, `/////ignore:my_lint`, `//ignore:     my_lint`
+       etc are all valid ignore directives.
+    2. `// / ignore: my_lint`, `// sdf ignore: my_lint` etc are all
+       invalid ones.
 
     Args:
-        Path of the file to analyze
+        filePath: Path of the file to analyze
 
     Returns:
         boolean: Whether the file contains ignore directive or not.
 
     """
-    
-    # Either it is a non-code file, or the file does not exist. This can happen when
-    # `develop-postgres` gets ahead of your branch and has some files which you don't have
-    
-    if (not filePath.startswith('lib') or not filePath.endswith('.dart') or not os.path.exists(filePath)):
+    # Either it is a non-code file, or the file does not exist.
+    # This can happen when `develop-postgres` gets ahead of your branch and
+    # has some files which you don't have
+
+    if (
+        not filePath.startswith("lib")
+        or not filePath.endswith(".dart")
+        or not os.path.exists(filePath)
+    ):
         return False
-    
-     # Exclude generated files
-    if 'generated' in filePath or filePath == 'lib/generated_plugin_registrant.dart':
+
+    # Exclude generated files
+    if (
+        "generated" in filePath
+        or filePath == "lib/generated_plugin_registrant.dart"
+    ):
         return False
-    
+
     with open(filePath, "r") as file:
         lines = file.readlines()
 
@@ -105,44 +128,69 @@ def _check_for_ignore_directive(filePath):
             content = line.strip()
 
             # If the line is empty or is a documentation (`///`)
-            if (content.endswith('/') or content == ''):
+            if content.endswith("/") or content == "":
                 continue
 
             # Points to the character just after '/' ends.
             # This is required because '/' can occur many times.
             non_comment_pos = 0
 
-            while content[non_comment_pos] == '/':
+            while content[non_comment_pos] == "/":
                 non_comment_pos += 1
 
             # Remove whitespace from non '/' part and split to whitespace
-            content = (content[non_comment_pos:].strip()).split(' ')
+            content = (content[non_comment_pos:].strip()).split(" ")
 
-            # filter out empty strings, which happen because of 'sdf <a lot of spaces> sdf'
+            # Filter out empty strings, which happen because of
+            # 'sdf <a lot of spaces> sdf'
             # .split(' ') adds these whitespaces to the token list too.
-            content = [token for token in content if token != '']
+            content = [token for token in content if token != ""]
 
             # if somehow we still ended up with whitespaces.
-            if (len(content) == 0):
+            if len(content) == 0:
                 break
 
             # After removing the '/'s, only two possibilities remain
-            # 1. The `ignore...:` and `lint_rule_name` have no whitespace between them
+            # 1. The `ignore...:` and `lint_rule_name` have no whitespace
+            #    between them
             # 2. They have some whitespace(s) between them
 
-            if ((len(content) == 1 and
-                (content[0] == 'ignore_for_file:talawa_api_doc'
-                or content[0] == 'ignore:talawa_api_doc'
-                or content[0] == 'ignore_for_file:talawa_good_doc_comments'
-                    or content[0] == 'ignore:talawa_good_doc_comments'))
-                or (len(content) == 2 and
-                ((content[0] == 'ignore_for_file:' and content[1] == 'talawa_api_doc')
-                 or (content[0] == 'ignore:' and content[1] == 'talawa_api_doc')
-                 or (content[0] == 'ignore_for_file:' and content[1] == 'talawa_good_doc_comments')
-                    or (content[0] == 'ignore:' and content[1] == 'talawa_good_doc_comments')))):
+            if (
+                len(content) == 1
+                and (
+                    content[0] == "ignore_for_file:talawa_api_doc"
+                    or content[0] == "ignore:talawa_api_doc"
+                    or content[0] == "ignore_for_file:talawa_good_doc_comments"
+                    or content[0] == "ignore:talawa_good_doc_comments"
+                )
+            ) or (
+                len(content) == 2
+                and (
+                    (
+                        content[0] == "ignore_for_file:"
+                        and content[1] == "talawa_api_doc"
+                    )
+                    or (
+                        content[0] == "ignore:"
+                        and content[1] == "talawa_api_doc"
+                    )
+                    or (
+                        content[0] == "ignore_for_file:"
+                        and content[1] == "talawa_good_doc_comments"
+                    )
+                    or (
+                        content[0] == "ignore:"
+                        and content[1] == "talawa_good_doc_comments"
+                    )
+                )
+            ):
 
-                print(("Ignore directive found at line {}, which suppresses a custom lint rule.\n"
-                    "Please remove this suppression and add valid documentation for the respective field(s).").format(index))
+                print(
+                    f"""\
+Ignore directive found at line {index}, which suppresses a custom lint rule.
+Please remove this suppression and add valid documentation for \
+the respective field(s)."""
+                )
 
                 return True
 
@@ -155,20 +203,31 @@ def main():
     Analyzes and prints the files that contain suppression for any custom
     lint rule
 
+    Args:
+        None
+
     Returns:
         None
 
     """
-    
-    print(subprocess.check_output(['git', 'branch']).decode('utf-8').strip('\n'))
-    
-    current_branch = subprocess.check_output(['git', 'branch', '--show-current']).decode('utf-8').strip('\n')
+    print(
+        subprocess.check_output(["git", "branch"]).decode("utf-8").strip("\n")
+    )
+
+    current_branch = (
+        subprocess.check_output(["git", "branch", "--show-current"])
+        .decode("utf-8")
+        .strip("\n")
+    )
     print(current_branch)
-    changed_files = subprocess.check_output(['git', 'diff', '--name-only', 'develop-postgres', current_branch]).decode('utf-8').splitlines()
+    changed_files = (
+        subprocess.check_output(
+            ["git", "diff", "--name-only", "develop-postgres", current_branch]
+        )
+        .decode("utf-8")
+        .splitlines()
+    )
     print(changed_files)
-    
-    
-    args = _arg_parser_resolver()
 
     # List of files where ignore directive is found
     err = []
@@ -181,16 +240,20 @@ def main():
     # If no changed/modified contains ignore directive for any custom lint rule
     if len(err) == 0:
         print(
-            '''🚀 {} Hurrah! No ignore directive found in any modified/added file(s)'''.format('\033[92m'))
+            """\
+Hurrah! No ignore directive found in any modified/added file(s)"""
+        )
         sys.exit(0)
     else:
         print(
-            '''🔍 {}Ignore directive for custom lint rule found. Please remove them and add valid documentation.'''.format(
-                '\033[91m'))
+            """\
+Ignore directive for custom lint rule found. \
+Please remove them and add valid documentation."""
+        )
         for failing_file in err:
-            print('''>>> File name: {}\n'''.format(failing_file))
+            print(f""">>> File name: {failing_file}\n""")
         sys.exit(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- .github/workflows/scripts/compare_translations.py
+++ .github/workflows/scripts/compare_translations.py
@@ -1,50 +1,8 @@
-"""
-Script to encourage more efficient coding practices.
+#!/usr/bin/env python3
+"""Utility for comparing translations between default and other languages.
 
-Methodology:
-
-    Utility for comparing translations between default and other languages.
-
-    This module defines a function to compare two translations
-    and print any missing keys in the other language's translation.
-Attributes:
-
-    FileTranslation : Named tuple to represent a combination of file and missing translations.
-
-        Fields:
-            - file (str): The file name.
-            - missing_translations (list): List of missing translations.
-
-Functions:
-    compare_translations(default_translation, other_translation):
-        Compare two translations and print missing keys.
-
-     load_translation(filepath):
-        Load translation from a file.
-
-    check_translations():
-        Load the default translation and compare it with other translations.
-
-     main():
-        The main function to run the script.
-        Parses command-line arguments, checks for the existence of the specified directory,
-        and then calls check_translations with the provided or default directory.
-
-
-Usage:
-    This script can be executed to check and print missing
-    translations in other languages based on the default English translation.
-
-Example:
-    python compare_translations.py
-NOTE:
-    This script complies with our python3 coding and documentation standards
-    and should be used as a reference guide. It complies with:
-
-        1) Pylint
-        2) Pydocstyle
-        3) Pycodestyle
-        4) Flake8
+This module defines a function to compare two translations
+and print any missing keys in the other language's translation.
 
 """
 # standard imports
@@ -55,11 +13,17 @@ import sys
 from collections import namedtuple
 
 # Named tuple for file and missing translations combination
-FileTranslation = namedtuple("FileTranslation", ["file", "missing_translations"])
+FileTranslation = namedtuple(
+    "FileTranslation", ["file", "missing_translations"]
+)
 
 
-def compare_translations(default_translation, other_translation, default_file, other_file):
-    """Compare two translations and return detailed info about missing/mismatched keys.
+def compare_translations(
+    default_translation, other_translation, default_file, other_file
+):
+    """Compare two translations.
+
+    Returns detailed info about missing/mismatched keys.
 
     Args:
         default_translation (dict): The default translation (en.json).
@@ -75,18 +39,21 @@ def compare_translations(default_translation, other_translation, default_file, o
     # Check for missing keys in other_translation
     for key in default_translation:
         if key not in other_translation:
-            error_msg = f"Missing Key: '{key}' - This key from '{default_file}' is missing in '{other_file}'."
+            error_msg = f"""\
+Missing Key: '{key}' - This key from '{default_file}' \
+is missing in '{other_file}'."""
             errors.append(error_msg)
 
-    # Check for keys in other_translation that don't match any in default_translation
+    # Check for keys in other_translation that don't
+    # match any in default_translation
     for key in other_translation:
         if key not in default_translation:
-            error_msg = f"Error Key: '{key}' - This key in '{other_file}' does not match any key in '{default_file}'."
+            error_msg = f"""\
+Error Key: '{key}' - This key in '{other_file}' \
+does not match any key in '{default_file}'."""
             errors.append(error_msg)
 
     return errors
-
-
 
 
 def load_translation(filepath):
@@ -103,7 +70,6 @@ def load_translation(filepath):
     return translation
 
 
-
 def check_translations(directory):
     """Load default translation and compare with other translations.
 
@@ -114,7 +80,9 @@ def check_translations(directory):
         None
     """
     default_file = "en.json"
-    default_translation = load_translation(os.path.join(directory, default_file))
+    default_translation = load_translation(
+        os.path.join(directory, default_file)
+    )
     translations = os.listdir(directory)
     translations.remove(default_file)  # Exclude default translation
 
@@ -125,7 +93,12 @@ def check_translations(directory):
         other_translation = load_translation(other_file)
 
         # Compare translations and get detailed error messages
-        errors = compare_translations(default_translation, other_translation, default_file, translation_file)
+        errors = compare_translations(
+            default_translation,
+            other_translation,
+            default_file,
+            translation_file,
+        )
         if errors:
             error_found = True
             print(f"File {translation_file} has missing translations for:")
@@ -140,25 +113,36 @@ def check_translations(directory):
 
 
 def main():
-    """
-     Parse command-line arguments, check for the existence of the specified directory,
+    """Parse command-line arguments.
+
+    Check for the existence of the specified directory,
     and call check_translations with the provided or default directory.
+
+    Args:
+        None
+
+    Returns:
+        None
 
     """
     parser = argparse.ArgumentParser(
-        description="Check and print missing translations for all non-default languages."
+        description="""\
+Check and print missing translations for all non-default languages."""
     )
     parser.add_argument(
         "--directory",
         type=str,
         nargs="?",
         default=os.path.join(os.getcwd(), "lang"),
-        help="Directory containing translation files(relative to the root directory).",
+        help="""\
+Directory containing translation files(relative to the root directory).""",
     )
     args = parser.parse_args()
 
     if not os.path.exists(args.directory):
-        print(f"Error: The specified directory '{args.directory}' does not exist.")
+        print(
+            f"Error: The specified directory '{args.directory}' does not exist."
+        )
         sys.exit(1)
 
     check_translations(args.directory)
