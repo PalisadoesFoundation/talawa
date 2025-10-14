2a03eecad4e18e5bf1b4551b6d9c167d009d8f0b
JXP25 <jai.pannu25@gmail.com>
2025-10-14 19:21:14 +0530

    prebundle

--- lib/plugin/available/index.dart
+++ lib/plugin/available/index.dart
@@ -1,4 +1,3 @@
-import 'package:talawa/plugin/available/hello_world/index.dart';
 import 'package:talawa/plugin/types.dart';
 import 'package:talawa/plugin/available/plugin_map/index.dart';
 import 'package:talawa/plugin/available/razorpay/index.dart';
@@ -6,7 +5,6 @@ import 'package:talawa/plugin/available/summarize_t5/index.dart';
 
 /// Pre-bundled plugins in the mobile app.
 List<TalawaMobilePlugin> getBundledPlugins() => [
-      HelloWorldPlugin(),
       PluginMapAppPlugin(),
       RazorpayPlugin(),
       SummarizeT5Plugin(),
