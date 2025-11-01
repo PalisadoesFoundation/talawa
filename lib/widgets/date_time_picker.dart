265401427218e77d8cef5b6623c33d3fa918a223
Mohit Maulekhi <mohitmaulekhi312@gmail.com>
2025-11-02 02:28:35 +0530

    Updated tests

--- lib/widgets/date_time_picker.dart
+++ lib/widgets/date_time_picker.dart
@@ -3,8 +3,11 @@ import 'package:talawa/locator.dart';
 
 /// Shows a dialog containing a Material Design date picker.
 ///
-/// The returned Future resolves to the date selected by the user when the user confirms the dialog.
-/// If the user cancels the dialog, null is returned.
+/// **params**:
+/// * `initialDate`: The initially selected date that the picker will show.
+///
+/// **returns**:
+/// * `Future<DateTime>`: Resolves to the date selected by the user. If the user cancels the dialog or picks the same `initialDate`, the function returns `initialDate`.
 Future<DateTime> customDatePicker({required DateTime initialDate}) async {
   // showDatePicker which shows a material design date range picker used to select a range of dates.
   final DateTime? picked = await showDatePicker(
@@ -19,10 +22,13 @@ Future<DateTime> customDatePicker({required DateTime initialDate}) async {
   return initialDate;
 }
 
-/// Shows a dialog containing a material design time picker.
+/// Shows a dialog containing a Material Design time picker.
 ///
-/// The returned Future resolves to the time selected by the user when the user closes the dialog.
-/// If the user cancels the dialog, null is returned.
+/// **params**:
+/// * `initialTime`: The initially selected time that the picker will show.
+///
+/// **returns**:
+/// * `Future<TimeOfDay>`: Resolves to the time chosen by the user. If the user cancels or picks the same `initialTime`, the function returns `initialTime`.
 Future<TimeOfDay> customTimePicker({required TimeOfDay initialTime}) async {
   // showTimePicker which shows a material design time range picker used to select a range of times.
   // Click ![here](https://api.flutter.dev/flutter/material/showTimePicker.html) to know more.
