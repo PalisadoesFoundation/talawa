05a2b5c5d0bf34f79cfd1073188621738ae27925
nsamuelreddy <nareshsamuelreddy@gmail.com>
2025-11-29 22:17:25 +0530

    refactor: address CodeRabbit review suggestions
    - Replace MockBuildContext with real context from Builder widget
    - Remove unused mock classes (MockBuildContext, MockMainScreenViewModel, MockCustomDrawerViewModel)
    - Remove unused imports (foundation, mockito, org_info, main_screen_view_model)
    - Rename second group to 'MainScreen drawer & demo mode' for clarity
    - Replace large commented test block with TODO comment

--- test/widget_tests/widgets/custom_drawer_test.dart
+++ test/widget_tests/widgets/custom_drawer_test.dart
@@ -1,55 +1,18 @@
 // ignore_for_file: talawa_api_doc
 // ignore_for_file: talawa_good_doc_comments
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
 import 'package:talawa/constants/custom_theme.dart';
 import 'package:talawa/models/mainscreen_navigation_args.dart';
-import 'package:talawa/models/organization/org_info.dart';
 import 'package:talawa/services/graphql_config.dart';
 import 'package:talawa/services/size_config.dart';
 import 'package:talawa/utils/app_localization.dart';
-import 'package:talawa/view_model/main_screen_view_model.dart';
 import 'package:talawa/view_model/widgets_view_models/custom_drawer_view_model.dart';
 import 'package:talawa/views/main_screen.dart';
 import '../../helpers/test_helpers.dart';
 import '../../helpers/test_locator.dart';
-
-class MockBuildContext extends Mock implements BuildContext {
-  @override
-  DiagnosticsNode describeElement(
-    String name, {
-    DiagnosticsTreeStyle style = DiagnosticsTreeStyle.errorProperty,
-  }) {
-    return DiagnosticsNode.message('');
-  }
-
-  @override
-  Widget get widget => const SizedBox();
-
-  @override
-  bool get debugDoingBuild => false;
-}
-
-class MockMainScreenViewModel extends Mock implements MainScreenViewModel {}
-
-class MockCustomDrawerViewModel extends Mock implements CustomDrawerViewModel {
-  final List<OrgInfo> switchAbleOrgValue = [];
-  OrgInfo? selectedOrgValue;
-  final ScrollController controllerValue = ScrollController();
-
-  @override
-  List<OrgInfo> get switchAbleOrg => switchAbleOrgValue;
-
-  @override
-  OrgInfo? get selectedOrg => selectedOrgValue;
-
-  @override
-  ScrollController get controller => controllerValue;
-}
 
 Widget createHomePageScreen({required bool demoMode}) {
   return MaterialApp(
@@ -100,8 +63,10 @@ void main() {
         ],
         themeMode: ThemeMode.light,
         theme: TalawaTheme.lightTheme,
-        home: Scaffold(
-          body: customDrawerViewModel.exitAlertDialog(MockBuildContext()),
+        home: Builder(
+          builder: (context) => Scaffold(
+            body: customDrawerViewModel.exitAlertDialog(context),
+          ),
         ),
       );
 
@@ -117,7 +82,7 @@ void main() {
     });
   });
 
-  group('Test Organization action Buttons', () {
+  group('MainScreen drawer & demo mode', () {
     testWidgets('Test main screen renders with drawer capability',
         (tester) async {
       await tester.pumpWidget(createHomePageScreen(demoMode: true));
@@ -154,57 +119,7 @@ void main() {
   });
 
   group('Custom Drawer Test', () {
-    /*testWidgets("Widget Testing", (tester) async {
-      // pumping the Widget
-      await tester.pumpWidget(createHomePageScreen());
-      await tester.pumpAndSettle();
-      // Opening the Drawer so that it can be loaded in the widget tree and built() is called
-      await tester.dragFrom(
-          tester.getTopLeft(find.byType(MaterialApp)), const Offset(300, 0));
-      await tester.pumpAndSettle();
-      // getting the Finders for Code Coverage
-      expect(find.byKey(const ValueKey("Drawer")), findsOneWidget);
-      expect(find.byKey(const ValueKey("Custom Drawer")), findsOneWidget);
-      expect(find.text("Selected Organization"), findsOneWidget);
-      expect(find.text("Switch Organization"), findsOneWidget);
-      final listOfOrgs = find.byKey(const ValueKey("Switching Org"));
-      expect(listOfOrgs, findsOneWidget);
-      expect(find.byKey(MainScreenViewModel.keyDrawerCurOrg), findsOneWidget);
-      expect(find.byKey(MainScreenViewModel.keyDrawerSwitchableOrg),
-          findsOneWidget);
-      expect(find.byType(UserAccountsDrawerHeader), findsOneWidget);
-      expect(find.text("Join new Organization"), findsOneWidget);
-      expect(find.text("Leave Current Organization"), findsOneWidget);
-      final fromPalisadoes = find.byKey(const ValueKey("From Palisadoes"));
-      expect(fromPalisadoes, findsOneWidget);
-    });
-    testWidgets("Tapping Tests for Org", (tester) async {
-      await tester.pumpWidget(createHomePageScreen());
-      await tester.pumpAndSettle();
-      // Opening the Drawer so that it can be loaded in the widget tree and built() is called
-      await tester.dragFrom(
-          tester.getTopLeft(find.byType(MaterialApp)), const Offset(300, 0));
-      await tester.pumpAndSettle();
-      final orgs = find.byKey(const ValueKey("Org"));
-      // Atleast One Org should be there
-      // ignore: invalid_use_of_protected_member
-      expect(orgs.allCandidates.isEmpty, false);
-      await tester.tap(orgs.first);
-      // Was not required but done for code Coverage
-      // Sized
-      final sizedbox = find.byKey(const ValueKey("Sized Box Drawer"));
-      // ignore: invalid_use_of_protected_member
-      expect(sizedbox.allCandidates.isEmpty, false);
-    });
-    testWidgets("Tapping Tests for Join", (tester) async {
-      await tester.pumpWidget(createHomePageScreen());
-      await tester.pumpAndSettle();
-      tester.binding.window.physicalSizeTestValue = const Size(800, 4000);
-      MainScreenViewModel.scaffoldKey.currentState?.openDrawer();
-      await tester.pumpAndSettle();
-      final joinOrg = find.byKey(MainScreenViewModel.keyDrawerJoinOrg);
-      await tester.tap(joinOrg);
-      // await tester.pumpAndSettle();
-    });*/
+    // TODO: Restore drawer interaction widget tests for the custom drawer
+    // once the drawer UI and navigation flows are stabilized.
   });
 }
