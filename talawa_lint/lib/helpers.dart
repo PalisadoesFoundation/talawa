dd5e4ca2cfd26ebd68df19401e33b386caf59b56
Swsatik <swastikroy157@gmail.com>
2025-12-27 02:17:00 +0530

    fix: resolve pub get failure by removing macros override  and update talawa_lint to current analyzer APIs

--- talawa_lint/lib/helpers.dart
+++ talawa_lint/lib/helpers.dart
@@ -147,7 +147,8 @@ bool isPrivate(Token? name) =>
 class TalawaLintHelpers {
   static bool isVoid(Declaration node) {
     return returnType(node) is VoidType ||
-        returnType(node)?.getDisplayString() == "Future<void>";
+        returnType(node)?.getDisplayString(withNullability: false) ==
+            "Future<void>";
   }
 
   static bool isImplicitReturn(Declaration node) {
