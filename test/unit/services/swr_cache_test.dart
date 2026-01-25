bcf47fdb96ce11c36948a36586cac973db352a84
aviralgarg05 <gargaviral99@gmail.com>
2026-01-26 01:00:03 +0530

    style: fix formatting in swr_cache_test.dart

--- test/unit/services/swr_cache_test.dart
+++ test/unit/services/swr_cache_test.dart
@@ -74,7 +74,8 @@ void main() {
 
     test('revalidate propagates fetcher exceptions', () async {
       expect(
-        () => cache.revalidate<String>('key', () async => throw Exception('fetch failed')),
+        () => cache.revalidate<String>(
+            'key', () async => throw Exception('fetch failed')),
         throwsException,
       );
       expect(cache.get<String>('key'), isNull);
