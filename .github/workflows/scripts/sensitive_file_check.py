283dc81871d275b4ac924fe31d4f18245fdbbc87
Nallana Hari Krishna <322107311061@andhrauniversity.edu.in>
2026-02-26 08:06:07 +0530

    fix: fix E501 line too long in sensitive_file_check.py

--- .github/workflows/scripts/sensitive_file_check.py
+++ .github/workflows/scripts/sensitive_file_check.py
@@ -130,7 +130,8 @@ def _filepaths_in_directories(directories):
     for directory in directories:
         if not os.path.isdir(directory):
             print(
-                f"Error: Directory '{directory}' not found or is not a valid directory."
+                f"Error: Directory '{directory}' not found"
+                " or is not a valid directory."
             )
             sys.exit(1)
         for root, _, files in os.walk(directory, topdown=False):
